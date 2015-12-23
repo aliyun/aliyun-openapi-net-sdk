21b375c852a8c3ff251f427d26e774c60942ea72
hi.yan.li <hi.yan.li@alibaba-inc.com>
2015-12-23 19:50:37 +0800

    push product sdk first commit

---
+++ aliyun-net-sdk-push/Push/Model/V20150827/PushRequest.cs
@@ -0,0 +1,341 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using Aliyun.Acs.Core;
+using Aliyun.Acs.Core.Http;
+using Aliyun.Acs.Core.Transform;
+using Aliyun.Acs.Core.Utils;
+using Aliyun.Acs.Push.Transform;
+using Aliyun.Acs.Push.Transform.V20150827;
+using System.Collections.Generic;
+
+namespace Aliyun.Acs.Push.Model.V20150827
+{
+    public class PushRequest : RpcAcsRequest<PushResponse>
+    {
+        public PushRequest()
+            : base("Push", "2015-08-27", "Push")
+        {
+        }
+
+		private long? appKey;
+
+		private string target;
+
+		private string targetValue;
+
+		private int? type;
+
+		private string title;
+
+		private bool? remind;
+
+		private bool? storeOffline;
+
+		private string body;
+
+		private int? deviceType;
+
+		private int? timeOut;
+
+		private string summary;
+
+		private string androidActivity;
+
+		private string androidOpenUrl;
+
+		private string androidExtParameters;
+
+		private string androidOpenType;
+
+		private string androidMusic;
+
+		private string batchNumber;
+
+		private string iOSMusic;
+
+		private string iOSBadge;
+
+		private string iOSExtParameters;
+
+		public long? AppKey
+		{
+			get
+			{
+				return appKey;
+			}
+			set	
+			{
+				appKey = value;
+				DictionaryUtil.Add(QueryParameters, "AppKey", value.ToString());
+			}
+		}
+
+		public string Target
+		{
+			get
+			{
+				return target;
+			}
+			set	
+			{
+				target = value;
+				DictionaryUtil.Add(QueryParameters, "Target", value);
+			}
+		}
+
+		public string TargetValue
+		{
+			get
+			{
+				return targetValue;
+			}
+			set	
+			{
+				targetValue = value;
+				DictionaryUtil.Add(QueryParameters, "TargetValue", value);
+			}
+		}
+
+		public int? Type
+		{
+			get
+			{
+				return type;
+			}
+			set	
+			{
+				type = value;
+				DictionaryUtil.Add(QueryParameters, "Type", value.ToString());
+			}
+		}
+
+		public string Title
+		{
+			get
+			{
+				return title;
+			}
+			set	
+			{
+				title = value;
+				DictionaryUtil.Add(QueryParameters, "Title", value);
+			}
+		}
+
+		public bool? Remind
+		{
+			get
+			{
+				return remind;
+			}
+			set	
+			{
+				remind = value;
+				DictionaryUtil.Add(QueryParameters, "Remind", value.ToString());
+			}
+		}
+
+		public bool? StoreOffline
+		{
+			get
+			{
+				return storeOffline;
+			}
+			set	
+			{
+				storeOffline = value;
+				DictionaryUtil.Add(QueryParameters, "StoreOffline", value.ToString());
+			}
+		}
+
+		public string Body
+		{
+			get
+			{
+				return body;
+			}
+			set	
+			{
+				body = value;
+				DictionaryUtil.Add(QueryParameters, "Body", value);
+			}
+		}
+
+		public int? DeviceType
+		{
+			get
+			{
+				return deviceType;
+			}
+			set	
+			{
+				deviceType = value;
+				DictionaryUtil.Add(QueryParameters, "DeviceType", value.ToString());
+			}
+		}
+
+		public int? TimeOut
+		{
+			get
+			{
+				return timeOut;
+			}
+			set	
+			{
+				timeOut = value;
+				DictionaryUtil.Add(QueryParameters, "TimeOut", value.ToString());
+			}
+		}
+
+		public string Summary
+		{
+			get
+			{
+				return summary;
+			}
+			set	
+			{
+				summary = value;
+				DictionaryUtil.Add(QueryParameters, "Summary", value);
+			}
+		}
+
+		public string AndroidActivity
+		{
+			get
+			{
+				return androidActivity;
+			}
+			set	
+			{
+				androidActivity = value;
+				DictionaryUtil.Add(QueryParameters, "AndroidActivity", value);
+			}
+		}
+
+		public string AndroidOpenUrl
+		{
+			get
+			{
+				return androidOpenUrl;
+			}
+			set	
+			{
+				androidOpenUrl = value;
+				DictionaryUtil.Add(QueryParameters, "AndroidOpenUrl", value);
+			}
+		}
+
+		public string AndroidExtParameters
+		{
+			get
+			{
+				return androidExtParameters;
+			}
+			set	
+			{
+				androidExtParameters = value;
+				DictionaryUtil.Add(QueryParameters, "AndroidExtParameters", value);
+			}
+		}
+
+		public string AndroidOpenType
+		{
+			get
+			{
+				return androidOpenType;
+			}
+			set	
+			{
+				androidOpenType = value;
+				DictionaryUtil.Add(QueryParameters, "AndroidOpenType", value);
+			}
+		}
+
+		public string AndroidMusic
+		{
+			get
+			{
+				return androidMusic;
+			}
+			set	
+			{
+				androidMusic = value;
+				DictionaryUtil.Add(QueryParameters, "AndroidMusic", value);
+			}
+		}
+
+		public string BatchNumber
+		{
+			get
+			{
+				return batchNumber;
+			}
+			set	
+			{
+				batchNumber = value;
+				DictionaryUtil.Add(QueryParameters, "BatchNumber", value);
+			}
+		}
+
+		public string iOSMusic
+		{
+			get
+			{
+				return iOSMusic;
+			}
+			set	
+			{
+				iOSMusic = value;
+				DictionaryUtil.Add(QueryParameters, "iOSMusic", value);
+			}
+		}
+
+		public string iOSBadge
+		{
+			get
+			{
+				return iOSBadge;
+			}
+			set	
+			{
+				iOSBadge = value;
+				DictionaryUtil.Add(QueryParameters, "iOSBadge", value);
+			}
+		}
+
+		public string iOSExtParameters
+		{
+			get
+			{
+				return iOSExtParameters;
+			}
+			set	
+			{
+				iOSExtParameters = value;
+				DictionaryUtil.Add(QueryParameters, "iOSExtParameters", value);
+			}
+		}
+
+        public override PushResponse GetResponse(Core.Transform.UnmarshallerContext unmarshallerContext)
+        {
+            return PushResponseUnmarshaller.Unmarshall(unmarshallerContext);
+        }
+    }
+}
