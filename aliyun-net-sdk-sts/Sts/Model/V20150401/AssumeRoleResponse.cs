ed0798071204a39acb93361f71ed0583278571b1
hi.yan.li <hi.yan.li@alibaba-inc.com>
2016-03-08 13:53:51 +0800

    add sts sdk

---
+++ aliyun-net-sdk-sts/Sts/Model/V20150401/AssumeRoleResponse.cs
@@ -0,0 +1,145 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using Aliyun.Acs.Core;
+using System.Collections.Generic;
+
+namespace Aliyun.Acs.Sts.Model.V20150401
+{
+	public class AssumeRoleResponse : AcsResponse
+	{
+
+		private Credentials_ credentials;
+
+		private AssumedRoleUser_ assumedRoleUser;
+
+		public Credentials_ Credentials
+		{
+			get
+			{
+				return credentials;
+			}
+			set	
+			{
+				credentials = value;
+			}
+		}
+
+		public AssumedRoleUser_ AssumedRoleUser
+		{
+			get
+			{
+				return assumedRoleUser;
+			}
+			set	
+			{
+				assumedRoleUser = value;
+			}
+		}
+
+		public class Credentials_{
+
+			private string securityToken;
+
+			private string accessKeySecret;
+
+			private string accessKeyId;
+
+			private string expiration;
+
+			public string SecurityToken
+			{
+				get
+				{
+					return securityToken;
+				}
+				set	
+				{
+					securityToken = value;
+				}
+			}
+
+			public string AccessKeySecret
+			{
+				get
+				{
+					return accessKeySecret;
+				}
+				set	
+				{
+					accessKeySecret = value;
+				}
+			}
+
+			public string AccessKeyId
+			{
+				get
+				{
+					return accessKeyId;
+				}
+				set	
+				{
+					accessKeyId = value;
+				}
+			}
+
+			public string Expiration
+			{
+				get
+				{
+					return expiration;
+				}
+				set	
+				{
+					expiration = value;
+				}
+			}
+		}
+
+		public class AssumedRoleUser_{
+
+			private string arn;
+
+			private string assumedRoleId;
+
+			public string Arn
+			{
+				get
+				{
+					return arn;
+				}
+				set	
+				{
+					arn = value;
+				}
+			}
+
+			public string AssumedRoleId
+			{
+				get
+				{
+					return assumedRoleId;
+				}
+				set	
+				{
+					assumedRoleId = value;
+				}
+			}
+		}
+	}
+}
