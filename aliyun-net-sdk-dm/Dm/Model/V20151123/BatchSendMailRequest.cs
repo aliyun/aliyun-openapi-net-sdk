aa44feb0c8d6cdebf4e64610fdd8f95c0eca0f7a
liyan2011 <346340936@qq.com>
2016-04-22 17:43:42 +0800

    dm sdk first push

---
+++ aliyun-net-sdk-dm/Dm/Model/V20151123/BatchSendMailRequest.cs
@@ -0,0 +1,162 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using Aliyun.Acs.Core;
+using Aliyun.Acs.Core.Http;
+using Aliyun.Acs.Core.Transform;
+using Aliyun.Acs.Core.Utils;
+using Aliyun.Acs.Dm.Transform;
+using Aliyun.Acs.Dm.Transform.V20151123;
+using System.Collections.Generic;
+
+namespace Aliyun.Acs.Dm.Model.V20151123
+{
+    public class BatchSendMailRequest : RpcAcsRequest<BatchSendMailResponse>
+    {
+        public BatchSendMailRequest()
+            : base("Dm", "2015-11-23", "BatchSendMail")
+        {
+            Method = MethodType.POST;
+        }
+
+		private long? ownerId;
+
+		private string resourceOwnerAccount;
+
+		private long? resourceOwnerId;
+
+		private string templateName;
+
+		private string accountName;
+
+		private string receiversName;
+
+		private int? addressType;
+
+		private string tagName;
+
+		public long? OwnerId
+		{
+			get
+			{
+				return ownerId;
+			}
+			set	
+			{
+				ownerId = value;
+				DictionaryUtil.Add(QueryParameters, "OwnerId", value.ToString());
+			}
+		}
+
+		public string ResourceOwnerAccount
+		{
+			get
+			{
+				return resourceOwnerAccount;
+			}
+			set	
+			{
+				resourceOwnerAccount = value;
+				DictionaryUtil.Add(QueryParameters, "ResourceOwnerAccount", value);
+			}
+		}
+
+		public long? ResourceOwnerId
+		{
+			get
+			{
+				return resourceOwnerId;
+			}
+			set	
+			{
+				resourceOwnerId = value;
+				DictionaryUtil.Add(QueryParameters, "ResourceOwnerId", value.ToString());
+			}
+		}
+
+		public string TemplateName
+		{
+			get
+			{
+				return templateName;
+			}
+			set	
+			{
+				templateName = value;
+				DictionaryUtil.Add(QueryParameters, "TemplateName", value);
+			}
+		}
+
+		public string AccountName
+		{
+			get
+			{
+				return accountName;
+			}
+			set	
+			{
+				accountName = value;
+				DictionaryUtil.Add(QueryParameters, "AccountName", value);
+			}
+		}
+
+		public string ReceiversName
+		{
+			get
+			{
+				return receiversName;
+			}
+			set	
+			{
+				receiversName = value;
+				DictionaryUtil.Add(QueryParameters, "ReceiversName", value);
+			}
+		}
+
+		public int? AddressType
+		{
+			get
+			{
+				return addressType;
+			}
+			set	
+			{
+				addressType = value;
+				DictionaryUtil.Add(QueryParameters, "AddressType", value.ToString());
+			}
+		}
+
+		public string TagName
+		{
+			get
+			{
+				return tagName;
+			}
+			set	
+			{
+				tagName = value;
+				DictionaryUtil.Add(QueryParameters, "TagName", value);
+			}
+		}
+
+        public override BatchSendMailResponse GetResponse(Core.Transform.UnmarshallerContext unmarshallerContext)
+        {
+            return BatchSendMailResponseUnmarshaller.Unmarshall(unmarshallerContext);
+        }
+    }
+}
---
+++ aliyun-net-sdk-dm/Dm/Model/V20151123/SingleSendMailRequest.cs
@@ -0,0 +1,222 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using Aliyun.Acs.Core;
+using Aliyun.Acs.Core.Http;
+using Aliyun.Acs.Core.Transform;
+using Aliyun.Acs.Core.Utils;
+using Aliyun.Acs.Dm.Transform;
+using Aliyun.Acs.Dm.Transform.V20151123;
+using System.Collections.Generic;
+
+namespace Aliyun.Acs.Dm.Model.V20151123
+{
+    public class SingleSendMailRequest : RpcAcsRequest<SingleSendMailResponse>
+    {
+        public SingleSendMailRequest()
+            : base("Dm", "2015-11-23", "SingleSendMail")
+        {
+            Method = MethodType.POST;
+        }
+
+		private long? ownerId;
+
+		private string resourceOwnerAccount;
+
+		private long? resourceOwnerId;
+
+		private string accountName;
+
+		private int? addressType;
+
+		private string tagName;
+
+		private bool? replyToAddress;
+
+		private string toAddress;
+
+		private string subject;
+
+		private string htmlBody;
+
+		private string textBody;
+
+		private string fromAlias;
+
+		public long? OwnerId
+		{
+			get
+			{
+				return ownerId;
+			}
+			set	
+			{
+				ownerId = value;
+				DictionaryUtil.Add(QueryParameters, "OwnerId", value.ToString());
+			}
+		}
+
+		public string ResourceOwnerAccount
+		{
+			get
+			{
+				return resourceOwnerAccount;
+			}
+			set	
+			{
+				resourceOwnerAccount = value;
+				DictionaryUtil.Add(QueryParameters, "ResourceOwnerAccount", value);
+			}
+		}
+
+		public long? ResourceOwnerId
+		{
+			get
+			{
+				return resourceOwnerId;
+			}
+			set	
+			{
+				resourceOwnerId = value;
+				DictionaryUtil.Add(QueryParameters, "ResourceOwnerId", value.ToString());
+			}
+		}
+
+		public string AccountName
+		{
+			get
+			{
+				return accountName;
+			}
+			set	
+			{
+				accountName = value;
+				DictionaryUtil.Add(QueryParameters, "AccountName", value);
+			}
+		}
+
+		public int? AddressType
+		{
+			get
+			{
+				return addressType;
+			}
+			set	
+			{
+				addressType = value;
+				DictionaryUtil.Add(QueryParameters, "AddressType", value.ToString());
+			}
+		}
+
+		public string TagName
+		{
+			get
+			{
+				return tagName;
+			}
+			set	
+			{
+				tagName = value;
+				DictionaryUtil.Add(QueryParameters, "TagName", value);
+			}
+		}
+
+		public bool? ReplyToAddress
+		{
+			get
+			{
+				return replyToAddress;
+			}
+			set	
+			{
+				replyToAddress = value;
+				DictionaryUtil.Add(QueryParameters, "ReplyToAddress", value.ToString());
+			}
+		}
+
+		public string ToAddress
+		{
+			get
+			{
+				return toAddress;
+			}
+			set	
+			{
+				toAddress = value;
+				DictionaryUtil.Add(QueryParameters, "ToAddress", value);
+			}
+		}
+
+		public string Subject
+		{
+			get
+			{
+				return subject;
+			}
+			set	
+			{
+				subject = value;
+				DictionaryUtil.Add(QueryParameters, "Subject", value);
+			}
+		}
+
+		public string HtmlBody
+		{
+			get
+			{
+				return htmlBody;
+			}
+			set	
+			{
+				htmlBody = value;
+				DictionaryUtil.Add(QueryParameters, "HtmlBody", value);
+			}
+		}
+
+		public string TextBody
+		{
+			get
+			{
+				return textBody;
+			}
+			set	
+			{
+				textBody = value;
+				DictionaryUtil.Add(QueryParameters, "TextBody", value);
+			}
+		}
+
+		public string FromAlias
+		{
+			get
+			{
+				return fromAlias;
+			}
+			set	
+			{
+				fromAlias = value;
+				DictionaryUtil.Add(QueryParameters, "FromAlias", value);
+			}
+		}
+
+        public override SingleSendMailResponse GetResponse(Core.Transform.UnmarshallerContext unmarshallerContext)
+        {
+            return SingleSendMailResponseUnmarshaller.Unmarshall(unmarshallerContext);
+        }
+    }
+}
