a3c05ea916174f1ed9da09e98079fed3d66976f7
haowei.yao <haowei.yao@alibaba-inc.com>
2017-07-19 15:16:02 +0800

    由青塘发起的RELEASE-TEST SDK自动发布, BUILD_ID=132, 版本号：4.4.8 发布日志： 1,  这是一次测试发布，用来测试SDK自动化发布工具。 2，请忽略。

--- aliyun-net-sdk-release-test/Properties/AssemblyInfo.cs
+++ aliyun-net-sdk-release-test/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("1a8fdd75-bbc1-4fc4-b4fe-3d11da9560ea")]
+[assembly: Guid("b5550e85-def0-40b9-8d7f-027394549311")]
 
 // 程序集的版本信息由下面四个值组成:
 //
