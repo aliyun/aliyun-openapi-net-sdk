e5aba7019b9ebae51c3f1d1a13256b1bb5ea54dc
haowei.yao <haowei.yao@alibaba-inc.com>
2017-08-17 15:27:45 +0800

    由顾怀发起的SAS-API SDK自动发布, BUILD_ID=158, 版本号：2.0.1 发布日志： 1, 首次发布态势感知对外sdk 2, 包含情报api两个接口（查询ip情报，查询手机号情报），和售卖的5个接口（查询实例数，新购，续费，升级，退款） 3, 由于之前已把jar包线下提供给客户，为了与当前保持一致，版本号从2.0.1开始

---
+++ aliyun-net-sdk-sas-api/Sas_api/Model/V20170705/GetIpProfileRequest.cs
@@ -0,0 +1,191 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using Aliyun.Acs.Core;
+using Aliyun.Acs.Core.Http;
+using Aliyun.Acs.Core.Transform;
+using Aliyun.Acs.Core.Utils;
+using Aliyun.Acs.Sas_api.Transform;
+using Aliyun.Acs.Sas_api.Transform.V20170705;
+using System.Collections.Generic;
+
+namespace Aliyun.Acs.Sas_api.Model.V20170705
+{
+    public class GetIpProfileRequest : RpcAcsRequest<GetIpProfileResponse>
+    {
+        public GetIpProfileRequest()
+            : base("Sas_api", "2017-07-05", "GetIpProfile")
+        {
+        }
+
+		private string deviceIdMd5;
+
+		private int? carrier;
+
+		private string os;
+
+		private string requestUrl;
+
+		private string ip;
+
+		private string userAgent;
+
+		private int? connectionType;
+
+		private int? sensType;
+
+		private int? deviceType;
+
+		private int? businessType;
+
+		public string DeviceIdMd5
+		{
+			get
+			{
+				return deviceIdMd5;
+			}
+			set	
+			{
+				deviceIdMd5 = value;
+				DictionaryUtil.Add(QueryParameters, "DeviceIdMd5", value);
+			}
+		}
+
+		public int? Carrier
+		{
+			get
+			{
+				return carrier;
+			}
+			set	
+			{
+				carrier = value;
+				DictionaryUtil.Add(QueryParameters, "Carrier", value.ToString());
+			}
+		}
+
+		public string Os
+		{
+			get
+			{
+				return os;
+			}
+			set	
+			{
+				os = value;
+				DictionaryUtil.Add(QueryParameters, "Os", value);
+			}
+		}
+
+		public string RequestUrl
+		{
+			get
+			{
+				return requestUrl;
+			}
+			set	
+			{
+				requestUrl = value;
+				DictionaryUtil.Add(QueryParameters, "RequestUrl", value);
+			}
+		}
+
+		public string Ip
+		{
+			get
+			{
+				return ip;
+			}
+			set	
+			{
+				ip = value;
+				DictionaryUtil.Add(QueryParameters, "Ip", value);
+			}
+		}
+
+		public string UserAgent
+		{
+			get
+			{
+				return userAgent;
+			}
+			set	
+			{
+				userAgent = value;
+				DictionaryUtil.Add(QueryParameters, "UserAgent", value);
+			}
+		}
+
+		public int? ConnectionType
+		{
+			get
+			{
+				return connectionType;
+			}
+			set	
+			{
+				connectionType = value;
+				DictionaryUtil.Add(QueryParameters, "ConnectionType", value.ToString());
+			}
+		}
+
+		public int? SensType
+		{
+			get
+			{
+				return sensType;
+			}
+			set	
+			{
+				sensType = value;
+				DictionaryUtil.Add(QueryParameters, "SensType", value.ToString());
+			}
+		}
+
+		public int? DeviceType
+		{
+			get
+			{
+				return deviceType;
+			}
+			set	
+			{
+				deviceType = value;
+				DictionaryUtil.Add(QueryParameters, "DeviceType", value.ToString());
+			}
+		}
+
+		public int? BusinessType
+		{
+			get
+			{
+				return businessType;
+			}
+			set	
+			{
+				businessType = value;
+				DictionaryUtil.Add(QueryParameters, "BusinessType", value.ToString());
+			}
+		}
+
+        public override GetIpProfileResponse GetResponse(Core.Transform.UnmarshallerContext unmarshallerContext)
+        {
+            return GetIpProfileResponseUnmarshaller.Unmarshall(unmarshallerContext);
+        }
+    }
+}
