ed2533c954ea5caf273d118b76f63fa2d15e4363
haowei.yao <haowei.yao@alibaba-inc.com>
2017-12-11 19:55:26 +0800

    由荣旸发起的TESLAMAXCOMPUTE SDK自动发布, BUILD_ID=204, 版本号：1.0.0 发布日志： 1, Add GetClusterInfo interface，Support fetch cluster instance info by cluster name. 2, Add GetProjectInfo interface，Support fetch cluster instance info by project. 3, Add GetQuotaInfo interface，Support fetch cluster instance info by quota ID and cluster name. 4, Add GetUserInfo interface，Support fetch cluster instance info by user.

---
+++ aliyun-net-sdk-teslamaxcompute/TeslaMaxCompute/Model/V20171130/GetUserInfoResponse.cs
@@ -0,0 +1,217 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using Aliyun.Acs.Core;
+using System.Collections.Generic;
+
+namespace Aliyun.Acs.TeslaMaxCompute.Model.V20171130
+{
+	public class GetUserInfoResponse : AcsResponse
+	{
+
+		private int? code;
+
+		private string message;
+
+		private string request_id;
+
+		private GetUserInfo_Data data;
+
+		public int? Code
+		{
+			get
+			{
+				return code;
+			}
+			set	
+			{
+				code = value;
+			}
+		}
+
+		public string Message
+		{
+			get
+			{
+				return message;
+			}
+			set	
+			{
+				message = value;
+			}
+		}
+
+		public string Request_id
+		{
+			get
+			{
+				return request_id;
+			}
+			set	
+			{
+				request_id = value;
+			}
+		}
+
+		public GetUserInfo_Data Data
+		{
+			get
+			{
+				return data;
+			}
+			set	
+			{
+				data = value;
+			}
+		}
+
+		public class GetUserInfo_Data
+		{
+
+			private int? total;
+
+			private List<GetUserInfo_Instance> detail;
+
+			public int? Total
+			{
+				get
+				{
+					return total;
+				}
+				set	
+				{
+					total = value;
+				}
+			}
+
+			public List<GetUserInfo_Instance> Detail
+			{
+				get
+				{
+					return detail;
+				}
+				set	
+				{
+					detail = value;
+				}
+			}
+
+			public class GetUserInfo_Instance
+			{
+
+				private string project;
+
+				private string instanceid;
+
+				private string status;
+
+				private string userAccount;
+
+				private string chName;
+
+				private string cluster;
+
+				private string runTime;
+
+				public string Project
+				{
+					get
+					{
+						return project;
+					}
+					set	
+					{
+						project = value;
+					}
+				}
+
+				public string Instanceid
+				{
+					get
+					{
+						return instanceid;
+					}
+					set	
+					{
+						instanceid = value;
+					}
+				}
+
+				public string Status
+				{
+					get
+					{
+						return status;
+					}
+					set	
+					{
+						status = value;
+					}
+				}
+
+				public string UserAccount
+				{
+					get
+					{
+						return userAccount;
+					}
+					set	
+					{
+						userAccount = value;
+					}
+				}
+
+				public string ChName
+				{
+					get
+					{
+						return chName;
+					}
+					set	
+					{
+						chName = value;
+					}
+				}
+
+				public string Cluster
+				{
+					get
+					{
+						return cluster;
+					}
+					set	
+					{
+						cluster = value;
+					}
+				}
+
+				public string RunTime
+				{
+					get
+					{
+						return runTime;
+					}
+					set	
+					{
+						runTime = value;
+					}
+				}
+			}
+		}
+	}
+}
---
+++ aliyun-net-sdk-teslamaxcompute/TeslaMaxCompute/Transform/V20171130/GetUserInfoResponseUnmarshaller.cs
@@ -0,0 +1,59 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using Aliyun.Acs.Core.Transform;
+using Aliyun.Acs.TeslaMaxCompute.Model.V20171130;
+using System;
+using System.Collections.Generic;
+
+namespace Aliyun.Acs.TeslaMaxCompute.Transform.V20171130
+{
+    public class GetUserInfoResponseUnmarshaller
+    {
+        public static GetUserInfoResponse Unmarshall(UnmarshallerContext context)
+        {
+			GetUserInfoResponse getUserInfoResponse = new GetUserInfoResponse();
+
+			getUserInfoResponse.HttpResponse = context.HttpResponse;
+			getUserInfoResponse.Code = context.IntegerValue("GetUserInfo.Code");
+			getUserInfoResponse.Message = context.StringValue("GetUserInfo.Message");
+			getUserInfoResponse.Request_id = context.StringValue("GetUserInfo.Request_id");
+
+			GetUserInfoResponse.GetUserInfo_Data data = new GetUserInfoResponse.GetUserInfo_Data();
+			data.Total = context.IntegerValue("GetUserInfo.Data.Total");
+
+			List<GetUserInfoResponse.GetUserInfo_Data.GetUserInfo_Instance> data_detail = new List<GetUserInfoResponse.GetUserInfo_Data.GetUserInfo_Instance>();
+			for (int i = 0; i < context.Length("GetUserInfo.Data.Detail.Length"); i++) {
+				GetUserInfoResponse.GetUserInfo_Data.GetUserInfo_Instance instance = new GetUserInfoResponse.GetUserInfo_Data.GetUserInfo_Instance();
+				instance.Project = context.StringValue("GetUserInfo.Data.Detail["+ i +"].Project");
+				instance.Instanceid = context.StringValue("GetUserInfo.Data.Detail["+ i +"].Instanceid");
+				instance.Status = context.StringValue("GetUserInfo.Data.Detail["+ i +"].Status");
+				instance.UserAccount = context.StringValue("GetUserInfo.Data.Detail["+ i +"].UserAccount");
+				instance.ChName = context.StringValue("GetUserInfo.Data.Detail["+ i +"].ChName");
+				instance.Cluster = context.StringValue("GetUserInfo.Data.Detail["+ i +"].Cluster");
+				instance.RunTime = context.StringValue("GetUserInfo.Data.Detail["+ i +"].RunTime");
+
+				data_detail.Add(instance);
+			}
+			data.Detail = data_detail;
+			getUserInfoResponse.Data = data;
+        
+			return getUserInfoResponse;
+        }
+    }
+}
