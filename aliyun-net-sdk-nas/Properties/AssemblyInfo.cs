5d1d40192e87d972454136feb9bd2feb5c88e549
haowei.yao <haowei.yao@alibaba-inc.com>
2017-12-15 15:04:01 +0800

    由执少发起的NAS SDK自动发布, BUILD_ID=271, 版本号：3.1.1 发布日志： 1, This release is for Aliyun Location service. 2, User no longer needs to addEndpoint in the initialization code.

--- aliyun-net-sdk-nas/Properties/AssemblyInfo.cs
+++ aliyun-net-sdk-nas/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("5d176283-05be-4452-b0e6-102dc2f1cb8a")]
+[assembly: Guid("878a933e-b8dd-4555-9dbd-b132699eed76")]
 
 // 程序集的版本信息由下面四个值组成:
 //
