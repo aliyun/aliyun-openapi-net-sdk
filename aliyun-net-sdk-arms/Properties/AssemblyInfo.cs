ffcc4b5cedee77260bb4ef8602887973947bb0eb
haowei.yao <haowei.yao@alibaba-inc.com>
2017-12-20 20:29:40 +0800

    由左羽发起的ARMS SDK自动发布, BUILD_ID=289, 版本号：2.0.1 发布日志： 1, support tile dataset 2, use dimension value type to replace isDrillDown

--- aliyun-net-sdk-arms/Properties/AssemblyInfo.cs
+++ aliyun-net-sdk-arms/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("b66dfa86-0289-49c3-9753-bba78015b848")]
+[assembly: Guid("2d81e3ca-63c7-439e-930f-3d3a6fcab4a3")]
 
 // 程序集的版本信息由下面四个值组成:
 //
