d7f3f4d3a82cdeac6f885d70ef3122df7732fcbd
haowei.yao <haowei.yao@alibaba-inc.com>
2018-02-09 15:08:34 +0800

    由谦正发起的EHPC SDK自动发布, BUILD_ID=409, 版本号：1.0.0 发布日志： 1, Aliyun E-HPC service SDK, initial version. 2, Include APIs of E-HPC clusters, nodes, users, jobs, job templates etc.

---
+++ aliyun-net-sdk-ehpc/EHPC/Model/V20170714/AddNodesRequest.cs
@@ -0,0 +1,161 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using Aliyun.Acs.Core;
+using Aliyun.Acs.Core.Http;
+using Aliyun.Acs.Core.Transform;
+using Aliyun.Acs.Core.Utils;
+using Aliyun.Acs.EHPC.Transform;
+using Aliyun.Acs.EHPC.Transform.V20170714;
+using System.Collections.Generic;
+
+namespace Aliyun.Acs.EHPC.Model.V20170714
+{
+    public class AddNodesRequest : RpcAcsRequest<AddNodesResponse>
+    {
+        public AddNodesRequest()
+            : base("EHPC", "2017-07-14", "AddNodes", "ehs", "openAPI")
+        {
+        }
+
+		private string imageId;
+
+		private int? count;
+
+		private string action;
+
+		private string clusterId;
+
+		private string computeSpotStrategy;
+
+		private string computeSpotPriceLimit;
+
+		private string accessKeyId;
+
+		private string imageOwnerAlias;
+
+		public string ImageId
+		{
+			get
+			{
+				return imageId;
+			}
+			set	
+			{
+				imageId = value;
+				DictionaryUtil.Add(QueryParameters, "ImageId", value);
+			}
+		}
+
+		public int? Count
+		{
+			get
+			{
+				return count;
+			}
+			set	
+			{
+				count = value;
+				DictionaryUtil.Add(QueryParameters, "Count", value.ToString());
+			}
+		}
+
+		public string Action
+		{
+			get
+			{
+				return action;
+			}
+			set	
+			{
+				action = value;
+				DictionaryUtil.Add(QueryParameters, "Action", value);
+			}
+		}
+
+		public string ClusterId
+		{
+			get
+			{
+				return clusterId;
+			}
+			set	
+			{
+				clusterId = value;
+				DictionaryUtil.Add(QueryParameters, "ClusterId", value);
+			}
+		}
+
+		public string ComputeSpotStrategy
+		{
+			get
+			{
+				return computeSpotStrategy;
+			}
+			set	
+			{
+				computeSpotStrategy = value;
+				DictionaryUtil.Add(QueryParameters, "ComputeSpotStrategy", value);
+			}
+		}
+
+		public string ComputeSpotPriceLimit
+		{
+			get
+			{
+				return computeSpotPriceLimit;
+			}
+			set	
+			{
+				computeSpotPriceLimit = value;
+				DictionaryUtil.Add(QueryParameters, "ComputeSpotPriceLimit", value);
+			}
+		}
+
+		public string AccessKeyId
+		{
+			get
+			{
+				return accessKeyId;
+			}
+			set	
+			{
+				accessKeyId = value;
+				DictionaryUtil.Add(QueryParameters, "AccessKeyId", value);
+			}
+		}
+
+		public string ImageOwnerAlias
+		{
+			get
+			{
+				return imageOwnerAlias;
+			}
+			set	
+			{
+				imageOwnerAlias = value;
+				DictionaryUtil.Add(QueryParameters, "ImageOwnerAlias", value);
+			}
+		}
+
+        public override AddNodesResponse GetResponse(Core.Transform.UnmarshallerContext unmarshallerContext)
+        {
+            return AddNodesResponseUnmarshaller.Unmarshall(unmarshallerContext);
+        }
+    }
+}
