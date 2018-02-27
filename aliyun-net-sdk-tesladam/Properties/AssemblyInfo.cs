489d2d4285fe35a71ffeb070eafd375147d014a5
haowei.yao <haowei.yao@alibaba-inc.com>
2018-02-27 12:05:29 +0800

    由荣旸发起的TESLADAM SDK自动发布, BUILD_ID=310, 版本号：1.0.1 发布日志： 1, Add Action API.

--- aliyun-net-sdk-tesladam/Properties/AssemblyInfo.cs
+++ aliyun-net-sdk-tesladam/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("43c0938e-7488-45ea-b4a7-ca67fdabf4f0")]
+[assembly: Guid("99b10cb5-50c9-46f6-9300-42d077e85abb")]
 
 // 程序集的版本信息由下面四个值组成:
 //
