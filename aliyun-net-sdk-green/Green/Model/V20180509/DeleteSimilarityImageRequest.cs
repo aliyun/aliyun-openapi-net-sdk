cfe9aea62f88bf35cdb045d6f7ad4851ede01526
haowei.yao <haowei.yao@alibaba-inc.com>
2018-05-28 19:20:36 +0800

    GREEN SDK Auto Released By Alibaba Cloud,Version：3.2.0 发布日志： 1, Add face 1-N,1-1 scan interface.

---
+++ aliyun-net-sdk-green/Green/Model/V20180509/DeleteSimilarityImageRequest.cs
@@ -0,0 +1,58 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using Aliyun.Acs.Core;
+using Aliyun.Acs.Core.Http;
+using Aliyun.Acs.Core.Transform;
+using Aliyun.Acs.Core.Utils;
+using Aliyun.Acs.Green.Transform;
+using Aliyun.Acs.Green.Transform.V20180509;
+using System.Collections.Generic;
+
+namespace Aliyun.Acs.Green.Model.V20180509
+{
+    public class DeleteSimilarityImageRequest : RoaAcsRequest<DeleteSimilarityImageResponse>
+    {
+        public DeleteSimilarityImageRequest()
+            : base("Green", "2018-05-09", "DeleteSimilarityImage", "green", "openAPI")
+        {
+			UriPattern = "/green/similarity/image/delete";
+			Method = MethodType.POST;
+        }
+
+		private string clientInfo;
+
+		public string ClientInfo
+		{
+			get
+			{
+				return clientInfo;
+			}
+			set	
+			{
+				clientInfo = value;
+				DictionaryUtil.Add(QueryParameters, "ClientInfo", value);
+			}
+		}
+
+        public override DeleteSimilarityImageResponse GetResponse(Core.Transform.UnmarshallerContext unmarshallerContext)
+        {
+            return DeleteSimilarityImageResponseUnmarshaller.Unmarshall(unmarshallerContext);
+        }
+    }
+}
