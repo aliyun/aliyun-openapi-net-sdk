6b7f4384ab79760e65b5e1e60657aaf7bc4fe038
HanXu <wb-hx510875@alibaba-inc.com>
2019-01-17 10:26:46 +0800

    AssumeRoleRequest Unit Test

---
+++ aliyun-net-sdk-core.Tests/Auth/Sts/AssumeRoleRequest.cs
@@ -0,0 +1,17 @@
+using System.Text;
+using System.Web;
+using Xunit;
+using Aliyun.Acs.Core.Auth.Sts;
+
+namespace Aliyun.Acs.Core.UnitTests.Auth.Sts
+{
+    public class AssumeRoleRequest_Test
+    {
+        [Fact]
+        public void AssumeRoleRequest()
+        {
+            AssumeRoleRequest assumeRoleRequest = new AssumeRoleRequest();
+            string policy = assumeRoleRequest.Policy;
+        }
+    }
+}
