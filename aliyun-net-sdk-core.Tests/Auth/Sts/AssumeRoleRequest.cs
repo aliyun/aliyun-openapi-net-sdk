a1526e741beba07f103e279082554fa5364209e3
HanXu <wb-hx510875@alibaba-inc.com>
2019-01-21 21:39:46 +0800

    Aliyun.Acs.Core.Auth.Sts.AssumeRoleRequest UnitTests
    fotmat change

--- aliyun-net-sdk-core.Tests/Auth/Sts/AssumeRoleRequest.cs
+++ aliyun-net-sdk-core.Tests/Auth/Sts/AssumeRoleRequest.cs
@@ -1,19 +1,64 @@
 using System.Text;
 using System.Web;
-using Xunit;
 using Aliyun.Acs.Core.Auth.Sts;
+using Xunit;
+using Aliyun.Acs.Core.Transform;
+using System.Collections.Generic;
 
 namespace Aliyun.Acs.Core.UnitTests.Auth.Sts
 {
-    public class AssumeRoleRequest_Test
+    public class AssumeRoleRequestTest
     {
         [Fact]
-        public void AssumeRoleRequest()
+        public void PolicyTest()
         {
             AssumeRoleRequest assumeRoleRequest = new AssumeRoleRequest();
             string source = "foo";
             assumeRoleRequest.Policy = source;
             Assert.Equal(source, assumeRoleRequest.Policy);
+        }
+
+        [Fact]
+        public void DurationSecondsTest()
+        {
+            AssumeRoleRequest assumeRoleRequest = new AssumeRoleRequest();
+            long source = 10000000000;
+            assumeRoleRequest.DurationSeconds = source;
+            Assert.Equal(source, assumeRoleRequest.DurationSeconds);
+        }
+
+        [Fact]
+        public void RoleArnTest()
+        {
+            AssumeRoleRequest assumeRoleRequest = new AssumeRoleRequest();
+            string source = "foo";
+            assumeRoleRequest.RoleArn = source;
+            Assert.Equal(source, assumeRoleRequest.RoleArn);
+        }
+
+        [Fact]
+        public void RoleSessionNameTest()
+        {
+            AssumeRoleRequest assumeRoleRequest = new AssumeRoleRequest();
+            string source = "foo";
+            assumeRoleRequest.RoleSessionName = source;
+            Assert.Equal(source, assumeRoleRequest.RoleSessionName);
+        }
+
+        [Fact]
+        public void GetResponseTest()
+        {
+            AssumeRoleRequest assumeRoleRequest = new AssumeRoleRequest();
+            UnmarshallerContext unmarshallerContext = new UnmarshallerContext();
+            if (unmarshallerContext.ResponseDictionary == null)
+            {
+                unmarshallerContext.ResponseDictionary = new Dictionary<string, string> {
+                    { "foo", "bar" },
+                    { "a", "A" },
+                    { "b", "B" }
+                };
+            }
+            assumeRoleRequest.GetResponse(unmarshallerContext);
         }
     }
 }
