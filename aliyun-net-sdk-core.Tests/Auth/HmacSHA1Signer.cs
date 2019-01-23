2079f38963b531aba7fa23745b59ad58134f59c3
Axios <369017150@qq.com>
2019-01-23 20:31:32 +0800

    与数字签名生成有关的ut (#72)

---
+++ aliyun-net-sdk-core.Tests/Auth/HmacSHA1Signer.cs
@@ -0,0 +1,35 @@
+using System;
+
+using Aliyun.Acs.Core.Auth;
+
+using Xunit;
+
+namespace Aliyun.Acs.Core.UnitTests.Auth
+{
+    public class HmacSHA1SignerTest
+    {
+
+        [Fact]
+        public void SignString()
+        {
+            HmacSHA1Signer instance = new HmacSHA1Signer();
+            string sign = instance.SignString("foo", "secret");
+            Assert.Equal("m67ZG+f1i1fIJLYNp8smKy7K+9I=", sign);
+        }
+
+        [Fact]
+        public void GetSignerName()
+        {
+            HmacSHA1Signer instance = new HmacSHA1Signer();
+            string signer = instance.GetSignerName();
+            Assert.Equal("HMAC-SHA1", signer);
+
+            string signerVersion = instance.GetSignerVersion();
+            Assert.Equal("1.0", signerVersion);
+
+            // 此方法只回调了一个 null 值
+            string signerType = instance.GetSignerType();
+            Assert.Null(signerType);
+        }
+    }
+}
---
+++ aliyun-net-sdk-core.Tests/Auth/ShaHmac256.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Security.Cryptography;
+
+using Aliyun.Acs.Core.Auth;
+
+using Xunit;
+
+namespace Aliyun.Acs.Core.UnitTests.Auth
+{
+    public class ShaHmac256Test
+    {
+        private ShaHmac256 instance;
+
+        public ShaHmac256 getInstance()
+        {
+            if (null == instance)
+            {
+                this.instance = new ShaHmac256();
+            }
+            return this.instance;
+        }
+
+        [Fact]
+        public void SignString()
+        {
+            string str = this.getInstance().SignString("foo", "secret");
+            Assert.Equal("dzukRpPHVT1u4g9h6l0nV6mk9KRNKEGuTpW1LkzWLbQ=", str);
+        }
+
+        [Fact]
+        public void GetSignerName()
+        {
+            string signer = this.getInstance().SignerName;
+            Assert.Equal("HMAC-SHA256", signer);
+        }
+
+        [Fact]
+        public void GetSignerVersion()
+        {
+            string signerVersion = this.getInstance().SignerVersion;
+            Assert.Equal("1.0", signerVersion);
+        }
+    }
+}
