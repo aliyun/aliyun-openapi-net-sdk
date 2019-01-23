da0452c6eec50a6467404a1e9f3421adf7097d25
Axios <369017150@qq.com>
2019-01-23 18:00:29 +0800

    Auth UT (#71)

---
+++ aliyun-net-sdk-core.Tests/Auth/ECSMetadataServiceCredentialsFetcher.cs
@@ -0,0 +1,51 @@
+using System;
+
+using Aliyun.Acs.Core.Auth;
+using Aliyun.Acs.Core.Profile;
+
+using Xunit;
+
+namespace Aliyun.Acs.Core.UnitTests.Auth
+{
+    public class ECSMetadataServiceCredentialsFetcherTest
+    {
+        private ECSMetadataServiceCredentialsFetcher instance;
+
+        [Fact]
+        public ECSMetadataServiceCredentialsFetcher getInstance()
+        {
+            if (null == this.instance)
+            {
+                this.instance = new ECSMetadataServiceCredentialsFetcher();
+            }
+            return this.instance;
+        }
+
+        [Fact]
+        public void SetRoleName()
+        {
+            Assert.Throws<ArgumentNullException>(
+                () => { this.getInstance().SetRoleName(""); }
+            );
+
+            this.getInstance().SetRoleName("roleName");
+            // DONE WITH NO EXCEPTION
+        }
+
+        [Fact]
+        public void WithECSMetadataServiceHost()
+        {
+            this.getInstance().WithECSMetadataServiceHost("host");
+
+            // DONE WITH NO EXCEPTION
+        }
+
+        [Fact]
+        public void WithConnectionTimeout()
+        {
+            this.getInstance().WithConnectionTimeout(100);
+
+            // DONE WITH NO EXCEPTION
+        }
+    }
+}
