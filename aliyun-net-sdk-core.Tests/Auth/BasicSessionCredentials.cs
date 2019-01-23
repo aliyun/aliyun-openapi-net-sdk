e6e1b226cd6c10d977427be397dc6531de4d8024
HanXu <wb-hx510875@alibaba-inc.com>
2019-01-23 16:45:11 +0800

    Auth UT

---
+++ aliyun-net-sdk-core.Tests/Auth/BasicSessionCredentials.cs
@@ -0,0 +1,60 @@
+using System;
+
+using Aliyun.Acs.Core.Auth;
+
+using Xunit;
+
+namespace Aliyun.Acs.Core.UnitTests.Auth
+{
+    public class BasicSessionCredentialsTest
+    {
+        [Fact]
+        public BasicSessionCredentials getInstance()
+        {
+            BasicSessionCredentials instance;
+            try
+            {
+                instance = new BasicSessionCredentials(null, null, "", 0);
+            }
+            catch (ArgumentOutOfRangeException) { }
+
+            try
+            {
+                instance = new BasicSessionCredentials("accessKeyId", null, "", 0);
+            }
+            catch (ArgumentOutOfRangeException) { }
+
+            instance = new BasicSessionCredentials("accessKeyId", "accessKeySecret", "sessionToken", 1);
+            return instance;
+        }
+
+        [Fact]
+        public void GetAccessKeyId()
+        {
+            Assert.Equal("accessKeyId", this.getInstance().GetAccessKeyId());
+        }
+
+        [Fact]
+        public void GetAccessKeySecret()
+        {
+            Assert.Equal("accessKeySecret", this.getInstance().GetAccessKeySecret());
+        }
+
+        [Fact]
+        public void GetSessionToken()
+        {
+            Assert.Equal("sessionToken", this.getInstance().GetSessionToken());
+        }
+
+        [Fact]
+        public void WillSoonExpire()
+        {
+            BasicSessionCredentials instance;
+            instance = new BasicSessionCredentials("accessKeyId", "accessKeySecret", "sessionToken", 1);
+            instance.WillSoonExpire();
+            instance = new BasicSessionCredentials("accessKeyId", "accessKeySecret", "sessionToken", 0);
+            instance.WillSoonExpire();
+            Assert.False(instance.WillSoonExpire());
+        }
+    }
+}
