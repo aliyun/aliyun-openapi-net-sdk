198fd7e20976194f8c3f1439f1dfb42f49cf2786
HanXu <wb-hx510875@alibaba-inc.com>
2019-01-24 11:17:16 +0800

    Utils UT

---
+++ aliyun-net-sdk-core.Tests/Utils/CacheTimeHelper.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core.Transform;
+using Aliyun.Acs.Core.Utils;
+
+using Xunit;
+
+namespace Aliyun.Acs.Core.UnitTests.Utils
+{
+    public class CacheTimeHelperTest
+    {
+        [Fact]
+        public void CheckCacheIsExpire()
+        {
+            // When Not Exist
+            Assert.False(CacheTimeHelper.CheckCacheIsExpire("someNotExist", "ch-hangzhou"));
+
+            // When Exist
+            DateTime lastClearTime = DateTime.Now.AddDays(-1);
+            CacheTimeHelper.AddLastClearTimePerProduct("ecs", "ch-hangzhou", lastClearTime);
+            Assert.True(CacheTimeHelper.CheckCacheIsExpire("ecs", "ch-hangzhou"));
+        }
+
+        [Fact]
+        public void AddLastClearTimePerProduct()
+        {
+            DateTime lastClearTime = DateTime.Now.AddDays(-1);
+            CacheTimeHelper.AddLastClearTimePerProduct("ecs", "ch-hangzhou", lastClearTime);
+            Assert.True(CacheTimeHelper.CheckCacheIsExpire("ecs", "ch-hangzhou"));
+
+            lastClearTime = DateTime.Now.AddDays(1);
+            CacheTimeHelper.AddLastClearTimePerProduct("ecs", "ch-hangzhou", lastClearTime);
+            Assert.False(CacheTimeHelper.CheckCacheIsExpire("ecs", "ch-hangzhou"));
+        }
+    }
+}
---
+++ aliyun-net-sdk-core.Tests/Utils/DateTimeExtensions.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core.Transform;
+using Aliyun.Acs.Core.Utils;
+
+using Xunit;
+
+namespace Aliyun.Acs.Core.UnitTests.Utils
+{
+    public class DateTimeExtensionsTest
+    {
+        [Fact]
+        public void currentTimeMillis()
+        {
+            DateTime d = DateTime.Now;
+            DateTimeExtensions.currentTimeMillis(d);
+        }
+    }
+}
