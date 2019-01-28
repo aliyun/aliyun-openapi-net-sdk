e86d829717939d288795e30321019f792d73a6e1
axios <axioscros@aliyun.com>
2019-01-28 21:23:57 +0800

    GetSessionAccessKeyRequest UT

---
+++ aliyun-net-sdk-core.Tests/Auth/Sts/GetSessionAccessKeyRequest.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+using Aliyun.Acs.Core.Auth.Sts;
+using Aliyun.Acs.Core.Transform;
+
+using Xunit;
+
+namespace Aliyun.Acs.Core.UnitTests.Auth.Sts
+{
+    public class GetSessionAccessKeyRequestTest
+    {
+        [Fact]
+        public void DurationSecondsTest()
+        {
+            GetSessionAccessKeyRequest instance = new GetSessionAccessKeyRequest();
+            int source = 100;
+            instance.DurationSeconds = source;
+            Assert.Equal(source, instance.DurationSeconds);
+        }
+
+        [Fact]
+        public void PublicKeyIdTest()
+        {
+            GetSessionAccessKeyRequest instance = new GetSessionAccessKeyRequest();
+            string source = "publicKeyId";
+            instance.PublicKeyId = source;
+            Assert.Equal(source, instance.PublicKeyId);
+        }
+
+        [Fact]
+        public void GetResponse()
+        {
+            GetSessionAccessKeyRequest instance = new GetSessionAccessKeyRequest();
+            UnmarshallerContext unmarshallerContext = new UnmarshallerContext();
+            if (unmarshallerContext.ResponseDictionary == null)
+            {
+                unmarshallerContext.ResponseDictionary = new Dictionary<string, string>
+                { { "foo", "bar" },
+                { "a", "A" },
+                { "b", "B" }
+                };
+            }
+            instance.GetResponse(unmarshallerContext);
+        }
+    }
+}
