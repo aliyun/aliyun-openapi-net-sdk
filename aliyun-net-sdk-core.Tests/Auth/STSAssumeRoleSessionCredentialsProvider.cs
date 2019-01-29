a961f1416e539570c8903b08d4a8230ab48a07ef
HanXu <wb-hx510875@alibaba-inc.com>
2019-01-29 11:24:12 +0800

    STSAssumeRoleSessionCredentialsProvider UT

--- aliyun-net-sdk-core.Tests/Auth/STSAssumeRoleSessionCredentialsProvider.cs
+++ aliyun-net-sdk-core.Tests/Auth/STSAssumeRoleSessionCredentialsProvider.cs
@@ -14,6 +14,7 @@ namespace Aliyun.Acs.Core.UnitTests.Auth
         [Fact]
         public void Instance()
         {
+            DefaultProfile.ClearDefaultProfile();
             var mock = new Mock<AlibabaCloudCredentials>();
             AlibabaCloudCredentials longLivedCredentials = mock.Object;
             string roleArn = "roleArn";
@@ -27,6 +28,7 @@ namespace Aliyun.Acs.Core.UnitTests.Auth
         [Fact]
         public void InstanceProvider()
         {
+            DefaultProfile.ClearDefaultProfile();
             var mock = new Mock<AlibabaCloudCredentialsProvider>();
             AlibabaCloudCredentialsProvider longLivedCredentialsProvider = mock.Object;
             string roleArn = "roleArn";
@@ -40,6 +42,7 @@ namespace Aliyun.Acs.Core.UnitTests.Auth
         [Fact]
         public void WithRoleSessionName()
         {
+            DefaultProfile.ClearDefaultProfile();
             var mock = new Mock<AlibabaCloudCredentials>();
             AlibabaCloudCredentials longLivedCredentials = mock.Object;
             string roleArn = "roleArn";
@@ -54,6 +57,7 @@ namespace Aliyun.Acs.Core.UnitTests.Auth
         [Fact]
         public void WithRoleSessionDurationSecondss()
         {
+            DefaultProfile.ClearDefaultProfile();
             var mock = new Mock<AlibabaCloudCredentials>();
             AlibabaCloudCredentials longLivedCredentials = mock.Object;
             string roleArn = "roleArn";
@@ -76,6 +80,7 @@ namespace Aliyun.Acs.Core.UnitTests.Auth
         [Fact]
         public void WithSTSClient()
         {
+            DefaultProfile.ClearDefaultProfile();
             var mock = new Mock<AlibabaCloudCredentials>();
             AlibabaCloudCredentials longLivedCredentials = mock.Object;
             string roleArn = "roleArn";
@@ -87,6 +92,41 @@ namespace Aliyun.Acs.Core.UnitTests.Auth
             instance.WithSTSClient(client); // 方法回调 STSAssumeRoleSessionCredentialsProvider 实例
 
             Assert.IsType<STSAssumeRoleSessionCredentialsProvider>(instance);
+        }
+
+        [Fact]
+        public void GetCredentials()
+        {
+            DefaultProfile.ClearDefaultProfile();
+            string accessKeyId = Environment.GetEnvironmentVariable("AKID");
+            string accessKeySecret = Environment.GetEnvironmentVariable("AKSE");
+            var mock = new Mock<AlibabaCloudCredentials>();
+            mock.Setup(foo => foo.GetAccessKeyId()).Returns(accessKeyId);
+            mock.Setup(foo => foo.GetAccessKeySecret()).Returns(accessKeySecret);
+
+            AlibabaCloudCredentials longLivedCredentials = mock.Object;
+            string roleArn = Environment.GetEnvironmentVariable("RAM");;
+            IClientProfile profile = DefaultProfile.GetProfile(
+                "cn-hangzhou",
+                accessKeyId,
+                accessKeySecret
+            );
+            // Console.Write(longLivedCredentials.GetAccessKeyId());
+            STSAssumeRoleSessionCredentialsProvider instance = new STSAssumeRoleSessionCredentialsProvider(longLivedCredentials, roleArn, profile);
+
+            var credentials = instance.GetCredentials(); // 执行credential初始化
+            Assert.IsType<BasicSessionCredentials>(credentials);
+
+            var credentials2 = instance.GetCredentials(); // 不执行credential初始化，直接获取
+            Assert.IsType<BasicSessionCredentials>(credentials);
+            Assert.Equal(credentials.GetAccessKeyId(), credentials2.GetAccessKeyId());
+            Assert.Equal(credentials.GetAccessKeySecret(), credentials2.GetAccessKeySecret());
+        }
+
+        [Fact]
+        public void GetNewSessionCredentials()
+        {
+            DefaultProfile.ClearDefaultProfile();
         }
     }
 }
