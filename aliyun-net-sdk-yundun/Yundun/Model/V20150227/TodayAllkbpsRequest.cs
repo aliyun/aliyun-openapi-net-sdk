ac483f397f95cd790a8bf3e5654e54bbe353a9e1
haowei.yao <haowei.yao@alibaba-inc.com>
2019-03-15 17:46:34 +0800

    YUNDUN SDK Auto Released By shenshi,Version：2.1.4 发布日志： 1,  Update Dependency

---
+++ aliyun-net-sdk-yundun/Yundun/Model/V20150227/TodayAllkbpsRequest.cs
@@ -0,0 +1,42 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core;
+using Aliyun.Acs.Core.Http;
+using Aliyun.Acs.Core.Transform;
+using Aliyun.Acs.Core.Utils;
+using Aliyun.Acs.Yundun.Transform;
+using Aliyun.Acs.Yundun.Transform.V20150227;
+
+namespace Aliyun.Acs.Yundun.Model.V20150227
+{
+    public class TodayAllkbpsRequest : RpcAcsRequest<TodayAllkbpsResponse>
+    {
+        public TodayAllkbpsRequest()
+            : base("Yundun", "2015-02-27", "TodayAllkbps", "yundun", "openAPI")
+        {
+        }
+
+        public override TodayAllkbpsResponse GetResponse(UnmarshallerContext unmarshallerContext)
+        {
+            return TodayAllkbpsResponseUnmarshaller.Unmarshall(unmarshallerContext);
+        }
+    }
+}
