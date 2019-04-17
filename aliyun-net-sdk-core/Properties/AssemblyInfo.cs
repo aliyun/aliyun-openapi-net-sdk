c953b07bfbf3e381683bf39202127ee421f0f082
doublnt <yinximail@gmail.com>
2019-04-17 14:51:46 +0800

    Support framework version from net45 to netstandard20
    * Support framework version from net45 to netstandard20
    
    * Update CI file config
    
    * Fix reference error

---
+++ aliyun-net-sdk-core/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+#if NETSTANDARD2_0
+#else
+[assembly: AssemblyTitle("aliyun-net-sdk-core")]
+[assembly: AssemblyDescription("aliyun net sdk core")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Alibaba Cloud")]
+[assembly: AssemblyProduct("aliyun net core")]
+[assembly: AssemblyCopyright("©2009-2019 Alibaba Cloud")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cf0a14b9-7544-47c7-93b0-919fd591df41")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.3.1.0")]
+[assembly: AssemblyFileVersion("1.3.1.0")]
+#endif
