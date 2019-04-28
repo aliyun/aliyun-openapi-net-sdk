3b679ee1252d50218e72b50d90816cdced3a784e
haowei.yao <haowei.yao@alibaba-inc.com>
2019-04-28 19:51:18 +0800

    R-KVSTORE SDK Auto Released By xiaoyan.yan,Version：2.0.8 发布日志： 1, Publish DescribeAvailableResource.

---
+++ aliyun-net-sdk-r-kvstore/R_kvstore/Model/V20150101/DescribeAvailableResourceResponse.cs
@@ -0,0 +1,372 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core;
+
+namespace Aliyun.Acs.R_kvstore.Model.V20150101
+{
+	public class DescribeAvailableResourceResponse : AcsResponse
+	{
+
+		private string requestId;
+
+		private List<DescribeAvailableResource_AvailableZone> availableZones;
+
+		public string RequestId
+		{
+			get
+			{
+				return requestId;
+			}
+			set	
+			{
+				requestId = value;
+			}
+		}
+
+		public List<DescribeAvailableResource_AvailableZone> AvailableZones
+		{
+			get
+			{
+				return availableZones;
+			}
+			set	
+			{
+				availableZones = value;
+			}
+		}
+
+		public class DescribeAvailableResource_AvailableZone
+		{
+
+			private string regionId;
+
+			private string zoneId;
+
+			private string status;
+
+			private string networkTypes;
+
+			private List<DescribeAvailableResource_SupportedEngine> supportedEngines;
+
+			public string RegionId
+			{
+				get
+				{
+					return regionId;
+				}
+				set	
+				{
+					regionId = value;
+				}
+			}
+
+			public string ZoneId
+			{
+				get
+				{
+					return zoneId;
+				}
+				set	
+				{
+					zoneId = value;
+				}
+			}
+
+			public string Status
+			{
+				get
+				{
+					return status;
+				}
+				set	
+				{
+					status = value;
+				}
+			}
+
+			public string NetworkTypes
+			{
+				get
+				{
+					return networkTypes;
+				}
+				set	
+				{
+					networkTypes = value;
+				}
+			}
+
+			public List<DescribeAvailableResource_SupportedEngine> SupportedEngines
+			{
+				get
+				{
+					return supportedEngines;
+				}
+				set	
+				{
+					supportedEngines = value;
+				}
+			}
+
+			public class DescribeAvailableResource_SupportedEngine
+			{
+
+				private string engine;
+
+				private List<DescribeAvailableResource_SupportedEngineVersion> supportedEngineVersions;
+
+				public string Engine
+				{
+					get
+					{
+						return engine;
+					}
+					set	
+					{
+						engine = value;
+					}
+				}
+
+				public List<DescribeAvailableResource_SupportedEngineVersion> SupportedEngineVersions
+				{
+					get
+					{
+						return supportedEngineVersions;
+					}
+					set	
+					{
+						supportedEngineVersions = value;
+					}
+				}
+
+				public class DescribeAvailableResource_SupportedEngineVersion
+				{
+
+					private string version;
+
+					private List<DescribeAvailableResource_SupportedArchitectureType> supportedArchitectureTypes;
+
+					public string Version
+					{
+						get
+						{
+							return version;
+						}
+						set	
+						{
+							version = value;
+						}
+					}
+
+					public List<DescribeAvailableResource_SupportedArchitectureType> SupportedArchitectureTypes
+					{
+						get
+						{
+							return supportedArchitectureTypes;
+						}
+						set	
+						{
+							supportedArchitectureTypes = value;
+						}
+					}
+
+					public class DescribeAvailableResource_SupportedArchitectureType
+					{
+
+						private string architecture;
+
+						private List<DescribeAvailableResource_SupportedPerformanceType> supportedPerformanceTypes;
+
+						public string Architecture
+						{
+							get
+							{
+								return architecture;
+							}
+							set	
+							{
+								architecture = value;
+							}
+						}
+
+						public List<DescribeAvailableResource_SupportedPerformanceType> SupportedPerformanceTypes
+						{
+							get
+							{
+								return supportedPerformanceTypes;
+							}
+							set	
+							{
+								supportedPerformanceTypes = value;
+							}
+						}
+
+						public class DescribeAvailableResource_SupportedPerformanceType
+						{
+
+							private string performanceType;
+
+							private List<DescribeAvailableResource_SupportedStorageType> supportedStorageTypes;
+
+							public string PerformanceType
+							{
+								get
+								{
+									return performanceType;
+								}
+								set	
+								{
+									performanceType = value;
+								}
+							}
+
+							public List<DescribeAvailableResource_SupportedStorageType> SupportedStorageTypes
+							{
+								get
+								{
+									return supportedStorageTypes;
+								}
+								set	
+								{
+									supportedStorageTypes = value;
+								}
+							}
+
+							public class DescribeAvailableResource_SupportedStorageType
+							{
+
+								private string storageType;
+
+								private List<DescribeAvailableResource_SupportedNodeType> supportedNodeTypes;
+
+								public string StorageType
+								{
+									get
+									{
+										return storageType;
+									}
+									set	
+									{
+										storageType = value;
+									}
+								}
+
+								public List<DescribeAvailableResource_SupportedNodeType> SupportedNodeTypes
+								{
+									get
+									{
+										return supportedNodeTypes;
+									}
+									set	
+									{
+										supportedNodeTypes = value;
+									}
+								}
+
+								public class DescribeAvailableResource_SupportedNodeType
+								{
+
+									private string nodeType;
+
+									private List<DescribeAvailableResource_SupportedPackageType> supportedPackageTypes;
+
+									public string NodeType
+									{
+										get
+										{
+											return nodeType;
+										}
+										set	
+										{
+											nodeType = value;
+										}
+									}
+
+									public List<DescribeAvailableResource_SupportedPackageType> SupportedPackageTypes
+									{
+										get
+										{
+											return supportedPackageTypes;
+										}
+										set	
+										{
+											supportedPackageTypes = value;
+										}
+									}
+
+									public class DescribeAvailableResource_SupportedPackageType
+									{
+
+										private string packageType;
+
+										private List<DescribeAvailableResource_AvailableResource> availableResources;
+
+										public string PackageType
+										{
+											get
+											{
+												return packageType;
+											}
+											set	
+											{
+												packageType = value;
+											}
+										}
+
+										public List<DescribeAvailableResource_AvailableResource> AvailableResources
+										{
+											get
+											{
+												return availableResources;
+											}
+											set	
+											{
+												availableResources = value;
+											}
+										}
+
+										public class DescribeAvailableResource_AvailableResource
+										{
+
+											private string instanceClass;
+
+											public string InstanceClass
+											{
+												get
+												{
+													return instanceClass;
+												}
+												set	
+												{
+													instanceClass = value;
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
---
+++ aliyun-net-sdk-r-kvstore/R_kvstore/Transform/V20150101/DescribeAvailableResourceResponseUnmarshaller.cs
@@ -0,0 +1,123 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System;
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core.Transform;
+using Aliyun.Acs.R_kvstore.Model.V20150101;
+
+namespace Aliyun.Acs.R_kvstore.Transform.V20150101
+{
+    public class DescribeAvailableResourceResponseUnmarshaller
+    {
+        public static DescribeAvailableResourceResponse Unmarshall(UnmarshallerContext context)
+        {
+			DescribeAvailableResourceResponse describeAvailableResourceResponse = new DescribeAvailableResourceResponse();
+
+			describeAvailableResourceResponse.HttpResponse = context.HttpResponse;
+			describeAvailableResourceResponse.RequestId = context.StringValue("DescribeAvailableResource.RequestId");
+
+			List<DescribeAvailableResourceResponse.DescribeAvailableResource_AvailableZone> describeAvailableResourceResponse_availableZones = new List<DescribeAvailableResourceResponse.DescribeAvailableResource_AvailableZone>();
+			for (int i = 0; i < context.Length("DescribeAvailableResource.AvailableZones.Length"); i++) {
+				DescribeAvailableResourceResponse.DescribeAvailableResource_AvailableZone availableZone = new DescribeAvailableResourceResponse.DescribeAvailableResource_AvailableZone();
+				availableZone.RegionId = context.StringValue("DescribeAvailableResource.AvailableZones["+ i +"].RegionId");
+				availableZone.ZoneId = context.StringValue("DescribeAvailableResource.AvailableZones["+ i +"].ZoneId");
+				availableZone.Status = context.StringValue("DescribeAvailableResource.AvailableZones["+ i +"].Status");
+				availableZone.NetworkTypes = context.StringValue("DescribeAvailableResource.AvailableZones["+ i +"].NetworkTypes");
+
+				List<DescribeAvailableResourceResponse.DescribeAvailableResource_AvailableZone.DescribeAvailableResource_SupportedEngine> availableZone_supportedEngines = new List<DescribeAvailableResourceResponse.DescribeAvailableResource_AvailableZone.DescribeAvailableResource_SupportedEngine>();
+				for (int j = 0; j < context.Length("DescribeAvailableResource.AvailableZones["+ i +"].SupportedEngines.Length"); j++) {
+					DescribeAvailableResourceResponse.DescribeAvailableResource_AvailableZone.DescribeAvailableResource_SupportedEngine supportedEngine = new DescribeAvailableResourceResponse.DescribeAvailableResource_AvailableZone.DescribeAvailableResource_SupportedEngine();
+					supportedEngine.Engine = context.StringValue("DescribeAvailableResource.AvailableZones["+ i +"].SupportedEngines["+ j +"].Engine");
+
+					List<DescribeAvailableResourceResponse.DescribeAvailableResource_AvailableZone.DescribeAvailableResource_SupportedEngine.DescribeAvailableResource_SupportedEngineVersion> supportedEngine_supportedEngineVersions = new List<DescribeAvailableResourceResponse.DescribeAvailableResource_AvailableZone.DescribeAvailableResource_SupportedEngine.DescribeAvailableResource_SupportedEngineVersion>();
+					for (int k = 0; k < context.Length("DescribeAvailableResource.AvailableZones["+ i +"].SupportedEngines["+ j +"].SupportedEngineVersions.Length"); k++) {
+						DescribeAvailableResourceResponse.DescribeAvailableResource_AvailableZone.DescribeAvailableResource_SupportedEngine.DescribeAvailableResource_SupportedEngineVersion supportedEngineVersion = new DescribeAvailableResourceResponse.DescribeAvailableResource_AvailableZone.DescribeAvailableResource_SupportedEngine.DescribeAvailableResource_SupportedEngineVersion();
+						supportedEngineVersion.Version = context.StringValue("DescribeAvailableResource.AvailableZones["+ i +"].SupportedEngines["+ j +"].SupportedEngineVersions["+ k +"].Version");
+
+						List<DescribeAvailableResourceResponse.DescribeAvailableResource_AvailableZone.DescribeAvailableResource_SupportedEngine.DescribeAvailableResource_SupportedEngineVersion.DescribeAvailableResource_SupportedArchitectureType> supportedEngineVersion_supportedArchitectureTypes = new List<DescribeAvailableResourceResponse.DescribeAvailableResource_AvailableZone.DescribeAvailableResource_SupportedEngine.DescribeAvailableResource_SupportedEngineVersion.DescribeAvailableResource_SupportedArchitectureType>();
+						for (int l = 0; l < context.Length("DescribeAvailableResource.AvailableZones["+ i +"].SupportedEngines["+ j +"].SupportedEngineVersions["+ k +"].SupportedArchitectureTypes.Length"); l++) {
+							DescribeAvailableResourceResponse.DescribeAvailableResource_AvailableZone.DescribeAvailableResource_SupportedEngine.DescribeAvailableResource_SupportedEngineVersion.DescribeAvailableResource_SupportedArchitectureType supportedArchitectureType = new DescribeAvailableResourceResponse.DescribeAvailableResource_AvailableZone.DescribeAvailableResource_SupportedEngine.DescribeAvailableResource_SupportedEngineVersion.DescribeAvailableResource_SupportedArchitectureType();
+							supportedArchitectureType.Architecture = context.StringValue("DescribeAvailableResource.AvailableZones["+ i +"].SupportedEngines["+ j +"].SupportedEngineVersions["+ k +"].SupportedArchitectureTypes["+ l +"].Architecture");
+
+							List<DescribeAvailableResourceResponse.DescribeAvailableResource_AvailableZone.DescribeAvailableResource_SupportedEngine.DescribeAvailableResource_SupportedEngineVersion.DescribeAvailableResource_SupportedArchitectureType.DescribeAvailableResource_SupportedPerformanceType> supportedArchitectureType_supportedPerformanceTypes = new List<DescribeAvailableResourceResponse.DescribeAvailableResource_AvailableZone.DescribeAvailableResource_SupportedEngine.DescribeAvailableResource_SupportedEngineVersion.DescribeAvailableResource_SupportedArchitectureType.DescribeAvailableResource_SupportedPerformanceType>();
+							for (int m = 0; m < context.Length("DescribeAvailableResource.AvailableZones["+ i +"].SupportedEngines["+ j +"].SupportedEngineVersions["+ k +"].SupportedArchitectureTypes["+ l +"].SupportedPerformanceTypes.Length"); m++) {
+								DescribeAvailableResourceResponse.DescribeAvailableResource_AvailableZone.DescribeAvailableResource_SupportedEngine.DescribeAvailableResource_SupportedEngineVersion.DescribeAvailableResource_SupportedArchitectureType.DescribeAvailableResource_SupportedPerformanceType supportedPerformanceType = new DescribeAvailableResourceResponse.DescribeAvailableResource_AvailableZone.DescribeAvailableResource_SupportedEngine.DescribeAvailableResource_SupportedEngineVersion.DescribeAvailableResource_SupportedArchitectureType.DescribeAvailableResource_SupportedPerformanceType();
+								supportedPerformanceType.PerformanceType = context.StringValue("DescribeAvailableResource.AvailableZones["+ i +"].SupportedEngines["+ j +"].SupportedEngineVersions["+ k +"].SupportedArchitectureTypes["+ l +"].SupportedPerformanceTypes["+ m +"].PerformanceType");
+
+								List<DescribeAvailableResourceResponse.DescribeAvailableResource_AvailableZone.DescribeAvailableResource_SupportedEngine.DescribeAvailableResource_SupportedEngineVersion.DescribeAvailableResource_SupportedArchitectureType.DescribeAvailableResource_SupportedPerformanceType.DescribeAvailableResource_SupportedStorageType> supportedPerformanceType_supportedStorageTypes = new List<DescribeAvailableResourceResponse.DescribeAvailableResource_AvailableZone.DescribeAvailableResource_SupportedEngine.DescribeAvailableResource_SupportedEngineVersion.DescribeAvailableResource_SupportedArchitectureType.DescribeAvailableResource_SupportedPerformanceType.DescribeAvailableResource_SupportedStorageType>();
+								for (int n = 0; n < context.Length("DescribeAvailableResource.AvailableZones["+ i +"].SupportedEngines["+ j +"].SupportedEngineVersions["+ k +"].SupportedArchitectureTypes["+ l +"].SupportedPerformanceTypes["+ m +"].SupportedStorageTypes.Length"); n++) {
+									DescribeAvailableResourceResponse.DescribeAvailableResource_AvailableZone.DescribeAvailableResource_SupportedEngine.DescribeAvailableResource_SupportedEngineVersion.DescribeAvailableResource_SupportedArchitectureType.DescribeAvailableResource_SupportedPerformanceType.DescribeAvailableResource_SupportedStorageType supportedStorageType = new DescribeAvailableResourceResponse.DescribeAvailableResource_AvailableZone.DescribeAvailableResource_SupportedEngine.DescribeAvailableResource_SupportedEngineVersion.DescribeAvailableResource_SupportedArchitectureType.DescribeAvailableResource_SupportedPerformanceType.DescribeAvailableResource_SupportedStorageType();
+									supportedStorageType.StorageType = context.StringValue("DescribeAvailableResource.AvailableZones["+ i +"].SupportedEngines["+ j +"].SupportedEngineVersions["+ k +"].SupportedArchitectureTypes["+ l +"].SupportedPerformanceTypes["+ m +"].SupportedStorageTypes["+ n +"].StorageType");
+
+									List<DescribeAvailableResourceResponse.DescribeAvailableResource_AvailableZone.DescribeAvailableResource_SupportedEngine.DescribeAvailableResource_SupportedEngineVersion.DescribeAvailableResource_SupportedArchitectureType.DescribeAvailableResource_SupportedPerformanceType.DescribeAvailableResource_SupportedStorageType.DescribeAvailableResource_SupportedNodeType> supportedStorageType_supportedNodeTypes = new List<DescribeAvailableResourceResponse.DescribeAvailableResource_AvailableZone.DescribeAvailableResource_SupportedEngine.DescribeAvailableResource_SupportedEngineVersion.DescribeAvailableResource_SupportedArchitectureType.DescribeAvailableResource_SupportedPerformanceType.DescribeAvailableResource_SupportedStorageType.DescribeAvailableResource_SupportedNodeType>();
+									for (int o = 0; o < context.Length("DescribeAvailableResource.AvailableZones["+ i +"].SupportedEngines["+ j +"].SupportedEngineVersions["+ k +"].SupportedArchitectureTypes["+ l +"].SupportedPerformanceTypes["+ m +"].SupportedStorageTypes["+ n +"].SupportedNodeTypes.Length"); o++) {
+										DescribeAvailableResourceResponse.DescribeAvailableResource_AvailableZone.DescribeAvailableResource_SupportedEngine.DescribeAvailableResource_SupportedEngineVersion.DescribeAvailableResource_SupportedArchitectureType.DescribeAvailableResource_SupportedPerformanceType.DescribeAvailableResource_SupportedStorageType.DescribeAvailableResource_SupportedNodeType supportedNodeType = new DescribeAvailableResourceResponse.DescribeAvailableResource_AvailableZone.DescribeAvailableResource_SupportedEngine.DescribeAvailableResource_SupportedEngineVersion.DescribeAvailableResource_SupportedArchitectureType.DescribeAvailableResource_SupportedPerformanceType.DescribeAvailableResource_SupportedStorageType.DescribeAvailableResource_SupportedNodeType();
+										supportedNodeType.NodeType = context.StringValue("DescribeAvailableResource.AvailableZones["+ i +"].SupportedEngines["+ j +"].SupportedEngineVersions["+ k +"].SupportedArchitectureTypes["+ l +"].SupportedPerformanceTypes["+ m +"].SupportedStorageTypes["+ n +"].SupportedNodeTypes["+ o +"].NodeType");
+
+										List<DescribeAvailableResourceResponse.DescribeAvailableResource_AvailableZone.DescribeAvailableResource_SupportedEngine.DescribeAvailableResource_SupportedEngineVersion.DescribeAvailableResource_SupportedArchitectureType.DescribeAvailableResource_SupportedPerformanceType.DescribeAvailableResource_SupportedStorageType.DescribeAvailableResource_SupportedNodeType.DescribeAvailableResource_SupportedPackageType> supportedNodeType_supportedPackageTypes = new List<DescribeAvailableResourceResponse.DescribeAvailableResource_AvailableZone.DescribeAvailableResource_SupportedEngine.DescribeAvailableResource_SupportedEngineVersion.DescribeAvailableResource_SupportedArchitectureType.DescribeAvailableResource_SupportedPerformanceType.DescribeAvailableResource_SupportedStorageType.DescribeAvailableResource_SupportedNodeType.DescribeAvailableResource_SupportedPackageType>();
+										for (int p = 0; p < context.Length("DescribeAvailableResource.AvailableZones["+ i +"].SupportedEngines["+ j +"].SupportedEngineVersions["+ k +"].SupportedArchitectureTypes["+ l +"].SupportedPerformanceTypes["+ m +"].SupportedStorageTypes["+ n +"].SupportedNodeTypes["+ o +"].SupportedPackageTypes.Length"); p++) {
+											DescribeAvailableResourceResponse.DescribeAvailableResource_AvailableZone.DescribeAvailableResource_SupportedEngine.DescribeAvailableResource_SupportedEngineVersion.DescribeAvailableResource_SupportedArchitectureType.DescribeAvailableResource_SupportedPerformanceType.DescribeAvailableResource_SupportedStorageType.DescribeAvailableResource_SupportedNodeType.DescribeAvailableResource_SupportedPackageType supportedPackageType = new DescribeAvailableResourceResponse.DescribeAvailableResource_AvailableZone.DescribeAvailableResource_SupportedEngine.DescribeAvailableResource_SupportedEngineVersion.DescribeAvailableResource_SupportedArchitectureType.DescribeAvailableResource_SupportedPerformanceType.DescribeAvailableResource_SupportedStorageType.DescribeAvailableResource_SupportedNodeType.DescribeAvailableResource_SupportedPackageType();
+											supportedPackageType.PackageType = context.StringValue("DescribeAvailableResource.AvailableZones["+ i +"].SupportedEngines["+ j +"].SupportedEngineVersions["+ k +"].SupportedArchitectureTypes["+ l +"].SupportedPerformanceTypes["+ m +"].SupportedStorageTypes["+ n +"].SupportedNodeTypes["+ o +"].SupportedPackageTypes["+ p +"].PackageType");
+
+											List<DescribeAvailableResourceResponse.DescribeAvailableResource_AvailableZone.DescribeAvailableResource_SupportedEngine.DescribeAvailableResource_SupportedEngineVersion.DescribeAvailableResource_SupportedArchitectureType.DescribeAvailableResource_SupportedPerformanceType.DescribeAvailableResource_SupportedStorageType.DescribeAvailableResource_SupportedNodeType.DescribeAvailableResource_SupportedPackageType.DescribeAvailableResource_AvailableResource> supportedPackageType_availableResources = new List<DescribeAvailableResourceResponse.DescribeAvailableResource_AvailableZone.DescribeAvailableResource_SupportedEngine.DescribeAvailableResource_SupportedEngineVersion.DescribeAvailableResource_SupportedArchitectureType.DescribeAvailableResource_SupportedPerformanceType.DescribeAvailableResource_SupportedStorageType.DescribeAvailableResource_SupportedNodeType.DescribeAvailableResource_SupportedPackageType.DescribeAvailableResource_AvailableResource>();
+											for (int q = 0; q < context.Length("DescribeAvailableResource.AvailableZones["+ i +"].SupportedEngines["+ j +"].SupportedEngineVersions["+ k +"].SupportedArchitectureTypes["+ l +"].SupportedPerformanceTypes["+ m +"].SupportedStorageTypes["+ n +"].SupportedNodeTypes["+ o +"].SupportedPackageTypes["+ p +"].AvailableResources.Length"); q++) {
+												DescribeAvailableResourceResponse.DescribeAvailableResource_AvailableZone.DescribeAvailableResource_SupportedEngine.DescribeAvailableResource_SupportedEngineVersion.DescribeAvailableResource_SupportedArchitectureType.DescribeAvailableResource_SupportedPerformanceType.DescribeAvailableResource_SupportedStorageType.DescribeAvailableResource_SupportedNodeType.DescribeAvailableResource_SupportedPackageType.DescribeAvailableResource_AvailableResource availableResource = new DescribeAvailableResourceResponse.DescribeAvailableResource_AvailableZone.DescribeAvailableResource_SupportedEngine.DescribeAvailableResource_SupportedEngineVersion.DescribeAvailableResource_SupportedArchitectureType.DescribeAvailableResource_SupportedPerformanceType.DescribeAvailableResource_SupportedStorageType.DescribeAvailableResource_SupportedNodeType.DescribeAvailableResource_SupportedPackageType.DescribeAvailableResource_AvailableResource();
+												availableResource.InstanceClass = context.StringValue("DescribeAvailableResource.AvailableZones["+ i +"].SupportedEngines["+ j +"].SupportedEngineVersions["+ k +"].SupportedArchitectureTypes["+ l +"].SupportedPerformanceTypes["+ m +"].SupportedStorageTypes["+ n +"].SupportedNodeTypes["+ o +"].SupportedPackageTypes["+ p +"].AvailableResources["+ q +"].InstanceClass");
+
+												supportedPackageType_availableResources.Add(availableResource);
+											}
+											supportedPackageType.AvailableResources = supportedPackageType_availableResources;
+
+											supportedNodeType_supportedPackageTypes.Add(supportedPackageType);
+										}
+										supportedNodeType.SupportedPackageTypes = supportedNodeType_supportedPackageTypes;
+
+										supportedStorageType_supportedNodeTypes.Add(supportedNodeType);
+									}
+									supportedStorageType.SupportedNodeTypes = supportedStorageType_supportedNodeTypes;
+
+									supportedPerformanceType_supportedStorageTypes.Add(supportedStorageType);
+								}
+								supportedPerformanceType.SupportedStorageTypes = supportedPerformanceType_supportedStorageTypes;
+
+								supportedArchitectureType_supportedPerformanceTypes.Add(supportedPerformanceType);
+							}
+							supportedArchitectureType.SupportedPerformanceTypes = supportedArchitectureType_supportedPerformanceTypes;
+
+							supportedEngineVersion_supportedArchitectureTypes.Add(supportedArchitectureType);
+						}
+						supportedEngineVersion.SupportedArchitectureTypes = supportedEngineVersion_supportedArchitectureTypes;
+
+						supportedEngine_supportedEngineVersions.Add(supportedEngineVersion);
+					}
+					supportedEngine.SupportedEngineVersions = supportedEngine_supportedEngineVersions;
+
+					availableZone_supportedEngines.Add(supportedEngine);
+				}
+				availableZone.SupportedEngines = availableZone_supportedEngines;
+
+				describeAvailableResourceResponse_availableZones.Add(availableZone);
+			}
+			describeAvailableResourceResponse.AvailableZones = describeAvailableResourceResponse_availableZones;
+        
+			return describeAvailableResourceResponse;
+        }
+    }
+}
