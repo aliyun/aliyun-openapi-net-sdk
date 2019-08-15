48e04310dec88c23a74bf6ae0463d06842b6781e
sdk-team <sdk-team@alibabacloud.com>
2019-08-15 14:42:48 +0800

    FnF public version.

---
+++ aliyun-net-sdk-fnf/Fnf/Model/V20190315/ReportTaskFailedRequest.cs
@@ -0,0 +1,103 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core;
+using Aliyun.Acs.Core.Http;
+using Aliyun.Acs.Core.Transform;
+using Aliyun.Acs.Core.Utils;
+using Aliyun.Acs.fnf.Transform;
+using Aliyun.Acs.fnf.Transform.V20190315;
+
+namespace Aliyun.Acs.fnf.Model.V20190315
+{
+    public class ReportTaskFailedRequest : RpcAcsRequest<ReportTaskFailedResponse>
+    {
+        public ReportTaskFailedRequest()
+            : base("fnf", "2019-03-15", "ReportTaskFailed", "fnf", "openAPI")
+        {
+			Method = MethodType.POST;
+        }
+
+		private string requestId;
+
+		private string cause;
+
+		private string taskToken;
+
+		private string error;
+
+		public string RequestId
+		{
+			get
+			{
+				return requestId;
+			}
+			set	
+			{
+				requestId = value;
+				DictionaryUtil.Add(QueryParameters, "RequestId", value);
+			}
+		}
+
+		public string Cause
+		{
+			get
+			{
+				return cause;
+			}
+			set	
+			{
+				cause = value;
+				DictionaryUtil.Add(BodyParameters, "Cause", value);
+			}
+		}
+
+		public string TaskToken
+		{
+			get
+			{
+				return taskToken;
+			}
+			set	
+			{
+				taskToken = value;
+				DictionaryUtil.Add(QueryParameters, "TaskToken", value);
+			}
+		}
+
+		public string Error
+		{
+			get
+			{
+				return error;
+			}
+			set	
+			{
+				error = value;
+				DictionaryUtil.Add(BodyParameters, "Error", value);
+			}
+		}
+
+        public override ReportTaskFailedResponse GetResponse(UnmarshallerContext unmarshallerContext)
+        {
+            return ReportTaskFailedResponseUnmarshaller.Unmarshall(unmarshallerContext);
+        }
+    }
+}
---
+++ aliyun-net-sdk-fnf/Fnf/Model/V20190315/ReportTaskSucceededRequest.cs
@@ -0,0 +1,88 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core;
+using Aliyun.Acs.Core.Http;
+using Aliyun.Acs.Core.Transform;
+using Aliyun.Acs.Core.Utils;
+using Aliyun.Acs.fnf.Transform;
+using Aliyun.Acs.fnf.Transform.V20190315;
+
+namespace Aliyun.Acs.fnf.Model.V20190315
+{
+    public class ReportTaskSucceededRequest : RpcAcsRequest<ReportTaskSucceededResponse>
+    {
+        public ReportTaskSucceededRequest()
+            : base("fnf", "2019-03-15", "ReportTaskSucceeded", "fnf", "openAPI")
+        {
+			Method = MethodType.POST;
+        }
+
+		private string output;
+
+		private string requestId;
+
+		private string taskToken;
+
+		public string Output
+		{
+			get
+			{
+				return output;
+			}
+			set	
+			{
+				output = value;
+				DictionaryUtil.Add(BodyParameters, "Output", value);
+			}
+		}
+
+		public string RequestId
+		{
+			get
+			{
+				return requestId;
+			}
+			set	
+			{
+				requestId = value;
+				DictionaryUtil.Add(QueryParameters, "RequestId", value);
+			}
+		}
+
+		public string TaskToken
+		{
+			get
+			{
+				return taskToken;
+			}
+			set	
+			{
+				taskToken = value;
+				DictionaryUtil.Add(QueryParameters, "TaskToken", value);
+			}
+		}
+
+        public override ReportTaskSucceededResponse GetResponse(UnmarshallerContext unmarshallerContext)
+        {
+            return ReportTaskSucceededResponseUnmarshaller.Unmarshall(unmarshallerContext);
+        }
+    }
+}
