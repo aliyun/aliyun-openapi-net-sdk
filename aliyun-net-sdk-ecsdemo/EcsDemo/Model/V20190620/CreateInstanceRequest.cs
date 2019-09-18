12d51a3ba9e736506ec70980122d2c95e167179a
sdk-team <sdk-team@alibabacloud.com>
2019-09-18 10:08:01 +0800

    test

---
+++ aliyun-net-sdk-ecsdemo/EcsDemo/Model/V20190620/CreateInstanceRequest.cs
@@ -0,0 +1,274 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core;
+using Aliyun.Acs.Core.Http;
+using Aliyun.Acs.Core.Transform;
+using Aliyun.Acs.Core.Utils;
+using Aliyun.Acs.EcsDemo.Transform;
+using Aliyun.Acs.EcsDemo.Transform.V20190620;
+
+namespace Aliyun.Acs.EcsDemo.Model.V20190620
+{
+    public class CreateInstanceRequest : RpcAcsRequest<CreateInstanceResponse>
+    {
+        public CreateInstanceRequest()
+            : base("EcsDemo", "2019-06-20", "CreateInstance", "EcsDemo", "innerAPI")
+        {
+			Protocol = ProtocolType.HTTPS;
+			Method = MethodType.POST;
+        }
+
+		private string deploymentSetId;
+
+		private int? autoRenewPeriod;
+
+		private string imageId;
+
+		private string clientToken;
+
+		private string securityGroupId;
+
+		private int? internetMaxBandwidthOut;
+
+		private string hostName;
+
+		private string password;
+
+		private string instanceName;
+
+		private string passwordInherit;
+
+		private bool? autoRenew;
+
+		private string internetChargeType;
+
+		private string instanceType;
+
+		private string zoneId;
+
+		private int? internetMaxBandwidthIn;
+
+		public string DeploymentSetId
+		{
+			get
+			{
+				return deploymentSetId;
+			}
+			set	
+			{
+				deploymentSetId = value;
+				DictionaryUtil.Add(BodyParameters, "DeploymentSetId", value);
+			}
+		}
+
+		public int? AutoRenewPeriod
+		{
+			get
+			{
+				return autoRenewPeriod;
+			}
+			set	
+			{
+				autoRenewPeriod = value;
+				DictionaryUtil.Add(BodyParameters, "AutoRenewPeriod", value.ToString());
+			}
+		}
+
+		public string ImageId
+		{
+			get
+			{
+				return imageId;
+			}
+			set	
+			{
+				imageId = value;
+				DictionaryUtil.Add(BodyParameters, "ImageId", value);
+			}
+		}
+
+		public string ClientToken
+		{
+			get
+			{
+				return clientToken;
+			}
+			set	
+			{
+				clientToken = value;
+				DictionaryUtil.Add(BodyParameters, "ClientToken", value);
+			}
+		}
+
+		public string SecurityGroupId
+		{
+			get
+			{
+				return securityGroupId;
+			}
+			set	
+			{
+				securityGroupId = value;
+				DictionaryUtil.Add(BodyParameters, "SecurityGroupId", value);
+			}
+		}
+
+		public int? InternetMaxBandwidthOut
+		{
+			get
+			{
+				return internetMaxBandwidthOut;
+			}
+			set	
+			{
+				internetMaxBandwidthOut = value;
+				DictionaryUtil.Add(BodyParameters, "InternetMaxBandwidthOut", value.ToString());
+			}
+		}
+
+		public string HostName
+		{
+			get
+			{
+				return hostName;
+			}
+			set	
+			{
+				hostName = value;
+				DictionaryUtil.Add(BodyParameters, "HostName", value);
+			}
+		}
+
+		public string Password
+		{
+			get
+			{
+				return password;
+			}
+			set	
+			{
+				password = value;
+				DictionaryUtil.Add(BodyParameters, "Password", value);
+			}
+		}
+
+		public string InstanceName
+		{
+			get
+			{
+				return instanceName;
+			}
+			set	
+			{
+				instanceName = value;
+				DictionaryUtil.Add(BodyParameters, "InstanceName", value);
+			}
+		}
+
+		public string PasswordInherit
+		{
+			get
+			{
+				return passwordInherit;
+			}
+			set	
+			{
+				passwordInherit = value;
+				DictionaryUtil.Add(BodyParameters, "PasswordInherit", value);
+			}
+		}
+
+		public bool? AutoRenew
+		{
+			get
+			{
+				return autoRenew;
+			}
+			set	
+			{
+				autoRenew = value;
+				DictionaryUtil.Add(BodyParameters, "AutoRenew", value.ToString());
+			}
+		}
+
+		public string InternetChargeType
+		{
+			get
+			{
+				return internetChargeType;
+			}
+			set	
+			{
+				internetChargeType = value;
+				DictionaryUtil.Add(BodyParameters, "InternetChargeType", value);
+			}
+		}
+
+		public string InstanceType
+		{
+			get
+			{
+				return instanceType;
+			}
+			set	
+			{
+				instanceType = value;
+				DictionaryUtil.Add(BodyParameters, "InstanceType", value);
+			}
+		}
+
+		public string ZoneId
+		{
+			get
+			{
+				return zoneId;
+			}
+			set	
+			{
+				zoneId = value;
+				DictionaryUtil.Add(BodyParameters, "ZoneId", value);
+			}
+		}
+
+		public int? InternetMaxBandwidthIn
+		{
+			get
+			{
+				return internetMaxBandwidthIn;
+			}
+			set	
+			{
+				internetMaxBandwidthIn = value;
+				DictionaryUtil.Add(BodyParameters, "InternetMaxBandwidthIn", value.ToString());
+			}
+		}
+
+		public override bool CheckShowJsonItemName()
+		{
+			return false;
+		}
+
+        public override CreateInstanceResponse GetResponse(UnmarshallerContext unmarshallerContext)
+        {
+            return CreateInstanceResponseUnmarshaller.Unmarshall(unmarshallerContext);
+        }
+    }
+}
---
+++ aliyun-net-sdk-ecsdemo/EcsDemo/Model/V20190620/DeleteInstanceRequest.cs
@@ -0,0 +1,108 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core;
+using Aliyun.Acs.Core.Http;
+using Aliyun.Acs.Core.Transform;
+using Aliyun.Acs.Core.Utils;
+using Aliyun.Acs.EcsDemo.Transform;
+using Aliyun.Acs.EcsDemo.Transform.V20190620;
+
+namespace Aliyun.Acs.EcsDemo.Model.V20190620
+{
+    public class DeleteInstanceRequest : RpcAcsRequest<DeleteInstanceResponse>
+    {
+        public DeleteInstanceRequest()
+            : base("EcsDemo", "2019-06-20", "DeleteInstance", "EcsDemo", "innerAPI")
+        {
+			Protocol = ProtocolType.HTTPS;
+        }
+
+		private string instanceId;
+
+		private bool? terminateSubscription;
+
+		private string ownerAccount;
+
+		private string force;
+
+		public string InstanceId
+		{
+			get
+			{
+				return instanceId;
+			}
+			set	
+			{
+				instanceId = value;
+				DictionaryUtil.Add(BodyParameters, "InstanceId", value);
+			}
+		}
+
+		public bool? TerminateSubscription
+		{
+			get
+			{
+				return terminateSubscription;
+			}
+			set	
+			{
+				terminateSubscription = value;
+				DictionaryUtil.Add(BodyParameters, "TerminateSubscription", value.ToString());
+			}
+		}
+
+		public string OwnerAccount
+		{
+			get
+			{
+				return ownerAccount;
+			}
+			set	
+			{
+				ownerAccount = value;
+				DictionaryUtil.Add(BodyParameters, "OwnerAccount", value);
+			}
+		}
+
+		public string Force
+		{
+			get
+			{
+				return force;
+			}
+			set	
+			{
+				force = value;
+				DictionaryUtil.Add(BodyParameters, "Force", value);
+			}
+		}
+
+		public override bool CheckShowJsonItemName()
+		{
+			return false;
+		}
+
+        public override DeleteInstanceResponse GetResponse(UnmarshallerContext unmarshallerContext)
+        {
+            return DeleteInstanceResponseUnmarshaller.Unmarshall(unmarshallerContext);
+        }
+    }
+}
