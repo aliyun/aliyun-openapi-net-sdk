e20b9ab352e12de5016d367c1c520f884f8d0f6a
sdk-team <sdk-team@alibabacloud.com>
2019-11-13 20:42:22 +0800

    Supported Stream analyse.

---
+++ aliyun-net-sdk-imm/Imm/Model/V20170906/CreateStreamAnalyseTaskRequest.cs
@@ -0,0 +1,122 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core;
+using Aliyun.Acs.Core.Http;
+using Aliyun.Acs.Core.Transform;
+using Aliyun.Acs.Core.Utils;
+using Aliyun.Acs.imm.Transform;
+using Aliyun.Acs.imm.Transform.V20170906;
+
+namespace Aliyun.Acs.imm.Model.V20170906
+{
+    public class CreateStreamAnalyseTaskRequest : RpcAcsRequest<CreateStreamAnalyseTaskResponse>
+    {
+        public CreateStreamAnalyseTaskRequest()
+            : base("imm", "2017-09-06", "CreateStreamAnalyseTask", "imm", "openAPI")
+        {
+        }
+
+		private string project;
+
+		private string notifyEndpoint;
+
+		private string streamUri;
+
+		private string notifyTopicName;
+
+		private string tgtUri;
+
+		public string Project
+		{
+			get
+			{
+				return project;
+			}
+			set	
+			{
+				project = value;
+				DictionaryUtil.Add(QueryParameters, "Project", value);
+			}
+		}
+
+		public string NotifyEndpoint
+		{
+			get
+			{
+				return notifyEndpoint;
+			}
+			set	
+			{
+				notifyEndpoint = value;
+				DictionaryUtil.Add(QueryParameters, "NotifyEndpoint", value);
+			}
+		}
+
+		public string StreamUri
+		{
+			get
+			{
+				return streamUri;
+			}
+			set	
+			{
+				streamUri = value;
+				DictionaryUtil.Add(QueryParameters, "StreamUri", value);
+			}
+		}
+
+		public string NotifyTopicName
+		{
+			get
+			{
+				return notifyTopicName;
+			}
+			set	
+			{
+				notifyTopicName = value;
+				DictionaryUtil.Add(QueryParameters, "NotifyTopicName", value);
+			}
+		}
+
+		public string TgtUri
+		{
+			get
+			{
+				return tgtUri;
+			}
+			set	
+			{
+				tgtUri = value;
+				DictionaryUtil.Add(QueryParameters, "TgtUri", value);
+			}
+		}
+
+		public override bool CheckShowJsonItemName()
+		{
+			return false;
+		}
+
+        public override CreateStreamAnalyseTaskResponse GetResponse(UnmarshallerContext unmarshallerContext)
+        {
+            return CreateStreamAnalyseTaskResponseUnmarshaller.Unmarshall(unmarshallerContext);
+        }
+    }
+}
---
+++ aliyun-net-sdk-imm/Imm/Transform/V20170906/CreateStreamAnalyseTaskResponseUnmarshaller.cs
@@ -0,0 +1,41 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System;
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core.Transform;
+using Aliyun.Acs.imm.Model.V20170906;
+
+namespace Aliyun.Acs.imm.Transform.V20170906
+{
+    public class CreateStreamAnalyseTaskResponseUnmarshaller
+    {
+        public static CreateStreamAnalyseTaskResponse Unmarshall(UnmarshallerContext context)
+        {
+			CreateStreamAnalyseTaskResponse createStreamAnalyseTaskResponse = new CreateStreamAnalyseTaskResponse();
+
+			createStreamAnalyseTaskResponse.HttpResponse = context.HttpResponse;
+			createStreamAnalyseTaskResponse.RequestId = context.StringValue("CreateStreamAnalyseTask.RequestId");
+			createStreamAnalyseTaskResponse.TaskId = context.StringValue("CreateStreamAnalyseTask.TaskId");
+			createStreamAnalyseTaskResponse.TaskType = context.StringValue("CreateStreamAnalyseTask.TaskType");
+        
+			return createStreamAnalyseTaskResponse;
+        }
+    }
+}
