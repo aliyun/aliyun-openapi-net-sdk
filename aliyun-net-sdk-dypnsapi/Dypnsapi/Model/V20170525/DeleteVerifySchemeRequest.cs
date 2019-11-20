e26f6d9a09653b4e87f6e39f90e0be884c86d78a
sdk-team <sdk-team@alibabacloud.com>
2019-11-20 19:55:52 +0800

    Add new pop api.

---
+++ aliyun-net-sdk-dypnsapi/Dypnsapi/Model/V20170525/DeleteVerifySchemeRequest.cs
@@ -0,0 +1,122 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core;
+using Aliyun.Acs.Core.Http;
+using Aliyun.Acs.Core.Transform;
+using Aliyun.Acs.Core.Utils;
+using Aliyun.Acs.Dypnsapi.Transform;
+using Aliyun.Acs.Dypnsapi.Transform.V20170525;
+
+namespace Aliyun.Acs.Dypnsapi.Model.V20170525
+{
+    public class DeleteVerifySchemeRequest : RpcAcsRequest<DeleteVerifySchemeResponse>
+    {
+        public DeleteVerifySchemeRequest()
+            : base("Dypnsapi", "2017-05-25", "DeleteVerifyScheme", "dypnsapi", "openAPI")
+        {
+            if (this.GetType().GetProperty("ProductEndpointMap") != null && this.GetType().GetProperty("ProductEndpointType") != null)
+            {
+                this.GetType().GetProperty("ProductEndpointMap").SetValue(this, Endpoint.endpointMap, null);
+                this.GetType().GetProperty("ProductEndpointType").SetValue(this, Endpoint.endpointRegionalType, null);
+            }
+        }
+
+		private long? resourceOwnerId;
+
+		private string resourceOwnerAccount;
+
+		private long? ownerId;
+
+		private string schemeCode;
+
+		private long? customerId;
+
+		public long? ResourceOwnerId
+		{
+			get
+			{
+				return resourceOwnerId;
+			}
+			set	
+			{
+				resourceOwnerId = value;
+				DictionaryUtil.Add(QueryParameters, "ResourceOwnerId", value.ToString());
+			}
+		}
+
+		public string ResourceOwnerAccount
+		{
+			get
+			{
+				return resourceOwnerAccount;
+			}
+			set	
+			{
+				resourceOwnerAccount = value;
+				DictionaryUtil.Add(QueryParameters, "ResourceOwnerAccount", value);
+			}
+		}
+
+		public long? OwnerId
+		{
+			get
+			{
+				return ownerId;
+			}
+			set	
+			{
+				ownerId = value;
+				DictionaryUtil.Add(QueryParameters, "OwnerId", value.ToString());
+			}
+		}
+
+		public string SchemeCode
+		{
+			get
+			{
+				return schemeCode;
+			}
+			set	
+			{
+				schemeCode = value;
+				DictionaryUtil.Add(QueryParameters, "SchemeCode", value);
+			}
+		}
+
+		public long? CustomerId
+		{
+			get
+			{
+				return customerId;
+			}
+			set	
+			{
+				customerId = value;
+				DictionaryUtil.Add(QueryParameters, "CustomerId", value.ToString());
+			}
+		}
+
+        public override DeleteVerifySchemeResponse GetResponse(UnmarshallerContext unmarshallerContext)
+        {
+            return DeleteVerifySchemeResponseUnmarshaller.Unmarshall(unmarshallerContext);
+        }
+    }
+}
