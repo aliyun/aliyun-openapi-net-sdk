7d27c5b90956b938ec5ffb11411c7b879205ca6e
sdk-team <sdk-team@alibabacloud.com>
2019-12-26 18:08:58 +0800

    NlpAutoml add async api.

---
+++ aliyun-net-sdk-nlp-automl/Nlp_automl/Model/V20191111/GetAsyncPredictRequest.cs
@@ -0,0 +1,67 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core;
+using Aliyun.Acs.Core.Http;
+using Aliyun.Acs.Core.Transform;
+using Aliyun.Acs.Core.Utils;
+using Aliyun.Acs.nlp_automl.Transform;
+using Aliyun.Acs.nlp_automl.Transform.V20191111;
+
+namespace Aliyun.Acs.nlp_automl.Model.V20191111
+{
+    public class GetAsyncPredictRequest : RpcAcsRequest<GetAsyncPredictResponse>
+    {
+        public GetAsyncPredictRequest()
+            : base("nlp-automl", "2019-11-11", "GetAsyncPredict", "nlpautoml", "openAPI")
+        {
+            if (this.GetType().GetProperty("ProductEndpointMap") != null && this.GetType().GetProperty("ProductEndpointType") != null)
+            {
+                this.GetType().GetProperty("ProductEndpointMap").SetValue(this, Endpoint.endpointMap, null);
+                this.GetType().GetProperty("ProductEndpointType").SetValue(this, Endpoint.endpointRegionalType, null);
+            }
+        }
+
+		private int? asyncPredictId;
+
+		public int? AsyncPredictId
+		{
+			get
+			{
+				return asyncPredictId;
+			}
+			set	
+			{
+				asyncPredictId = value;
+				DictionaryUtil.Add(QueryParameters, "AsyncPredictId", value.ToString());
+			}
+		}
+
+		public override bool CheckShowJsonItemName()
+		{
+			return false;
+		}
+
+        public override GetAsyncPredictResponse GetResponse(UnmarshallerContext unmarshallerContext)
+        {
+            return GetAsyncPredictResponseUnmarshaller.Unmarshall(unmarshallerContext);
+        }
+    }
+}
---
+++ aliyun-net-sdk-nlp-automl/Nlp_automl/Transform/V20191111/GetAsyncPredictResponseUnmarshaller.cs
@@ -0,0 +1,42 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System;
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core.Transform;
+using Aliyun.Acs.nlp_automl.Model.V20191111;
+
+namespace Aliyun.Acs.nlp_automl.Transform.V20191111
+{
+    public class GetAsyncPredictResponseUnmarshaller
+    {
+        public static GetAsyncPredictResponse Unmarshall(UnmarshallerContext context)
+        {
+			GetAsyncPredictResponse getAsyncPredictResponse = new GetAsyncPredictResponse();
+
+			getAsyncPredictResponse.HttpResponse = context.HttpResponse;
+			getAsyncPredictResponse.RequestId = context.StringValue("GetAsyncPredict.RequestId");
+			getAsyncPredictResponse.AsyncPredictId = context.IntegerValue("GetAsyncPredict.AsyncPredictId");
+			getAsyncPredictResponse.Status = context.IntegerValue("GetAsyncPredict.Status");
+			getAsyncPredictResponse.Content = context.StringValue("GetAsyncPredict.Content");
+        
+			return getAsyncPredictResponse;
+        }
+    }
+}
