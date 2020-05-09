0cf56262af93bc4da5d24fe349fb00e072ae2df2
sdk-team <sdk-team@alibabacloud.com>
2020-05-09 22:51:32 +0800

    Generated 2019-01-01 for Cassandra.

---
+++ aliyun-net-sdk-cassandra/Cassandra/Model/V20190101/DescribeDataCenterResponse.cs
@@ -0,0 +1,280 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core;
+
+namespace Aliyun.Acs.Cassandra.Model.V20190101
+{
+	public class DescribeDataCenterResponse : AcsResponse
+	{
+
+		private string requestId;
+
+		private string dataCenterId;
+
+		private string regionId;
+
+		private string zoneId;
+
+		private string clusterId;
+
+		private string dataCenterName;
+
+		private string status;
+
+		private string createdTime;
+
+		private string instanceType;
+
+		private int? nodeCount;
+
+		private string diskType;
+
+		private int? diskSize;
+
+		private string vpcId;
+
+		private string vswitchId;
+
+		private string payType;
+
+		private string commodityInstance;
+
+		private string expireTime;
+
+		private string lockMode;
+
+		public string RequestId
+		{
+			get
+			{
+				return requestId;
+			}
+			set	
+			{
+				requestId = value;
+			}
+		}
+
+		public string DataCenterId
+		{
+			get
+			{
+				return dataCenterId;
+			}
+			set	
+			{
+				dataCenterId = value;
+			}
+		}
+
+		public string RegionId
+		{
+			get
+			{
+				return regionId;
+			}
+			set	
+			{
+				regionId = value;
+			}
+		}
+
+		public string ZoneId
+		{
+			get
+			{
+				return zoneId;
+			}
+			set	
+			{
+				zoneId = value;
+			}
+		}
+
+		public string ClusterId
+		{
+			get
+			{
+				return clusterId;
+			}
+			set	
+			{
+				clusterId = value;
+			}
+		}
+
+		public string DataCenterName
+		{
+			get
+			{
+				return dataCenterName;
+			}
+			set	
+			{
+				dataCenterName = value;
+			}
+		}
+
+		public string Status
+		{
+			get
+			{
+				return status;
+			}
+			set	
+			{
+				status = value;
+			}
+		}
+
+		public string CreatedTime
+		{
+			get
+			{
+				return createdTime;
+			}
+			set	
+			{
+				createdTime = value;
+			}
+		}
+
+		public string InstanceType
+		{
+			get
+			{
+				return instanceType;
+			}
+			set	
+			{
+				instanceType = value;
+			}
+		}
+
+		public int? NodeCount
+		{
+			get
+			{
+				return nodeCount;
+			}
+			set	
+			{
+				nodeCount = value;
+			}
+		}
+
+		public string DiskType
+		{
+			get
+			{
+				return diskType;
+			}
+			set	
+			{
+				diskType = value;
+			}
+		}
+
+		public int? DiskSize
+		{
+			get
+			{
+				return diskSize;
+			}
+			set	
+			{
+				diskSize = value;
+			}
+		}
+
+		public string VpcId
+		{
+			get
+			{
+				return vpcId;
+			}
+			set	
+			{
+				vpcId = value;
+			}
+		}
+
+		public string VswitchId
+		{
+			get
+			{
+				return vswitchId;
+			}
+			set	
+			{
+				vswitchId = value;
+			}
+		}
+
+		public string PayType
+		{
+			get
+			{
+				return payType;
+			}
+			set	
+			{
+				payType = value;
+			}
+		}
+
+		public string CommodityInstance
+		{
+			get
+			{
+				return commodityInstance;
+			}
+			set	
+			{
+				commodityInstance = value;
+			}
+		}
+
+		public string ExpireTime
+		{
+			get
+			{
+				return expireTime;
+			}
+			set	
+			{
+				expireTime = value;
+			}
+		}
+
+		public string LockMode
+		{
+			get
+			{
+				return lockMode;
+			}
+			set	
+			{
+				lockMode = value;
+			}
+		}
+	}
+}
---
+++ aliyun-net-sdk-cassandra/Cassandra/Transform/V20190101/DescribeDataCenterResponseUnmarshaller.cs
@@ -0,0 +1,56 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System;
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core.Transform;
+using Aliyun.Acs.Cassandra.Model.V20190101;
+
+namespace Aliyun.Acs.Cassandra.Transform.V20190101
+{
+    public class DescribeDataCenterResponseUnmarshaller
+    {
+        public static DescribeDataCenterResponse Unmarshall(UnmarshallerContext context)
+        {
+			DescribeDataCenterResponse describeDataCenterResponse = new DescribeDataCenterResponse();
+
+			describeDataCenterResponse.HttpResponse = context.HttpResponse;
+			describeDataCenterResponse.RequestId = context.StringValue("DescribeDataCenter.RequestId");
+			describeDataCenterResponse.DataCenterId = context.StringValue("DescribeDataCenter.DataCenterId");
+			describeDataCenterResponse.RegionId = context.StringValue("DescribeDataCenter.RegionId");
+			describeDataCenterResponse.ZoneId = context.StringValue("DescribeDataCenter.ZoneId");
+			describeDataCenterResponse.ClusterId = context.StringValue("DescribeDataCenter.ClusterId");
+			describeDataCenterResponse.DataCenterName = context.StringValue("DescribeDataCenter.DataCenterName");
+			describeDataCenterResponse.Status = context.StringValue("DescribeDataCenter.Status");
+			describeDataCenterResponse.CreatedTime = context.StringValue("DescribeDataCenter.CreatedTime");
+			describeDataCenterResponse.InstanceType = context.StringValue("DescribeDataCenter.InstanceType");
+			describeDataCenterResponse.NodeCount = context.IntegerValue("DescribeDataCenter.NodeCount");
+			describeDataCenterResponse.DiskType = context.StringValue("DescribeDataCenter.DiskType");
+			describeDataCenterResponse.DiskSize = context.IntegerValue("DescribeDataCenter.DiskSize");
+			describeDataCenterResponse.VpcId = context.StringValue("DescribeDataCenter.VpcId");
+			describeDataCenterResponse.VswitchId = context.StringValue("DescribeDataCenter.VswitchId");
+			describeDataCenterResponse.PayType = context.StringValue("DescribeDataCenter.PayType");
+			describeDataCenterResponse.CommodityInstance = context.StringValue("DescribeDataCenter.CommodityInstance");
+			describeDataCenterResponse.ExpireTime = context.StringValue("DescribeDataCenter.ExpireTime");
+			describeDataCenterResponse.LockMode = context.StringValue("DescribeDataCenter.LockMode");
+        
+			return describeDataCenterResponse;
+        }
+    }
+}
