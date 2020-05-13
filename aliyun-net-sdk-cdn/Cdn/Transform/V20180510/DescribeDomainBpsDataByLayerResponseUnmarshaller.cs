748d21932a2c911bb039770690e7af90e43d5699
sdk-team <sdk-team@alibabacloud.com>
2020-05-13 11:32:00 +0800

    Add ExportTas apis.

---
+++ aliyun-net-sdk-cdn/Cdn/Transform/V20180510/DescribeDomainBpsDataByLayerResponseUnmarshaller.cs
@@ -0,0 +1,51 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System;
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core.Transform;
+using Aliyun.Acs.Cdn.Model.V20180510;
+
+namespace Aliyun.Acs.Cdn.Transform.V20180510
+{
+    public class DescribeDomainBpsDataByLayerResponseUnmarshaller
+    {
+        public static DescribeDomainBpsDataByLayerResponse Unmarshall(UnmarshallerContext context)
+        {
+			DescribeDomainBpsDataByLayerResponse describeDomainBpsDataByLayerResponse = new DescribeDomainBpsDataByLayerResponse();
+
+			describeDomainBpsDataByLayerResponse.HttpResponse = context.HttpResponse;
+			describeDomainBpsDataByLayerResponse.RequestId = context.StringValue("DescribeDomainBpsDataByLayer.RequestId");
+			describeDomainBpsDataByLayerResponse.DataInterval = context.StringValue("DescribeDomainBpsDataByLayer.DataInterval");
+
+			List<DescribeDomainBpsDataByLayerResponse.DescribeDomainBpsDataByLayer_DataModule> describeDomainBpsDataByLayerResponse_bpsDataInterval = new List<DescribeDomainBpsDataByLayerResponse.DescribeDomainBpsDataByLayer_DataModule>();
+			for (int i = 0; i < context.Length("DescribeDomainBpsDataByLayer.BpsDataInterval.Length"); i++) {
+				DescribeDomainBpsDataByLayerResponse.DescribeDomainBpsDataByLayer_DataModule dataModule = new DescribeDomainBpsDataByLayerResponse.DescribeDomainBpsDataByLayer_DataModule();
+				dataModule.TimeStamp = context.StringValue("DescribeDomainBpsDataByLayer.BpsDataInterval["+ i +"].TimeStamp");
+				dataModule._Value = context.StringValue("DescribeDomainBpsDataByLayer.BpsDataInterval["+ i +"].Value");
+				dataModule.TrafficValue = context.StringValue("DescribeDomainBpsDataByLayer.BpsDataInterval["+ i +"].TrafficValue");
+
+				describeDomainBpsDataByLayerResponse_bpsDataInterval.Add(dataModule);
+			}
+			describeDomainBpsDataByLayerResponse.BpsDataInterval = describeDomainBpsDataByLayerResponse_bpsDataInterval;
+        
+			return describeDomainBpsDataByLayerResponse;
+        }
+    }
+}
---
+++ aliyun-net-sdk-cdn/Cdn/Transform/V20180510/DescribeDomainHttpCodeDataByLayerResponseUnmarshaller.cs
@@ -0,0 +1,51 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System;
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core.Transform;
+using Aliyun.Acs.Cdn.Model.V20180510;
+
+namespace Aliyun.Acs.Cdn.Transform.V20180510
+{
+    public class DescribeDomainHttpCodeDataByLayerResponseUnmarshaller
+    {
+        public static DescribeDomainHttpCodeDataByLayerResponse Unmarshall(UnmarshallerContext context)
+        {
+			DescribeDomainHttpCodeDataByLayerResponse describeDomainHttpCodeDataByLayerResponse = new DescribeDomainHttpCodeDataByLayerResponse();
+
+			describeDomainHttpCodeDataByLayerResponse.HttpResponse = context.HttpResponse;
+			describeDomainHttpCodeDataByLayerResponse.RequestId = context.StringValue("DescribeDomainHttpCodeDataByLayer.RequestId");
+			describeDomainHttpCodeDataByLayerResponse.DataInterval = context.StringValue("DescribeDomainHttpCodeDataByLayer.DataInterval");
+
+			List<DescribeDomainHttpCodeDataByLayerResponse.DescribeDomainHttpCodeDataByLayer_DataModule> describeDomainHttpCodeDataByLayerResponse_httpCodeDataInterval = new List<DescribeDomainHttpCodeDataByLayerResponse.DescribeDomainHttpCodeDataByLayer_DataModule>();
+			for (int i = 0; i < context.Length("DescribeDomainHttpCodeDataByLayer.HttpCodeDataInterval.Length"); i++) {
+				DescribeDomainHttpCodeDataByLayerResponse.DescribeDomainHttpCodeDataByLayer_DataModule dataModule = new DescribeDomainHttpCodeDataByLayerResponse.DescribeDomainHttpCodeDataByLayer_DataModule();
+				dataModule.TimeStamp = context.StringValue("DescribeDomainHttpCodeDataByLayer.HttpCodeDataInterval["+ i +"].TimeStamp");
+				dataModule._Value = context.StringValue("DescribeDomainHttpCodeDataByLayer.HttpCodeDataInterval["+ i +"].Value");
+				dataModule.TotalValue = context.StringValue("DescribeDomainHttpCodeDataByLayer.HttpCodeDataInterval["+ i +"].TotalValue");
+
+				describeDomainHttpCodeDataByLayerResponse_httpCodeDataInterval.Add(dataModule);
+			}
+			describeDomainHttpCodeDataByLayerResponse.HttpCodeDataInterval = describeDomainHttpCodeDataByLayerResponse_httpCodeDataInterval;
+        
+			return describeDomainHttpCodeDataByLayerResponse;
+        }
+    }
+}
