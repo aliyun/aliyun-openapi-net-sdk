1ea201731dd0911588f451449f2499623baaa41c
sdk-team <sdk-team@alibabacloud.com>
2020-07-01 22:33:43 +0800

    Add ListFace-MotorAlgorithmResults API.

---
+++ aliyun-net-sdk-vcs/Vcs/Model/V20200515/ListFaceAlgorithmResultsResponse.cs
@@ -0,0 +1,372 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core;
+
+namespace Aliyun.Acs.Vcs.Model.V20200515
+{
+	public class ListFaceAlgorithmResultsResponse : AcsResponse
+	{
+
+		private string code;
+
+		private string message;
+
+		private string requestId;
+
+		private ListFaceAlgorithmResults_Data data;
+
+		public string Code
+		{
+			get
+			{
+				return code;
+			}
+			set	
+			{
+				code = value;
+			}
+		}
+
+		public string Message
+		{
+			get
+			{
+				return message;
+			}
+			set	
+			{
+				message = value;
+			}
+		}
+
+		public string RequestId
+		{
+			get
+			{
+				return requestId;
+			}
+			set	
+			{
+				requestId = value;
+			}
+		}
+
+		public ListFaceAlgorithmResults_Data Data
+		{
+			get
+			{
+				return data;
+			}
+			set	
+			{
+				data = value;
+			}
+		}
+
+		public class ListFaceAlgorithmResults_Data
+		{
+
+			private int? pageNumber;
+
+			private int? pageSize;
+
+			private int? totalCount;
+
+			private int? totalPage;
+
+			private List<ListFaceAlgorithmResults_RecordsItem> records;
+
+			public int? PageNumber
+			{
+				get
+				{
+					return pageNumber;
+				}
+				set	
+				{
+					pageNumber = value;
+				}
+			}
+
+			public int? PageSize
+			{
+				get
+				{
+					return pageSize;
+				}
+				set	
+				{
+					pageSize = value;
+				}
+			}
+
+			public int? TotalCount
+			{
+				get
+				{
+					return totalCount;
+				}
+				set	
+				{
+					totalCount = value;
+				}
+			}
+
+			public int? TotalPage
+			{
+				get
+				{
+					return totalPage;
+				}
+				set	
+				{
+					totalPage = value;
+				}
+			}
+
+			public List<ListFaceAlgorithmResults_RecordsItem> Records
+			{
+				get
+				{
+					return records;
+				}
+				set	
+				{
+					records = value;
+				}
+			}
+
+			public class ListFaceAlgorithmResults_RecordsItem
+			{
+
+				private string faceId;
+
+				private string corpId;
+
+				private string dataSourceId;
+
+				private string shotTime;
+
+				private string genderCode;
+
+				private string minAge;
+
+				private string maxAge;
+
+				private string capStyle;
+
+				private string hairStyle;
+
+				private float? leftTopX;
+
+				private float? leftTopY;
+
+				private float? rightBottomX;
+
+				private float? rightBottomY;
+
+				private string picUrlPath;
+
+				private string targetPicUrlPath;
+
+				public string FaceId
+				{
+					get
+					{
+						return faceId;
+					}
+					set	
+					{
+						faceId = value;
+					}
+				}
+
+				public string CorpId
+				{
+					get
+					{
+						return corpId;
+					}
+					set	
+					{
+						corpId = value;
+					}
+				}
+
+				public string DataSourceId
+				{
+					get
+					{
+						return dataSourceId;
+					}
+					set	
+					{
+						dataSourceId = value;
+					}
+				}
+
+				public string ShotTime
+				{
+					get
+					{
+						return shotTime;
+					}
+					set	
+					{
+						shotTime = value;
+					}
+				}
+
+				public string GenderCode
+				{
+					get
+					{
+						return genderCode;
+					}
+					set	
+					{
+						genderCode = value;
+					}
+				}
+
+				public string MinAge
+				{
+					get
+					{
+						return minAge;
+					}
+					set	
+					{
+						minAge = value;
+					}
+				}
+
+				public string MaxAge
+				{
+					get
+					{
+						return maxAge;
+					}
+					set	
+					{
+						maxAge = value;
+					}
+				}
+
+				public string CapStyle
+				{
+					get
+					{
+						return capStyle;
+					}
+					set	
+					{
+						capStyle = value;
+					}
+				}
+
+				public string HairStyle
+				{
+					get
+					{
+						return hairStyle;
+					}
+					set	
+					{
+						hairStyle = value;
+					}
+				}
+
+				public float? LeftTopX
+				{
+					get
+					{
+						return leftTopX;
+					}
+					set	
+					{
+						leftTopX = value;
+					}
+				}
+
+				public float? LeftTopY
+				{
+					get
+					{
+						return leftTopY;
+					}
+					set	
+					{
+						leftTopY = value;
+					}
+				}
+
+				public float? RightBottomX
+				{
+					get
+					{
+						return rightBottomX;
+					}
+					set	
+					{
+						rightBottomX = value;
+					}
+				}
+
+				public float? RightBottomY
+				{
+					get
+					{
+						return rightBottomY;
+					}
+					set	
+					{
+						rightBottomY = value;
+					}
+				}
+
+				public string PicUrlPath
+				{
+					get
+					{
+						return picUrlPath;
+					}
+					set	
+					{
+						picUrlPath = value;
+					}
+				}
+
+				public string TargetPicUrlPath
+				{
+					get
+					{
+						return targetPicUrlPath;
+					}
+					set	
+					{
+						targetPicUrlPath = value;
+					}
+				}
+			}
+		}
+	}
+}
---
+++ aliyun-net-sdk-vcs/Vcs/Transform/V20200515/ListFaceAlgorithmResultsResponseUnmarshaller.cs
@@ -0,0 +1,71 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System;
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core.Transform;
+using Aliyun.Acs.Vcs.Model.V20200515;
+
+namespace Aliyun.Acs.Vcs.Transform.V20200515
+{
+    public class ListFaceAlgorithmResultsResponseUnmarshaller
+    {
+        public static ListFaceAlgorithmResultsResponse Unmarshall(UnmarshallerContext context)
+        {
+			ListFaceAlgorithmResultsResponse listFaceAlgorithmResultsResponse = new ListFaceAlgorithmResultsResponse();
+
+			listFaceAlgorithmResultsResponse.HttpResponse = context.HttpResponse;
+			listFaceAlgorithmResultsResponse.Code = context.StringValue("ListFaceAlgorithmResults.Code");
+			listFaceAlgorithmResultsResponse.Message = context.StringValue("ListFaceAlgorithmResults.Message");
+			listFaceAlgorithmResultsResponse.RequestId = context.StringValue("ListFaceAlgorithmResults.RequestId");
+
+			ListFaceAlgorithmResultsResponse.ListFaceAlgorithmResults_Data data = new ListFaceAlgorithmResultsResponse.ListFaceAlgorithmResults_Data();
+			data.PageNumber = context.IntegerValue("ListFaceAlgorithmResults.Data.PageNumber");
+			data.PageSize = context.IntegerValue("ListFaceAlgorithmResults.Data.PageSize");
+			data.TotalCount = context.IntegerValue("ListFaceAlgorithmResults.Data.TotalCount");
+			data.TotalPage = context.IntegerValue("ListFaceAlgorithmResults.Data.TotalPage");
+
+			List<ListFaceAlgorithmResultsResponse.ListFaceAlgorithmResults_Data.ListFaceAlgorithmResults_RecordsItem> data_records = new List<ListFaceAlgorithmResultsResponse.ListFaceAlgorithmResults_Data.ListFaceAlgorithmResults_RecordsItem>();
+			for (int i = 0; i < context.Length("ListFaceAlgorithmResults.Data.Records.Length"); i++) {
+				ListFaceAlgorithmResultsResponse.ListFaceAlgorithmResults_Data.ListFaceAlgorithmResults_RecordsItem recordsItem = new ListFaceAlgorithmResultsResponse.ListFaceAlgorithmResults_Data.ListFaceAlgorithmResults_RecordsItem();
+				recordsItem.FaceId = context.StringValue("ListFaceAlgorithmResults.Data.Records["+ i +"].FaceId");
+				recordsItem.CorpId = context.StringValue("ListFaceAlgorithmResults.Data.Records["+ i +"].CorpId");
+				recordsItem.DataSourceId = context.StringValue("ListFaceAlgorithmResults.Data.Records["+ i +"].DataSourceId");
+				recordsItem.ShotTime = context.StringValue("ListFaceAlgorithmResults.Data.Records["+ i +"].ShotTime");
+				recordsItem.GenderCode = context.StringValue("ListFaceAlgorithmResults.Data.Records["+ i +"].GenderCode");
+				recordsItem.MinAge = context.StringValue("ListFaceAlgorithmResults.Data.Records["+ i +"].MinAge");
+				recordsItem.MaxAge = context.StringValue("ListFaceAlgorithmResults.Data.Records["+ i +"].MaxAge");
+				recordsItem.CapStyle = context.StringValue("ListFaceAlgorithmResults.Data.Records["+ i +"].CapStyle");
+				recordsItem.HairStyle = context.StringValue("ListFaceAlgorithmResults.Data.Records["+ i +"].HairStyle");
+				recordsItem.LeftTopX = context.FloatValue("ListFaceAlgorithmResults.Data.Records["+ i +"].LeftTopX");
+				recordsItem.LeftTopY = context.FloatValue("ListFaceAlgorithmResults.Data.Records["+ i +"].LeftTopY");
+				recordsItem.RightBottomX = context.FloatValue("ListFaceAlgorithmResults.Data.Records["+ i +"].RightBottomX");
+				recordsItem.RightBottomY = context.FloatValue("ListFaceAlgorithmResults.Data.Records["+ i +"].RightBottomY");
+				recordsItem.PicUrlPath = context.StringValue("ListFaceAlgorithmResults.Data.Records["+ i +"].PicUrlPath");
+				recordsItem.TargetPicUrlPath = context.StringValue("ListFaceAlgorithmResults.Data.Records["+ i +"].TargetPicUrlPath");
+
+				data_records.Add(recordsItem);
+			}
+			data.Records = data_records;
+			listFaceAlgorithmResultsResponse.Data = data;
+        
+			return listFaceAlgorithmResultsResponse;
+        }
+    }
+}
