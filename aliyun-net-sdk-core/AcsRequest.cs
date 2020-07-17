ecec7f0db8cb5d78266511b5e0a408db4598bcdb
liaoyustudent <wb-ly617236@alibaba-inc.com>
2020-07-17 10:09:23 +0800

    test

--- aliyun-net-sdk-core/AcsRequest.cs
+++ aliyun-net-sdk-core/AcsRequest.cs
@@ -20,7 +20,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-using Aliyun.Acs.Core.Auth;
+using Aliyun.Acs.Core.Auth; 
 using Aliyun.Acs.Core.Http;
 using Aliyun.Acs.Core.Regions;
 using Aliyun.Acs.Core.Transform;
