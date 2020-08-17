b3b63e8725f39284f87643959307bbf168354d5d
sdk-team <sdk-team@alibabacloud.com>
2020-08-17 15:34:05 +0800

    Add list event algorithm details.

---
+++ aliyun-net-sdk-vcs/Vcs/Model/V20200515/ListEventAlgorithmDetailsResponse.cs
@@ -0,0 +1,396 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core;
+
+namespace Aliyun.Acs.Vcs.Model.V20200515
+{
+	public class ListEventAlgorithmDetailsResponse : AcsResponse
+	{
+
+		private string code;
+
+		private string message;
+
+		private int? pageNumber;
+
+		private int? pageSize;
+
+		private string requestId;
+
+		private string success;
+
+		private int? totalCount;
+
+		private List<ListEventAlgorithmDetails_Datas> data;
+
+		public string Code
+		{
+			get
+			{
+				return code;
+			}
+			set	
+			{
+				code = value;
+			}
+		}
+
+		public string Message
+		{
+			get
+			{
+				return message;
+			}
+			set	
+			{
+				message = value;
+			}
+		}
+
+		public int? PageNumber
+		{
+			get
+			{
+				return pageNumber;
+			}
+			set	
+			{
+				pageNumber = value;
+			}
+		}
+
+		public int? PageSize
+		{
+			get
+			{
+				return pageSize;
+			}
+			set	
+			{
+				pageSize = value;
+			}
+		}
+
+		public string RequestId
+		{
+			get
+			{
+				return requestId;
+			}
+			set	
+			{
+				requestId = value;
+			}
+		}
+
+		public string Success
+		{
+			get
+			{
+				return success;
+			}
+			set	
+			{
+				success = value;
+			}
+		}
+
+		public int? TotalCount
+		{
+			get
+			{
+				return totalCount;
+			}
+			set	
+			{
+				totalCount = value;
+			}
+		}
+
+		public List<ListEventAlgorithmDetails_Datas> Data
+		{
+			get
+			{
+				return data;
+			}
+			set	
+			{
+				data = value;
+			}
+		}
+
+		public class ListEventAlgorithmDetails_Datas
+		{
+
+			private string corpId;
+
+			private string dataSourceId;
+
+			private string eventType;
+
+			private string eventValue;
+
+			private string extendValue;
+
+			private string extraExtendValue;
+
+			private string faceCount;
+
+			private string leftTopX;
+
+			private string leftTopY;
+
+			private string picUrlPath;
+
+			private string pointX;
+
+			private string pointY;
+
+			private string recordId;
+
+			private string rightBottomX;
+
+			private string rightBottomY;
+
+			private string shotTime;
+
+			private string sourceId;
+
+			private string targetPicUrlPath;
+
+			public string CorpId
+			{
+				get
+				{
+					return corpId;
+				}
+				set	
+				{
+					corpId = value;
+				}
+			}
+
+			public string DataSourceId
+			{
+				get
+				{
+					return dataSourceId;
+				}
+				set	
+				{
+					dataSourceId = value;
+				}
+			}
+
+			public string EventType
+			{
+				get
+				{
+					return eventType;
+				}
+				set	
+				{
+					eventType = value;
+				}
+			}
+
+			public string EventValue
+			{
+				get
+				{
+					return eventValue;
+				}
+				set	
+				{
+					eventValue = value;
+				}
+			}
+
+			public string ExtendValue
+			{
+				get
+				{
+					return extendValue;
+				}
+				set	
+				{
+					extendValue = value;
+				}
+			}
+
+			public string ExtraExtendValue
+			{
+				get
+				{
+					return extraExtendValue;
+				}
+				set	
+				{
+					extraExtendValue = value;
+				}
+			}
+
+			public string FaceCount
+			{
+				get
+				{
+					return faceCount;
+				}
+				set	
+				{
+					faceCount = value;
+				}
+			}
+
+			public string LeftTopX
+			{
+				get
+				{
+					return leftTopX;
+				}
+				set	
+				{
+					leftTopX = value;
+				}
+			}
+
+			public string LeftTopY
+			{
+				get
+				{
+					return leftTopY;
+				}
+				set	
+				{
+					leftTopY = value;
+				}
+			}
+
+			public string PicUrlPath
+			{
+				get
+				{
+					return picUrlPath;
+				}
+				set	
+				{
+					picUrlPath = value;
+				}
+			}
+
+			public string PointX
+			{
+				get
+				{
+					return pointX;
+				}
+				set	
+				{
+					pointX = value;
+				}
+			}
+
+			public string PointY
+			{
+				get
+				{
+					return pointY;
+				}
+				set	
+				{
+					pointY = value;
+				}
+			}
+
+			public string RecordId
+			{
+				get
+				{
+					return recordId;
+				}
+				set	
+				{
+					recordId = value;
+				}
+			}
+
+			public string RightBottomX
+			{
+				get
+				{
+					return rightBottomX;
+				}
+				set	
+				{
+					rightBottomX = value;
+				}
+			}
+
+			public string RightBottomY
+			{
+				get
+				{
+					return rightBottomY;
+				}
+				set	
+				{
+					rightBottomY = value;
+				}
+			}
+
+			public string ShotTime
+			{
+				get
+				{
+					return shotTime;
+				}
+				set	
+				{
+					shotTime = value;
+				}
+			}
+
+			public string SourceId
+			{
+				get
+				{
+					return sourceId;
+				}
+				set	
+				{
+					sourceId = value;
+				}
+			}
+
+			public string TargetPicUrlPath
+			{
+				get
+				{
+					return targetPicUrlPath;
+				}
+				set	
+				{
+					targetPicUrlPath = value;
+				}
+			}
+		}
+	}
+}
---
+++ aliyun-net-sdk-vcs/Vcs/Transform/V20200515/ListEventAlgorithmDetailsResponseUnmarshaller.cs
@@ -0,0 +1,71 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System;
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core.Transform;
+using Aliyun.Acs.Vcs.Model.V20200515;
+
+namespace Aliyun.Acs.Vcs.Transform.V20200515
+{
+    public class ListEventAlgorithmDetailsResponseUnmarshaller
+    {
+        public static ListEventAlgorithmDetailsResponse Unmarshall(UnmarshallerContext context)
+        {
+			ListEventAlgorithmDetailsResponse listEventAlgorithmDetailsResponse = new ListEventAlgorithmDetailsResponse();
+
+			listEventAlgorithmDetailsResponse.HttpResponse = context.HttpResponse;
+			listEventAlgorithmDetailsResponse.Code = context.StringValue("ListEventAlgorithmDetails.Code");
+			listEventAlgorithmDetailsResponse.Message = context.StringValue("ListEventAlgorithmDetails.Message");
+			listEventAlgorithmDetailsResponse.PageNumber = context.IntegerValue("ListEventAlgorithmDetails.PageNumber");
+			listEventAlgorithmDetailsResponse.PageSize = context.IntegerValue("ListEventAlgorithmDetails.PageSize");
+			listEventAlgorithmDetailsResponse.RequestId = context.StringValue("ListEventAlgorithmDetails.RequestId");
+			listEventAlgorithmDetailsResponse.Success = context.StringValue("ListEventAlgorithmDetails.Success");
+			listEventAlgorithmDetailsResponse.TotalCount = context.IntegerValue("ListEventAlgorithmDetails.TotalCount");
+
+			List<ListEventAlgorithmDetailsResponse.ListEventAlgorithmDetails_Datas> listEventAlgorithmDetailsResponse_data = new List<ListEventAlgorithmDetailsResponse.ListEventAlgorithmDetails_Datas>();
+			for (int i = 0; i < context.Length("ListEventAlgorithmDetails.Data.Length"); i++) {
+				ListEventAlgorithmDetailsResponse.ListEventAlgorithmDetails_Datas datas = new ListEventAlgorithmDetailsResponse.ListEventAlgorithmDetails_Datas();
+				datas.CorpId = context.StringValue("ListEventAlgorithmDetails.Data["+ i +"].CorpId");
+				datas.DataSourceId = context.StringValue("ListEventAlgorithmDetails.Data["+ i +"].DataSourceId");
+				datas.EventType = context.StringValue("ListEventAlgorithmDetails.Data["+ i +"].EventType");
+				datas.EventValue = context.StringValue("ListEventAlgorithmDetails.Data["+ i +"].EventValue");
+				datas.ExtendValue = context.StringValue("ListEventAlgorithmDetails.Data["+ i +"].ExtendValue");
+				datas.ExtraExtendValue = context.StringValue("ListEventAlgorithmDetails.Data["+ i +"].ExtraExtendValue");
+				datas.FaceCount = context.StringValue("ListEventAlgorithmDetails.Data["+ i +"].FaceCount");
+				datas.LeftTopX = context.StringValue("ListEventAlgorithmDetails.Data["+ i +"].LeftTopX");
+				datas.LeftTopY = context.StringValue("ListEventAlgorithmDetails.Data["+ i +"].LeftTopY");
+				datas.PicUrlPath = context.StringValue("ListEventAlgorithmDetails.Data["+ i +"].PicUrlPath");
+				datas.PointX = context.StringValue("ListEventAlgorithmDetails.Data["+ i +"].PointX");
+				datas.PointY = context.StringValue("ListEventAlgorithmDetails.Data["+ i +"].PointY");
+				datas.RecordId = context.StringValue("ListEventAlgorithmDetails.Data["+ i +"].RecordId");
+				datas.RightBottomX = context.StringValue("ListEventAlgorithmDetails.Data["+ i +"].RightBottomX");
+				datas.RightBottomY = context.StringValue("ListEventAlgorithmDetails.Data["+ i +"].RightBottomY");
+				datas.ShotTime = context.StringValue("ListEventAlgorithmDetails.Data["+ i +"].ShotTime");
+				datas.SourceId = context.StringValue("ListEventAlgorithmDetails.Data["+ i +"].SourceId");
+				datas.TargetPicUrlPath = context.StringValue("ListEventAlgorithmDetails.Data["+ i +"].TargetPicUrlPath");
+
+				listEventAlgorithmDetailsResponse_data.Add(datas);
+			}
+			listEventAlgorithmDetailsResponse.Data = listEventAlgorithmDetailsResponse_data;
+        
+			return listEventAlgorithmDetailsResponse;
+        }
+    }
+}
