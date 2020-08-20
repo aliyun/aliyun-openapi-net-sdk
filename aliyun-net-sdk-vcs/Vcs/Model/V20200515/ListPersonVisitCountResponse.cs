64ed7c93321072be8cf388a4d350fe5983cd2175
sdk-team <sdk-team@alibabacloud.com>
2020-08-20 12:13:17 +0800

    Add list person visit count API.

---
+++ aliyun-net-sdk-vcs/Vcs/Model/V20200515/ListPersonVisitCountResponse.cs
@@ -0,0 +1,256 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core;
+
+namespace Aliyun.Acs.Vcs.Model.V20200515
+{
+	public class ListPersonVisitCountResponse : AcsResponse
+	{
+
+		private string code;
+
+		private string message;
+
+		private string pageNo;
+
+		private string pageSize;
+
+		private string requestId;
+
+		private string success;
+
+		private string totalCount;
+
+		private List<ListPersonVisitCount_Datas> data;
+
+		public string Code
+		{
+			get
+			{
+				return code;
+			}
+			set	
+			{
+				code = value;
+			}
+		}
+
+		public string Message
+		{
+			get
+			{
+				return message;
+			}
+			set	
+			{
+				message = value;
+			}
+		}
+
+		public string PageNo
+		{
+			get
+			{
+				return pageNo;
+			}
+			set	
+			{
+				pageNo = value;
+			}
+		}
+
+		public string PageSize
+		{
+			get
+			{
+				return pageSize;
+			}
+			set	
+			{
+				pageSize = value;
+			}
+		}
+
+		public string RequestId
+		{
+			get
+			{
+				return requestId;
+			}
+			set	
+			{
+				requestId = value;
+			}
+		}
+
+		public string Success
+		{
+			get
+			{
+				return success;
+			}
+			set	
+			{
+				success = value;
+			}
+		}
+
+		public string TotalCount
+		{
+			get
+			{
+				return totalCount;
+			}
+			set	
+			{
+				totalCount = value;
+			}
+		}
+
+		public List<ListPersonVisitCount_Datas> Data
+		{
+			get
+			{
+				return data;
+			}
+			set	
+			{
+				data = value;
+			}
+		}
+
+		public class ListPersonVisitCount_Datas
+		{
+
+			private string corpId;
+
+			private string deviceId;
+
+			private string groupId;
+
+			private string personId;
+
+			private string tagCode;
+
+			private string tagMetrics;
+
+			private string hourId;
+
+			private string dayId;
+
+			public string CorpId
+			{
+				get
+				{
+					return corpId;
+				}
+				set	
+				{
+					corpId = value;
+				}
+			}
+
+			public string DeviceId
+			{
+				get
+				{
+					return deviceId;
+				}
+				set	
+				{
+					deviceId = value;
+				}
+			}
+
+			public string GroupId
+			{
+				get
+				{
+					return groupId;
+				}
+				set	
+				{
+					groupId = value;
+				}
+			}
+
+			public string PersonId
+			{
+				get
+				{
+					return personId;
+				}
+				set	
+				{
+					personId = value;
+				}
+			}
+
+			public string TagCode
+			{
+				get
+				{
+					return tagCode;
+				}
+				set	
+				{
+					tagCode = value;
+				}
+			}
+
+			public string TagMetrics
+			{
+				get
+				{
+					return tagMetrics;
+				}
+				set	
+				{
+					tagMetrics = value;
+				}
+			}
+
+			public string HourId
+			{
+				get
+				{
+					return hourId;
+				}
+				set	
+				{
+					hourId = value;
+				}
+			}
+
+			public string DayId
+			{
+				get
+				{
+					return dayId;
+				}
+				set	
+				{
+					dayId = value;
+				}
+			}
+		}
+	}
+}
---
+++ aliyun-net-sdk-vcs/Vcs/Transform/V20200515/ListPersonVisitCountResponseUnmarshaller.cs
@@ -0,0 +1,61 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System;
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core.Transform;
+using Aliyun.Acs.Vcs.Model.V20200515;
+
+namespace Aliyun.Acs.Vcs.Transform.V20200515
+{
+    public class ListPersonVisitCountResponseUnmarshaller
+    {
+        public static ListPersonVisitCountResponse Unmarshall(UnmarshallerContext context)
+        {
+			ListPersonVisitCountResponse listPersonVisitCountResponse = new ListPersonVisitCountResponse();
+
+			listPersonVisitCountResponse.HttpResponse = context.HttpResponse;
+			listPersonVisitCountResponse.Code = context.StringValue("ListPersonVisitCount.Code");
+			listPersonVisitCountResponse.Message = context.StringValue("ListPersonVisitCount.Message");
+			listPersonVisitCountResponse.PageNo = context.StringValue("ListPersonVisitCount.PageNo");
+			listPersonVisitCountResponse.PageSize = context.StringValue("ListPersonVisitCount.PageSize");
+			listPersonVisitCountResponse.RequestId = context.StringValue("ListPersonVisitCount.RequestId");
+			listPersonVisitCountResponse.Success = context.StringValue("ListPersonVisitCount.Success");
+			listPersonVisitCountResponse.TotalCount = context.StringValue("ListPersonVisitCount.TotalCount");
+
+			List<ListPersonVisitCountResponse.ListPersonVisitCount_Datas> listPersonVisitCountResponse_data = new List<ListPersonVisitCountResponse.ListPersonVisitCount_Datas>();
+			for (int i = 0; i < context.Length("ListPersonVisitCount.Data.Length"); i++) {
+				ListPersonVisitCountResponse.ListPersonVisitCount_Datas datas = new ListPersonVisitCountResponse.ListPersonVisitCount_Datas();
+				datas.CorpId = context.StringValue("ListPersonVisitCount.Data["+ i +"].CorpId");
+				datas.DeviceId = context.StringValue("ListPersonVisitCount.Data["+ i +"].DeviceId");
+				datas.GroupId = context.StringValue("ListPersonVisitCount.Data["+ i +"].GroupId");
+				datas.PersonId = context.StringValue("ListPersonVisitCount.Data["+ i +"].PersonId");
+				datas.TagCode = context.StringValue("ListPersonVisitCount.Data["+ i +"].TagCode");
+				datas.TagMetrics = context.StringValue("ListPersonVisitCount.Data["+ i +"].TagMetrics");
+				datas.HourId = context.StringValue("ListPersonVisitCount.Data["+ i +"].HourId");
+				datas.DayId = context.StringValue("ListPersonVisitCount.Data["+ i +"].DayId");
+
+				listPersonVisitCountResponse_data.Add(datas);
+			}
+			listPersonVisitCountResponse.Data = listPersonVisitCountResponse_data;
+        
+			return listPersonVisitCountResponse;
+        }
+    }
+}
