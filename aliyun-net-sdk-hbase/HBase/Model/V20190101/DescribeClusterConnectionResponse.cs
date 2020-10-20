18b033d1a06483e5b6a4574278dae845173f7c21
sdk-team <sdk-team@alibabacloud.com>
2020-10-20 13:24:19 +0000

    Support modifyInstnaceType.

---
+++ aliyun-net-sdk-hbase/HBase/Model/V20190101/DescribeClusterConnectionResponse.cs
@@ -0,0 +1,476 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core;
+
+namespace Aliyun.Acs.HBase.Model.V20190101
+{
+	public class DescribeClusterConnectionResponse : AcsResponse
+	{
+
+		private string requestId;
+
+		private string netType;
+
+		private string vpcId;
+
+		private string vSwitchId;
+
+		private string dbType;
+
+		private string isMultimod;
+
+		private List<DescribeClusterConnection_ZkConnAddr> zkConnAddrs;
+
+		private List<DescribeClusterConnection_SlbConnAddr> slbConnAddrs;
+
+		private List<DescribeClusterConnection_ServiceConnAddr> serviceConnAddrs;
+
+		private DescribeClusterConnection_UiProxyConnAddrInfo uiProxyConnAddrInfo;
+
+		private DescribeClusterConnection_ThriftConn thriftConn;
+
+		public string RequestId
+		{
+			get
+			{
+				return requestId;
+			}
+			set	
+			{
+				requestId = value;
+			}
+		}
+
+		public string NetType
+		{
+			get
+			{
+				return netType;
+			}
+			set	
+			{
+				netType = value;
+			}
+		}
+
+		public string VpcId
+		{
+			get
+			{
+				return vpcId;
+			}
+			set	
+			{
+				vpcId = value;
+			}
+		}
+
+		public string VSwitchId
+		{
+			get
+			{
+				return vSwitchId;
+			}
+			set	
+			{
+				vSwitchId = value;
+			}
+		}
+
+		public string DbType
+		{
+			get
+			{
+				return dbType;
+			}
+			set	
+			{
+				dbType = value;
+			}
+		}
+
+		public string IsMultimod
+		{
+			get
+			{
+				return isMultimod;
+			}
+			set	
+			{
+				isMultimod = value;
+			}
+		}
+
+		public List<DescribeClusterConnection_ZkConnAddr> ZkConnAddrs
+		{
+			get
+			{
+				return zkConnAddrs;
+			}
+			set	
+			{
+				zkConnAddrs = value;
+			}
+		}
+
+		public List<DescribeClusterConnection_SlbConnAddr> SlbConnAddrs
+		{
+			get
+			{
+				return slbConnAddrs;
+			}
+			set	
+			{
+				slbConnAddrs = value;
+			}
+		}
+
+		public List<DescribeClusterConnection_ServiceConnAddr> ServiceConnAddrs
+		{
+			get
+			{
+				return serviceConnAddrs;
+			}
+			set	
+			{
+				serviceConnAddrs = value;
+			}
+		}
+
+		public DescribeClusterConnection_UiProxyConnAddrInfo UiProxyConnAddrInfo
+		{
+			get
+			{
+				return uiProxyConnAddrInfo;
+			}
+			set	
+			{
+				uiProxyConnAddrInfo = value;
+			}
+		}
+
+		public DescribeClusterConnection_ThriftConn ThriftConn
+		{
+			get
+			{
+				return thriftConn;
+			}
+			set	
+			{
+				thriftConn = value;
+			}
+		}
+
+		public class DescribeClusterConnection_ZkConnAddr
+		{
+
+			private string connAddr;
+
+			private string connAddrPort;
+
+			private string netType;
+
+			public string ConnAddr
+			{
+				get
+				{
+					return connAddr;
+				}
+				set	
+				{
+					connAddr = value;
+				}
+			}
+
+			public string ConnAddrPort
+			{
+				get
+				{
+					return connAddrPort;
+				}
+				set	
+				{
+					connAddrPort = value;
+				}
+			}
+
+			public string NetType
+			{
+				get
+				{
+					return netType;
+				}
+				set	
+				{
+					netType = value;
+				}
+			}
+		}
+
+		public class DescribeClusterConnection_SlbConnAddr
+		{
+
+			private string slbType;
+
+			private DescribeClusterConnection_ConnAddrInfo connAddrInfo;
+
+			public string SlbType
+			{
+				get
+				{
+					return slbType;
+				}
+				set	
+				{
+					slbType = value;
+				}
+			}
+
+			public DescribeClusterConnection_ConnAddrInfo ConnAddrInfo
+			{
+				get
+				{
+					return connAddrInfo;
+				}
+				set	
+				{
+					connAddrInfo = value;
+				}
+			}
+
+			public class DescribeClusterConnection_ConnAddrInfo
+			{
+
+				private string connAddr;
+
+				private string connAddrPort;
+
+				private string netType;
+
+				public string ConnAddr
+				{
+					get
+					{
+						return connAddr;
+					}
+					set	
+					{
+						connAddr = value;
+					}
+				}
+
+				public string ConnAddrPort
+				{
+					get
+					{
+						return connAddrPort;
+					}
+					set	
+					{
+						connAddrPort = value;
+					}
+				}
+
+				public string NetType
+				{
+					get
+					{
+						return netType;
+					}
+					set	
+					{
+						netType = value;
+					}
+				}
+			}
+		}
+
+		public class DescribeClusterConnection_ServiceConnAddr
+		{
+
+			private string connType;
+
+			private DescribeClusterConnection_ConnAddrInfo1 connAddrInfo1;
+
+			public string ConnType
+			{
+				get
+				{
+					return connType;
+				}
+				set	
+				{
+					connType = value;
+				}
+			}
+
+			public DescribeClusterConnection_ConnAddrInfo1 ConnAddrInfo1
+			{
+				get
+				{
+					return connAddrInfo1;
+				}
+				set	
+				{
+					connAddrInfo1 = value;
+				}
+			}
+
+			public class DescribeClusterConnection_ConnAddrInfo1
+			{
+
+				private string connAddr;
+
+				private string connAddrPort;
+
+				private string netType;
+
+				public string ConnAddr
+				{
+					get
+					{
+						return connAddr;
+					}
+					set	
+					{
+						connAddr = value;
+					}
+				}
+
+				public string ConnAddrPort
+				{
+					get
+					{
+						return connAddrPort;
+					}
+					set	
+					{
+						connAddrPort = value;
+					}
+				}
+
+				public string NetType
+				{
+					get
+					{
+						return netType;
+					}
+					set	
+					{
+						netType = value;
+					}
+				}
+			}
+		}
+
+		public class DescribeClusterConnection_UiProxyConnAddrInfo
+		{
+
+			private string connAddr;
+
+			private string connAddrPort;
+
+			private string netType;
+
+			public string ConnAddr
+			{
+				get
+				{
+					return connAddr;
+				}
+				set	
+				{
+					connAddr = value;
+				}
+			}
+
+			public string ConnAddrPort
+			{
+				get
+				{
+					return connAddrPort;
+				}
+				set	
+				{
+					connAddrPort = value;
+				}
+			}
+
+			public string NetType
+			{
+				get
+				{
+					return netType;
+				}
+				set	
+				{
+					netType = value;
+				}
+			}
+		}
+
+		public class DescribeClusterConnection_ThriftConn
+		{
+
+			private string connAddr;
+
+			private string connAddrPort;
+
+			private string netType;
+
+			public string ConnAddr
+			{
+				get
+				{
+					return connAddr;
+				}
+				set	
+				{
+					connAddr = value;
+				}
+			}
+
+			public string ConnAddrPort
+			{
+				get
+				{
+					return connAddrPort;
+				}
+				set	
+				{
+					connAddrPort = value;
+				}
+			}
+
+			public string NetType
+			{
+				get
+				{
+					return netType;
+				}
+				set	
+				{
+					netType = value;
+				}
+			}
+		}
+	}
+}
---
+++ aliyun-net-sdk-hbase/HBase/Transform/V20190101/DescribeClusterConnectionResponseUnmarshaller.cs
@@ -0,0 +1,97 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System;
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core.Transform;
+using Aliyun.Acs.HBase.Model.V20190101;
+
+namespace Aliyun.Acs.HBase.Transform.V20190101
+{
+    public class DescribeClusterConnectionResponseUnmarshaller
+    {
+        public static DescribeClusterConnectionResponse Unmarshall(UnmarshallerContext context)
+        {
+			DescribeClusterConnectionResponse describeClusterConnectionResponse = new DescribeClusterConnectionResponse();
+
+			describeClusterConnectionResponse.HttpResponse = context.HttpResponse;
+			describeClusterConnectionResponse.RequestId = context.StringValue("DescribeClusterConnection.RequestId");
+			describeClusterConnectionResponse.NetType = context.StringValue("DescribeClusterConnection.NetType");
+			describeClusterConnectionResponse.VpcId = context.StringValue("DescribeClusterConnection.VpcId");
+			describeClusterConnectionResponse.VSwitchId = context.StringValue("DescribeClusterConnection.VSwitchId");
+			describeClusterConnectionResponse.DbType = context.StringValue("DescribeClusterConnection.DbType");
+			describeClusterConnectionResponse.IsMultimod = context.StringValue("DescribeClusterConnection.IsMultimod");
+
+			DescribeClusterConnectionResponse.DescribeClusterConnection_UiProxyConnAddrInfo uiProxyConnAddrInfo = new DescribeClusterConnectionResponse.DescribeClusterConnection_UiProxyConnAddrInfo();
+			uiProxyConnAddrInfo.ConnAddr = context.StringValue("DescribeClusterConnection.UiProxyConnAddrInfo.ConnAddr");
+			uiProxyConnAddrInfo.ConnAddrPort = context.StringValue("DescribeClusterConnection.UiProxyConnAddrInfo.ConnAddrPort");
+			uiProxyConnAddrInfo.NetType = context.StringValue("DescribeClusterConnection.UiProxyConnAddrInfo.NetType");
+			describeClusterConnectionResponse.UiProxyConnAddrInfo = uiProxyConnAddrInfo;
+
+			DescribeClusterConnectionResponse.DescribeClusterConnection_ThriftConn thriftConn = new DescribeClusterConnectionResponse.DescribeClusterConnection_ThriftConn();
+			thriftConn.ConnAddr = context.StringValue("DescribeClusterConnection.ThriftConn.ConnAddr");
+			thriftConn.ConnAddrPort = context.StringValue("DescribeClusterConnection.ThriftConn.ConnAddrPort");
+			thriftConn.NetType = context.StringValue("DescribeClusterConnection.ThriftConn.NetType");
+			describeClusterConnectionResponse.ThriftConn = thriftConn;
+
+			List<DescribeClusterConnectionResponse.DescribeClusterConnection_ZkConnAddr> describeClusterConnectionResponse_zkConnAddrs = new List<DescribeClusterConnectionResponse.DescribeClusterConnection_ZkConnAddr>();
+			for (int i = 0; i < context.Length("DescribeClusterConnection.ZkConnAddrs.Length"); i++) {
+				DescribeClusterConnectionResponse.DescribeClusterConnection_ZkConnAddr zkConnAddr = new DescribeClusterConnectionResponse.DescribeClusterConnection_ZkConnAddr();
+				zkConnAddr.ConnAddr = context.StringValue("DescribeClusterConnection.ZkConnAddrs["+ i +"].ConnAddr");
+				zkConnAddr.ConnAddrPort = context.StringValue("DescribeClusterConnection.ZkConnAddrs["+ i +"].ConnAddrPort");
+				zkConnAddr.NetType = context.StringValue("DescribeClusterConnection.ZkConnAddrs["+ i +"].NetType");
+
+				describeClusterConnectionResponse_zkConnAddrs.Add(zkConnAddr);
+			}
+			describeClusterConnectionResponse.ZkConnAddrs = describeClusterConnectionResponse_zkConnAddrs;
+
+			List<DescribeClusterConnectionResponse.DescribeClusterConnection_SlbConnAddr> describeClusterConnectionResponse_slbConnAddrs = new List<DescribeClusterConnectionResponse.DescribeClusterConnection_SlbConnAddr>();
+			for (int i = 0; i < context.Length("DescribeClusterConnection.SlbConnAddrs.Length"); i++) {
+				DescribeClusterConnectionResponse.DescribeClusterConnection_SlbConnAddr slbConnAddr = new DescribeClusterConnectionResponse.DescribeClusterConnection_SlbConnAddr();
+				slbConnAddr.SlbType = context.StringValue("DescribeClusterConnection.SlbConnAddrs["+ i +"].SlbType");
+
+				DescribeClusterConnectionResponse.DescribeClusterConnection_SlbConnAddr.DescribeClusterConnection_ConnAddrInfo connAddrInfo = new DescribeClusterConnectionResponse.DescribeClusterConnection_SlbConnAddr.DescribeClusterConnection_ConnAddrInfo();
+				connAddrInfo.ConnAddr = context.StringValue("DescribeClusterConnection.SlbConnAddrs["+ i +"].ConnAddrInfo.ConnAddr");
+				connAddrInfo.ConnAddrPort = context.StringValue("DescribeClusterConnection.SlbConnAddrs["+ i +"].ConnAddrInfo.ConnAddrPort");
+				connAddrInfo.NetType = context.StringValue("DescribeClusterConnection.SlbConnAddrs["+ i +"].ConnAddrInfo.NetType");
+				slbConnAddr.ConnAddrInfo = connAddrInfo;
+
+				describeClusterConnectionResponse_slbConnAddrs.Add(slbConnAddr);
+			}
+			describeClusterConnectionResponse.SlbConnAddrs = describeClusterConnectionResponse_slbConnAddrs;
+
+			List<DescribeClusterConnectionResponse.DescribeClusterConnection_ServiceConnAddr> describeClusterConnectionResponse_serviceConnAddrs = new List<DescribeClusterConnectionResponse.DescribeClusterConnection_ServiceConnAddr>();
+			for (int i = 0; i < context.Length("DescribeClusterConnection.ServiceConnAddrs.Length"); i++) {
+				DescribeClusterConnectionResponse.DescribeClusterConnection_ServiceConnAddr serviceConnAddr = new DescribeClusterConnectionResponse.DescribeClusterConnection_ServiceConnAddr();
+				serviceConnAddr.ConnType = context.StringValue("DescribeClusterConnection.ServiceConnAddrs["+ i +"].ConnType");
+
+				DescribeClusterConnectionResponse.DescribeClusterConnection_ServiceConnAddr.DescribeClusterConnection_ConnAddrInfo1 connAddrInfo1 = new DescribeClusterConnectionResponse.DescribeClusterConnection_ServiceConnAddr.DescribeClusterConnection_ConnAddrInfo1();
+				connAddrInfo1.ConnAddr = context.StringValue("DescribeClusterConnection.ServiceConnAddrs["+ i +"].ConnAddrInfo.ConnAddr");
+				connAddrInfo1.ConnAddrPort = context.StringValue("DescribeClusterConnection.ServiceConnAddrs["+ i +"].ConnAddrInfo.ConnAddrPort");
+				connAddrInfo1.NetType = context.StringValue("DescribeClusterConnection.ServiceConnAddrs["+ i +"].ConnAddrInfo.NetType");
+				serviceConnAddr.ConnAddrInfo1 = connAddrInfo1;
+
+				describeClusterConnectionResponse_serviceConnAddrs.Add(serviceConnAddr);
+			}
+			describeClusterConnectionResponse.ServiceConnAddrs = describeClusterConnectionResponse_serviceConnAddrs;
+        
+			return describeClusterConnectionResponse;
+        }
+    }
+}
