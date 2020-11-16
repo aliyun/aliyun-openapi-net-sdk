4febfedc62e5de03d815430b37e68d764932342e
sdk-team <sdk-team@alibabacloud.com>
2020-11-16 02:36:05 +0000

    Update goodstech.

---
+++ aliyun-net-sdk-goodstech/Goodstech/Model/V20191230/ClassifyCommodityResponse.cs
@@ -0,0 +1,120 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core;
+
+namespace Aliyun.Acs.goodstech.Model.V20191230
+{
+	public class ClassifyCommodityResponse : AcsResponse
+	{
+
+		private string requestId;
+
+		private ClassifyCommodity_Data data;
+
+		public string RequestId
+		{
+			get
+			{
+				return requestId;
+			}
+			set	
+			{
+				requestId = value;
+			}
+		}
+
+		public ClassifyCommodity_Data Data
+		{
+			get
+			{
+				return data;
+			}
+			set	
+			{
+				data = value;
+			}
+		}
+
+		public class ClassifyCommodity_Data
+		{
+
+			private List<ClassifyCommodity_Category> categories;
+
+			public List<ClassifyCommodity_Category> Categories
+			{
+				get
+				{
+					return categories;
+				}
+				set	
+				{
+					categories = value;
+				}
+			}
+
+			public class ClassifyCommodity_Category
+			{
+
+				private float? score;
+
+				private string categoryName;
+
+				private string categoryId;
+
+				public float? Score
+				{
+					get
+					{
+						return score;
+					}
+					set	
+					{
+						score = value;
+					}
+				}
+
+				public string CategoryName
+				{
+					get
+					{
+						return categoryName;
+					}
+					set	
+					{
+						categoryName = value;
+					}
+				}
+
+				public string CategoryId
+				{
+					get
+					{
+						return categoryId;
+					}
+					set	
+					{
+						categoryId = value;
+					}
+				}
+			}
+		}
+	}
+}
---
+++ aliyun-net-sdk-goodstech/Goodstech/Model/V20191230/RecognizeFurnitureSpuRequest.cs
@@ -0,0 +1,113 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core;
+using Aliyun.Acs.Core.Http;
+using Aliyun.Acs.Core.Transform;
+using Aliyun.Acs.Core.Utils;
+using Aliyun.Acs.goodstech.Transform;
+using Aliyun.Acs.goodstech.Transform.V20191230;
+
+namespace Aliyun.Acs.goodstech.Model.V20191230
+{
+    public class RecognizeFurnitureSpuRequest : RpcAcsRequest<RecognizeFurnitureSpuResponse>
+    {
+        public RecognizeFurnitureSpuRequest()
+            : base("goodstech", "2019-12-30", "RecognizeFurnitureSpu", "goodstech", "openAPI")
+        {
+            if (this.GetType().GetProperty("ProductEndpointMap") != null && this.GetType().GetProperty("ProductEndpointType") != null)
+            {
+                this.GetType().GetProperty("ProductEndpointMap").SetValue(this, Aliyun.Acs.goodstech.Endpoint.endpointMap, null);
+                this.GetType().GetProperty("ProductEndpointType").SetValue(this, Aliyun.Acs.goodstech.Endpoint.endpointRegionalType, null);
+            }
+			Method = MethodType.POST;
+        }
+
+		private float? xLength;
+
+		private float? zLength;
+
+		private string imageURL;
+
+		private float? yLength;
+
+		public float? XLength
+		{
+			get
+			{
+				return xLength;
+			}
+			set	
+			{
+				xLength = value;
+				DictionaryUtil.Add(BodyParameters, "XLength", value.ToString());
+			}
+		}
+
+		public float? ZLength
+		{
+			get
+			{
+				return zLength;
+			}
+			set	
+			{
+				zLength = value;
+				DictionaryUtil.Add(BodyParameters, "ZLength", value.ToString());
+			}
+		}
+
+		public string ImageURL
+		{
+			get
+			{
+				return imageURL;
+			}
+			set	
+			{
+				imageURL = value;
+				DictionaryUtil.Add(BodyParameters, "ImageURL", value);
+			}
+		}
+
+		public float? YLength
+		{
+			get
+			{
+				return yLength;
+			}
+			set	
+			{
+				yLength = value;
+				DictionaryUtil.Add(BodyParameters, "YLength", value.ToString());
+			}
+		}
+
+		public override bool CheckShowJsonItemName()
+		{
+			return false;
+		}
+
+        public override RecognizeFurnitureSpuResponse GetResponse(UnmarshallerContext unmarshallerContext)
+        {
+            return RecognizeFurnitureSpuResponseUnmarshaller.Unmarshall(unmarshallerContext);
+        }
+    }
+}
