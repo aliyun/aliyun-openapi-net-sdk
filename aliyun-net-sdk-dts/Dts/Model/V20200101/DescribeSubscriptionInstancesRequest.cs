862ddd277a5d8450f2feb4700631e0d11186cf10
sdk-team <sdk-team@alibabacloud.com>
2020-12-02 07:00:27 +0000

    Support C Sharp.

---
+++ aliyun-net-sdk-dts/Dts/Model/V20200101/DescribeSubscriptionInstancesRequest.cs
@@ -0,0 +1,138 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core;
+using Aliyun.Acs.Core.Http;
+using Aliyun.Acs.Core.Transform;
+using Aliyun.Acs.Core.Utils;
+using Aliyun.Acs.Dts.Transform;
+using Aliyun.Acs.Dts.Transform.V20200101;
+
+namespace Aliyun.Acs.Dts.Model.V20200101
+{
+    public class DescribeSubscriptionInstancesRequest : RpcAcsRequest<DescribeSubscriptionInstancesResponse>
+    {
+        public DescribeSubscriptionInstancesRequest()
+            : base("Dts", "2020-01-01", "DescribeSubscriptionInstances", "dts", "openAPI")
+        {
+            if (this.GetType().GetProperty("ProductEndpointMap") != null && this.GetType().GetProperty("ProductEndpointType") != null)
+            {
+                this.GetType().GetProperty("ProductEndpointMap").SetValue(this, Aliyun.Acs.Dts.Endpoint.endpointMap, null);
+                this.GetType().GetProperty("ProductEndpointType").SetValue(this, Aliyun.Acs.Dts.Endpoint.endpointRegionalType, null);
+            }
+			Method = MethodType.POST;
+        }
+
+		private string clientToken;
+
+		private int? pageNum;
+
+		private string ownerId;
+
+		private string accountId;
+
+		private int? pageSize;
+
+		private string subscriptionInstanceName;
+
+		public string ClientToken
+		{
+			get
+			{
+				return clientToken;
+			}
+			set	
+			{
+				clientToken = value;
+				DictionaryUtil.Add(QueryParameters, "ClientToken", value);
+			}
+		}
+
+		public int? PageNum
+		{
+			get
+			{
+				return pageNum;
+			}
+			set	
+			{
+				pageNum = value;
+				DictionaryUtil.Add(QueryParameters, "PageNum", value.ToString());
+			}
+		}
+
+		public string OwnerId
+		{
+			get
+			{
+				return ownerId;
+			}
+			set	
+			{
+				ownerId = value;
+				DictionaryUtil.Add(QueryParameters, "OwnerId", value);
+			}
+		}
+
+		public string AccountId
+		{
+			get
+			{
+				return accountId;
+			}
+			set	
+			{
+				accountId = value;
+				DictionaryUtil.Add(QueryParameters, "AccountId", value);
+			}
+		}
+
+		public int? PageSize
+		{
+			get
+			{
+				return pageSize;
+			}
+			set	
+			{
+				pageSize = value;
+				DictionaryUtil.Add(QueryParameters, "PageSize", value.ToString());
+			}
+		}
+
+		public string SubscriptionInstanceName
+		{
+			get
+			{
+				return subscriptionInstanceName;
+			}
+			set	
+			{
+				subscriptionInstanceName = value;
+				DictionaryUtil.Add(QueryParameters, "SubscriptionInstanceName", value);
+			}
+		}
+
+        public override DescribeSubscriptionInstancesResponse GetResponse(UnmarshallerContext unmarshallerContext)
+        {
+            return DescribeSubscriptionInstancesResponseUnmarshaller.Unmarshall(unmarshallerContext);
+        }
+    }
+}
