53c9c7a684c8173d999b501c7bf99dfea00ac516
sdk-team <sdk-team@alibabacloud.com>
2021-01-21 08:25:42 +0000

    Generated 2019-09-10 for waf-openapi.

---
+++ aliyun-net-sdk-waf-openapi/Waf_openapi/Model/V20190910/DescribeProtectionModuleCodeConfigRequest.cs
@@ -0,0 +1,143 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core;
+using Aliyun.Acs.Core.Http;
+using Aliyun.Acs.Core.Transform;
+using Aliyun.Acs.Core.Utils;
+using Aliyun.Acs.waf_openapi.Transform;
+using Aliyun.Acs.waf_openapi.Transform.V20190910;
+
+namespace Aliyun.Acs.waf_openapi.Model.V20190910
+{
+    public class DescribeProtectionModuleCodeConfigRequest : RpcAcsRequest<DescribeProtectionModuleCodeConfigResponse>
+    {
+        public DescribeProtectionModuleCodeConfigRequest()
+            : base("waf-openapi", "2019-09-10", "DescribeProtectionModuleCodeConfig", "waf", "openAPI")
+        {
+            if (this.GetType().GetProperty("ProductEndpointMap") != null && this.GetType().GetProperty("ProductEndpointType") != null)
+            {
+                this.GetType().GetProperty("ProductEndpointMap").SetValue(this, Aliyun.Acs.waf_openapi.Endpoint.endpointMap, null);
+                this.GetType().GetProperty("ProductEndpointType").SetValue(this, Aliyun.Acs.waf_openapi.Endpoint.endpointRegionalType, null);
+            }
+			Method = MethodType.POST;
+        }
+
+		private int? codeValue;
+
+		private string resourceGroupId;
+
+		private string instanceId;
+
+		private string sourceIp;
+
+		private int? codeType;
+
+		private string lang;
+
+		public int? CodeValue
+		{
+			get
+			{
+				return codeValue;
+			}
+			set	
+			{
+				codeValue = value;
+				DictionaryUtil.Add(QueryParameters, "CodeValue", value.ToString());
+			}
+		}
+
+		public string ResourceGroupId
+		{
+			get
+			{
+				return resourceGroupId;
+			}
+			set	
+			{
+				resourceGroupId = value;
+				DictionaryUtil.Add(QueryParameters, "ResourceGroupId", value);
+			}
+		}
+
+		public string InstanceId
+		{
+			get
+			{
+				return instanceId;
+			}
+			set	
+			{
+				instanceId = value;
+				DictionaryUtil.Add(QueryParameters, "InstanceId", value);
+			}
+		}
+
+		public string SourceIp
+		{
+			get
+			{
+				return sourceIp;
+			}
+			set	
+			{
+				sourceIp = value;
+				DictionaryUtil.Add(QueryParameters, "SourceIp", value);
+			}
+		}
+
+		public int? CodeType
+		{
+			get
+			{
+				return codeType;
+			}
+			set	
+			{
+				codeType = value;
+				DictionaryUtil.Add(QueryParameters, "CodeType", value.ToString());
+			}
+		}
+
+		public string Lang
+		{
+			get
+			{
+				return lang;
+			}
+			set	
+			{
+				lang = value;
+				DictionaryUtil.Add(QueryParameters, "Lang", value);
+			}
+		}
+
+		public override bool CheckShowJsonItemName()
+		{
+			return false;
+		}
+
+        public override DescribeProtectionModuleCodeConfigResponse GetResponse(UnmarshallerContext unmarshallerContext)
+        {
+            return DescribeProtectionModuleCodeConfigResponseUnmarshaller.Unmarshall(unmarshallerContext);
+        }
+    }
+}
