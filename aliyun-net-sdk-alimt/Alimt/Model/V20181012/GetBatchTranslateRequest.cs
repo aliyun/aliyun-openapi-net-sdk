0eb9ff373b3630c8bd958cb447e32d225b51e78b
sdk-team <sdk-team@alibabacloud.com>
2021-01-27 07:24:08 +0000

    Add New BatchTranslate API.

---
+++ aliyun-net-sdk-alimt/Alimt/Model/V20181012/GetBatchTranslateRequest.cs
@@ -0,0 +1,143 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core;
+using Aliyun.Acs.Core.Http;
+using Aliyun.Acs.Core.Transform;
+using Aliyun.Acs.Core.Utils;
+using Aliyun.Acs.alimt.Transform;
+using Aliyun.Acs.alimt.Transform.V20181012;
+
+namespace Aliyun.Acs.alimt.Model.V20181012
+{
+    public class GetBatchTranslateRequest : RpcAcsRequest<GetBatchTranslateResponse>
+    {
+        public GetBatchTranslateRequest()
+            : base("alimt", "2018-10-12", "GetBatchTranslate", "alimt", "openAPI")
+        {
+            if (this.GetType().GetProperty("ProductEndpointMap") != null && this.GetType().GetProperty("ProductEndpointType") != null)
+            {
+                this.GetType().GetProperty("ProductEndpointMap").SetValue(this, Aliyun.Acs.alimt.Endpoint.endpointMap, null);
+                this.GetType().GetProperty("ProductEndpointType").SetValue(this, Aliyun.Acs.alimt.Endpoint.endpointRegionalType, null);
+            }
+			Method = MethodType.POST;
+        }
+
+		private string sourceLanguage;
+
+		private string sourceText;
+
+		private string formatType;
+
+		private string apiType;
+
+		private string scene;
+
+		private string targetLanguage;
+
+		public string SourceLanguage
+		{
+			get
+			{
+				return sourceLanguage;
+			}
+			set	
+			{
+				sourceLanguage = value;
+				DictionaryUtil.Add(BodyParameters, "SourceLanguage", value);
+			}
+		}
+
+		public string SourceText
+		{
+			get
+			{
+				return sourceText;
+			}
+			set	
+			{
+				sourceText = value;
+				DictionaryUtil.Add(BodyParameters, "SourceText", value);
+			}
+		}
+
+		public string FormatType
+		{
+			get
+			{
+				return formatType;
+			}
+			set	
+			{
+				formatType = value;
+				DictionaryUtil.Add(BodyParameters, "FormatType", value);
+			}
+		}
+
+		public string ApiType
+		{
+			get
+			{
+				return apiType;
+			}
+			set	
+			{
+				apiType = value;
+				DictionaryUtil.Add(BodyParameters, "ApiType", value);
+			}
+		}
+
+		public string Scene
+		{
+			get
+			{
+				return scene;
+			}
+			set	
+			{
+				scene = value;
+				DictionaryUtil.Add(BodyParameters, "Scene", value);
+			}
+		}
+
+		public string TargetLanguage
+		{
+			get
+			{
+				return targetLanguage;
+			}
+			set	
+			{
+				targetLanguage = value;
+				DictionaryUtil.Add(BodyParameters, "TargetLanguage", value);
+			}
+		}
+
+		public override bool CheckShowJsonItemName()
+		{
+			return false;
+		}
+
+        public override GetBatchTranslateResponse GetResponse(UnmarshallerContext unmarshallerContext)
+        {
+            return GetBatchTranslateResponseUnmarshaller.Unmarshall(unmarshallerContext);
+        }
+    }
+}
---
+++ aliyun-net-sdk-alimt/Alimt/Transform/V20181012/GetBatchTranslateResponseUnmarshaller.cs
@@ -0,0 +1,56 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System;
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core.Transform;
+using Aliyun.Acs.alimt.Model.V20181012;
+
+namespace Aliyun.Acs.alimt.Transform.V20181012
+{
+    public class GetBatchTranslateResponseUnmarshaller
+    {
+        public static GetBatchTranslateResponse Unmarshall(UnmarshallerContext _ctx)
+        {
+			GetBatchTranslateResponse getBatchTranslateResponse = new GetBatchTranslateResponse();
+
+			getBatchTranslateResponse.HttpResponse = _ctx.HttpResponse;
+			getBatchTranslateResponse.RequestId = _ctx.StringValue("GetBatchTranslate.RequestId");
+			getBatchTranslateResponse.Code = _ctx.IntegerValue("GetBatchTranslate.Code");
+			getBatchTranslateResponse.Message = _ctx.StringValue("GetBatchTranslate.Message");
+
+			List<Dictionary<string, string>> getBatchTranslateResponse_translatedList = new List<Dictionary<string, string>>();
+			for (int i = 0; i < _ctx.Length("GetBatchTranslate.TranslatedList.Length"); i++) {
+				Dictionary<string, string> tmp = new Dictionary<string, string>() { };
+				foreach (var _item in _ctx.ResponseDictionary){
+					string prefix = "GetBatchTranslate.TranslatedList["+ i +"].";
+					if (_item.Key.IndexOf(prefix) == 0){
+						tmp.Add(_item.Key.Substring(prefix.Length), _item.Value);
+					}
+				}
+				if (tmp.Count > 0){
+					getBatchTranslateResponse_translatedList.Add(tmp);
+				}
+			}
+			getBatchTranslateResponse.TranslatedList = getBatchTranslateResponse_translatedList;
+        
+			return getBatchTranslateResponse;
+        }
+    }
+}
