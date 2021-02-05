f52971efa902d48f1b17d82ef49706bcbe61fcfc
sdk-team <sdk-team@alibabacloud.com>
2021-02-05 02:30:17 +0000

    Generated 2020-05-18 for dataworks-public.

---
+++ aliyun-net-sdk-dataworks-public/Dataworks_public/Model/V20200518/CreateBusinessRequest.cs
@@ -0,0 +1,144 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core;
+using Aliyun.Acs.Core.Http;
+using Aliyun.Acs.Core.Transform;
+using Aliyun.Acs.Core.Utils;
+using Aliyun.Acs.dataworks_public;
+using Aliyun.Acs.dataworks_public.Transform;
+using Aliyun.Acs.dataworks_public.Transform.V20200518;
+
+namespace Aliyun.Acs.dataworks_public.Model.V20200518
+{
+    public class CreateBusinessRequest : RpcAcsRequest<CreateBusinessResponse>
+    {
+        public CreateBusinessRequest()
+            : base("dataworks-public", "2020-05-18", "CreateBusiness")
+        {
+            if (this.GetType().GetProperty("ProductEndpointMap") != null && this.GetType().GetProperty("ProductEndpointType") != null)
+            {
+                this.GetType().GetProperty("ProductEndpointMap").SetValue(this, Aliyun.Acs.dataworks_public.Endpoint.endpointMap, null);
+                this.GetType().GetProperty("ProductEndpointType").SetValue(this, Aliyun.Acs.dataworks_public.Endpoint.endpointRegionalType, null);
+            }
+			Method = MethodType.POST;
+        }
+
+		private string owner;
+
+		private string businessName;
+
+		private string description;
+
+		private long? projectId;
+
+		private string useType;
+
+		private string projectIdentifier;
+
+		public string Owner
+		{
+			get
+			{
+				return owner;
+			}
+			set	
+			{
+				owner = value;
+				DictionaryUtil.Add(BodyParameters, "Owner", value);
+			}
+		}
+
+		public string BusinessName
+		{
+			get
+			{
+				return businessName;
+			}
+			set	
+			{
+				businessName = value;
+				DictionaryUtil.Add(BodyParameters, "BusinessName", value);
+			}
+		}
+
+		public string Description
+		{
+			get
+			{
+				return description;
+			}
+			set	
+			{
+				description = value;
+				DictionaryUtil.Add(BodyParameters, "Description", value);
+			}
+		}
+
+		public long? ProjectId
+		{
+			get
+			{
+				return projectId;
+			}
+			set	
+			{
+				projectId = value;
+				DictionaryUtil.Add(BodyParameters, "ProjectId", value.ToString());
+			}
+		}
+
+		public string UseType
+		{
+			get
+			{
+				return useType;
+			}
+			set	
+			{
+				useType = value;
+				DictionaryUtil.Add(BodyParameters, "UseType", value);
+			}
+		}
+
+		public string ProjectIdentifier
+		{
+			get
+			{
+				return projectIdentifier;
+			}
+			set	
+			{
+				projectIdentifier = value;
+				DictionaryUtil.Add(BodyParameters, "ProjectIdentifier", value);
+			}
+		}
+
+		public override bool CheckShowJsonItemName()
+		{
+			return false;
+		}
+
+        public override CreateBusinessResponse GetResponse(UnmarshallerContext unmarshallerContext)
+        {
+            return CreateBusinessResponseUnmarshaller.Unmarshall(unmarshallerContext);
+        }
+    }
+}
