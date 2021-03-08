86e9d5e00e1956679bb533c82ff7c10bf3caf33e
sdk-team <sdk-team@alibabacloud.com>
2021-03-08 03:22:34 +0000

    Generated 2020-05-18 for dataworks-public.

---
+++ aliyun-net-sdk-dataworks-public/Dataworks_public/Transform/V20200518/GetMigrationProcessResponseUnmarshaller.cs
@@ -0,0 +1,53 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System;
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core.Transform;
+using Aliyun.Acs.dataworks_public.Model.V20200518;
+
+namespace Aliyun.Acs.dataworks_public.Transform.V20200518
+{
+    public class GetMigrationProcessResponseUnmarshaller
+    {
+        public static GetMigrationProcessResponse Unmarshall(UnmarshallerContext _ctx)
+        {
+			GetMigrationProcessResponse getMigrationProcessResponse = new GetMigrationProcessResponse();
+
+			getMigrationProcessResponse.HttpResponse = _ctx.HttpResponse;
+			getMigrationProcessResponse.ErrorCode = _ctx.StringValue("GetMigrationProcess.ErrorCode");
+			getMigrationProcessResponse.ErrorMessage = _ctx.StringValue("GetMigrationProcess.ErrorMessage");
+			getMigrationProcessResponse.HttpStatusCode = _ctx.IntegerValue("GetMigrationProcess.HttpStatusCode");
+			getMigrationProcessResponse.RequestId = _ctx.StringValue("GetMigrationProcess.RequestId");
+			getMigrationProcessResponse.Success = _ctx.BooleanValue("GetMigrationProcess.Success");
+
+			List<GetMigrationProcessResponse.GetMigrationProcess_ProgressTaskItem> getMigrationProcessResponse_data = new List<GetMigrationProcessResponse.GetMigrationProcess_ProgressTaskItem>();
+			for (int i = 0; i < _ctx.Length("GetMigrationProcess.Data.Length"); i++) {
+				GetMigrationProcessResponse.GetMigrationProcess_ProgressTaskItem progressTaskItem = new GetMigrationProcessResponse.GetMigrationProcess_ProgressTaskItem();
+				progressTaskItem.TaskName = _ctx.StringValue("GetMigrationProcess.Data["+ i +"].TaskName");
+				progressTaskItem.TaskStatus = _ctx.StringValue("GetMigrationProcess.Data["+ i +"].TaskStatus");
+
+				getMigrationProcessResponse_data.Add(progressTaskItem);
+			}
+			getMigrationProcessResponse.Data = getMigrationProcessResponse_data;
+        
+			return getMigrationProcessResponse;
+        }
+    }
+}
---
+++ aliyun-net-sdk-dataworks-public/Dataworks_public/Transform/V20200518/StartMigrationResponseUnmarshaller.cs
@@ -0,0 +1,44 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System;
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core.Transform;
+using Aliyun.Acs.dataworks_public.Model.V20200518;
+
+namespace Aliyun.Acs.dataworks_public.Transform.V20200518
+{
+    public class StartMigrationResponseUnmarshaller
+    {
+        public static StartMigrationResponse Unmarshall(UnmarshallerContext _ctx)
+        {
+			StartMigrationResponse startMigrationResponse = new StartMigrationResponse();
+
+			startMigrationResponse.HttpResponse = _ctx.HttpResponse;
+			startMigrationResponse.ErrorCode = _ctx.StringValue("StartMigration.ErrorCode");
+			startMigrationResponse.ErrorMessage = _ctx.StringValue("StartMigration.ErrorMessage");
+			startMigrationResponse.HttpStatusCode = _ctx.IntegerValue("StartMigration.HttpStatusCode");
+			startMigrationResponse.RequestId = _ctx.StringValue("StartMigration.RequestId");
+			startMigrationResponse.Success = _ctx.BooleanValue("StartMigration.Success");
+			startMigrationResponse.Data = _ctx.BooleanValue("StartMigration.Data");
+        
+			return startMigrationResponse;
+        }
+    }
+}
