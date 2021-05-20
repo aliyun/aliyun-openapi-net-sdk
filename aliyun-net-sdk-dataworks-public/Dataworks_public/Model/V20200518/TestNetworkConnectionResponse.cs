0d6ed3767fc50978efaeeb007ddff785d193629a
sdk-team <sdk-team@alibabacloud.com>
2021-05-20 07:26:19 +0000

    Generated 2020-05-18 for dataworks-public.

---
+++ aliyun-net-sdk-dataworks-public/Dataworks_public/Model/V20200518/TestNetworkConnectionResponse.cs
@@ -0,0 +1,102 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core;
+
+namespace Aliyun.Acs.dataworks_public.Model.V20200518
+{
+	public class TestNetworkConnectionResponse : AcsResponse
+	{
+
+		private string requestId;
+
+		private bool? success;
+
+		private TestNetworkConnection_TaskList taskList;
+
+		public string RequestId
+		{
+			get
+			{
+				return requestId;
+			}
+			set	
+			{
+				requestId = value;
+			}
+		}
+
+		public bool? Success
+		{
+			get
+			{
+				return success;
+			}
+			set	
+			{
+				success = value;
+			}
+		}
+
+		public TestNetworkConnection_TaskList TaskList
+		{
+			get
+			{
+				return taskList;
+			}
+			set	
+			{
+				taskList = value;
+			}
+		}
+
+		public class TestNetworkConnection_TaskList
+		{
+
+			private bool? connectStatus;
+
+			private string connectMessage;
+
+			public bool? ConnectStatus
+			{
+				get
+				{
+					return connectStatus;
+				}
+				set	
+				{
+					connectStatus = value;
+				}
+			}
+
+			public string ConnectMessage
+			{
+				get
+				{
+					return connectMessage;
+				}
+				set	
+				{
+					connectMessage = value;
+				}
+			}
+		}
+	}
+}
