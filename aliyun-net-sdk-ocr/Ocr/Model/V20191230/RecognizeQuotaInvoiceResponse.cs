a3e276598ede3cfc44792010339145be30eb2878
sdk-team <sdk-team@alibabacloud.com>
2021-07-02 03:25:44 +0000

    Release RecognizeQuotaInvoice RecognizeTicketInvoice RecognizePdf.

---
+++ aliyun-net-sdk-ocr/Ocr/Model/V20191230/RecognizeQuotaInvoiceResponse.cs
@@ -0,0 +1,352 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core;
+
+namespace Aliyun.Acs.ocr.Model.V20191230
+{
+	public class RecognizeQuotaInvoiceResponse : AcsResponse
+	{
+
+		private string requestId;
+
+		private string code;
+
+		private string message;
+
+		private RecognizeQuotaInvoice_Data data;
+
+		public string RequestId
+		{
+			get
+			{
+				return requestId;
+			}
+			set	
+			{
+				requestId = value;
+			}
+		}
+
+		public string Code
+		{
+			get
+			{
+				return code;
+			}
+			set	
+			{
+				code = value;
+			}
+		}
+
+		public string Message
+		{
+			get
+			{
+				return message;
+			}
+			set	
+			{
+				message = value;
+			}
+		}
+
+		public RecognizeQuotaInvoice_Data Data
+		{
+			get
+			{
+				return data;
+			}
+			set	
+			{
+				data = value;
+			}
+		}
+
+		public class RecognizeQuotaInvoice_Data
+		{
+
+			private long? angle;
+
+			private long? height;
+
+			private long? width;
+
+			private long? orgHeight;
+
+			private long? orgWidth;
+
+			private List<RecognizeQuotaInvoice_KeyValueInfosItem> keyValueInfos;
+
+			private RecognizeQuotaInvoice_Content content;
+
+			public long? Angle
+			{
+				get
+				{
+					return angle;
+				}
+				set	
+				{
+					angle = value;
+				}
+			}
+
+			public long? Height
+			{
+				get
+				{
+					return height;
+				}
+				set	
+				{
+					height = value;
+				}
+			}
+
+			public long? Width
+			{
+				get
+				{
+					return width;
+				}
+				set	
+				{
+					width = value;
+				}
+			}
+
+			public long? OrgHeight
+			{
+				get
+				{
+					return orgHeight;
+				}
+				set	
+				{
+					orgHeight = value;
+				}
+			}
+
+			public long? OrgWidth
+			{
+				get
+				{
+					return orgWidth;
+				}
+				set	
+				{
+					orgWidth = value;
+				}
+			}
+
+			public List<RecognizeQuotaInvoice_KeyValueInfosItem> KeyValueInfos
+			{
+				get
+				{
+					return keyValueInfos;
+				}
+				set	
+				{
+					keyValueInfos = value;
+				}
+			}
+
+			public RecognizeQuotaInvoice_Content Content
+			{
+				get
+				{
+					return content;
+				}
+				set	
+				{
+					content = value;
+				}
+			}
+
+			public class RecognizeQuotaInvoice_KeyValueInfosItem
+			{
+
+				private string key;
+
+				private string _value;
+
+				private float? valueScore;
+
+				private List<RecognizeQuotaInvoice_ValuePositionsItem> valuePositions;
+
+				public string Key
+				{
+					get
+					{
+						return key;
+					}
+					set	
+					{
+						key = value;
+					}
+				}
+
+				public string _Value
+				{
+					get
+					{
+						return _value;
+					}
+					set	
+					{
+						_value = value;
+					}
+				}
+
+				public float? ValueScore
+				{
+					get
+					{
+						return valueScore;
+					}
+					set	
+					{
+						valueScore = value;
+					}
+				}
+
+				public List<RecognizeQuotaInvoice_ValuePositionsItem> ValuePositions
+				{
+					get
+					{
+						return valuePositions;
+					}
+					set	
+					{
+						valuePositions = value;
+					}
+				}
+
+				public class RecognizeQuotaInvoice_ValuePositionsItem
+				{
+
+					private long? x;
+
+					private long? y;
+
+					public long? X
+					{
+						get
+						{
+							return x;
+						}
+						set	
+						{
+							x = value;
+						}
+					}
+
+					public long? Y
+					{
+						get
+						{
+							return y;
+						}
+						set	
+						{
+							y = value;
+						}
+					}
+				}
+			}
+
+			public class RecognizeQuotaInvoice_Content
+			{
+
+				private string sumAmount;
+
+				private string invoiceCode;
+
+				private string invoiceNo;
+
+				private string invoiceAmount;
+
+				private string invoiceDetails;
+
+				public string SumAmount
+				{
+					get
+					{
+						return sumAmount;
+					}
+					set	
+					{
+						sumAmount = value;
+					}
+				}
+
+				public string InvoiceCode
+				{
+					get
+					{
+						return invoiceCode;
+					}
+					set	
+					{
+						invoiceCode = value;
+					}
+				}
+
+				public string InvoiceNo
+				{
+					get
+					{
+						return invoiceNo;
+					}
+					set	
+					{
+						invoiceNo = value;
+					}
+				}
+
+				public string InvoiceAmount
+				{
+					get
+					{
+						return invoiceAmount;
+					}
+					set	
+					{
+						invoiceAmount = value;
+					}
+				}
+
+				public string InvoiceDetails
+				{
+					get
+					{
+						return invoiceDetails;
+					}
+					set	
+					{
+						invoiceDetails = value;
+					}
+				}
+			}
+		}
+	}
+}
---
+++ aliyun-net-sdk-ocr/Ocr/Transform/V20191230/RecognizeQuotaInvoiceResponseUnmarshaller.cs
@@ -0,0 +1,78 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System;
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core.Transform;
+using Aliyun.Acs.ocr.Model.V20191230;
+
+namespace Aliyun.Acs.ocr.Transform.V20191230
+{
+    public class RecognizeQuotaInvoiceResponseUnmarshaller
+    {
+        public static RecognizeQuotaInvoiceResponse Unmarshall(UnmarshallerContext _ctx)
+        {
+			RecognizeQuotaInvoiceResponse recognizeQuotaInvoiceResponse = new RecognizeQuotaInvoiceResponse();
+
+			recognizeQuotaInvoiceResponse.HttpResponse = _ctx.HttpResponse;
+			recognizeQuotaInvoiceResponse.RequestId = _ctx.StringValue("RecognizeQuotaInvoice.RequestId");
+			recognizeQuotaInvoiceResponse.Code = _ctx.StringValue("RecognizeQuotaInvoice.Code");
+			recognizeQuotaInvoiceResponse.Message = _ctx.StringValue("RecognizeQuotaInvoice.Message");
+
+			RecognizeQuotaInvoiceResponse.RecognizeQuotaInvoice_Data data = new RecognizeQuotaInvoiceResponse.RecognizeQuotaInvoice_Data();
+			data.Angle = _ctx.LongValue("RecognizeQuotaInvoice.Data.Angle");
+			data.Height = _ctx.LongValue("RecognizeQuotaInvoice.Data.Height");
+			data.Width = _ctx.LongValue("RecognizeQuotaInvoice.Data.Width");
+			data.OrgHeight = _ctx.LongValue("RecognizeQuotaInvoice.Data.OrgHeight");
+			data.OrgWidth = _ctx.LongValue("RecognizeQuotaInvoice.Data.OrgWidth");
+
+			RecognizeQuotaInvoiceResponse.RecognizeQuotaInvoice_Data.RecognizeQuotaInvoice_Content content = new RecognizeQuotaInvoiceResponse.RecognizeQuotaInvoice_Data.RecognizeQuotaInvoice_Content();
+			content.SumAmount = _ctx.StringValue("RecognizeQuotaInvoice.Data.Content.SumAmount");
+			content.InvoiceCode = _ctx.StringValue("RecognizeQuotaInvoice.Data.Content.InvoiceCode");
+			content.InvoiceNo = _ctx.StringValue("RecognizeQuotaInvoice.Data.Content.InvoiceNo");
+			content.InvoiceAmount = _ctx.StringValue("RecognizeQuotaInvoice.Data.Content.InvoiceAmount");
+			content.InvoiceDetails = _ctx.StringValue("RecognizeQuotaInvoice.Data.Content.InvoiceDetails");
+			data.Content = content;
+
+			List<RecognizeQuotaInvoiceResponse.RecognizeQuotaInvoice_Data.RecognizeQuotaInvoice_KeyValueInfosItem> data_keyValueInfos = new List<RecognizeQuotaInvoiceResponse.RecognizeQuotaInvoice_Data.RecognizeQuotaInvoice_KeyValueInfosItem>();
+			for (int i = 0; i < _ctx.Length("RecognizeQuotaInvoice.Data.KeyValueInfos.Length"); i++) {
+				RecognizeQuotaInvoiceResponse.RecognizeQuotaInvoice_Data.RecognizeQuotaInvoice_KeyValueInfosItem keyValueInfosItem = new RecognizeQuotaInvoiceResponse.RecognizeQuotaInvoice_Data.RecognizeQuotaInvoice_KeyValueInfosItem();
+				keyValueInfosItem.Key = _ctx.StringValue("RecognizeQuotaInvoice.Data.KeyValueInfos["+ i +"].Key");
+				keyValueInfosItem._Value = _ctx.StringValue("RecognizeQuotaInvoice.Data.KeyValueInfos["+ i +"].Value");
+				keyValueInfosItem.ValueScore = _ctx.FloatValue("RecognizeQuotaInvoice.Data.KeyValueInfos["+ i +"].ValueScore");
+
+				List<RecognizeQuotaInvoiceResponse.RecognizeQuotaInvoice_Data.RecognizeQuotaInvoice_KeyValueInfosItem.RecognizeQuotaInvoice_ValuePositionsItem> keyValueInfosItem_valuePositions = new List<RecognizeQuotaInvoiceResponse.RecognizeQuotaInvoice_Data.RecognizeQuotaInvoice_KeyValueInfosItem.RecognizeQuotaInvoice_ValuePositionsItem>();
+				for (int j = 0; j < _ctx.Length("RecognizeQuotaInvoice.Data.KeyValueInfos["+ i +"].ValuePositions.Length"); j++) {
+					RecognizeQuotaInvoiceResponse.RecognizeQuotaInvoice_Data.RecognizeQuotaInvoice_KeyValueInfosItem.RecognizeQuotaInvoice_ValuePositionsItem valuePositionsItem = new RecognizeQuotaInvoiceResponse.RecognizeQuotaInvoice_Data.RecognizeQuotaInvoice_KeyValueInfosItem.RecognizeQuotaInvoice_ValuePositionsItem();
+					valuePositionsItem.X = _ctx.LongValue("RecognizeQuotaInvoice.Data.KeyValueInfos["+ i +"].ValuePositions["+ j +"].X");
+					valuePositionsItem.Y = _ctx.LongValue("RecognizeQuotaInvoice.Data.KeyValueInfos["+ i +"].ValuePositions["+ j +"].Y");
+
+					keyValueInfosItem_valuePositions.Add(valuePositionsItem);
+				}
+				keyValueInfosItem.ValuePositions = keyValueInfosItem_valuePositions;
+
+				data_keyValueInfos.Add(keyValueInfosItem);
+			}
+			data.KeyValueInfos = data_keyValueInfos;
+			recognizeQuotaInvoiceResponse.Data = data;
+        
+			return recognizeQuotaInvoiceResponse;
+        }
+    }
+}
