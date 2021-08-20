636ef2bd9db1040025afa3d712661b08351ef34a
sdk-team <sdk-team@alibabacloud.com>
2021-08-20 06:56:54 +0000

    Update Dyvmsapi SDK.

---
+++ aliyun-net-sdk-dyvmsapi/Dyvmsapi/Transform/V20170525/GetCallInfoResponseUnmarshaller.cs
@@ -0,0 +1,46 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System;
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core.Transform;
+using Aliyun.Acs.Dyvmsapi.Model.V20170525;
+
+namespace Aliyun.Acs.Dyvmsapi.Transform.V20170525
+{
+    public class GetCallInfoResponseUnmarshaller
+    {
+        public static GetCallInfoResponse Unmarshall(UnmarshallerContext _ctx)
+        {
+			GetCallInfoResponse getCallInfoResponse = new GetCallInfoResponse();
+
+			getCallInfoResponse.HttpResponse = _ctx.HttpResponse;
+			getCallInfoResponse.Code = _ctx.StringValue("GetCallInfo.Code");
+			getCallInfoResponse.Message = _ctx.StringValue("GetCallInfo.Message");
+			getCallInfoResponse.RequestId = _ctx.StringValue("GetCallInfo.RequestId");
+
+			GetCallInfoResponse.GetCallInfo_Data data = new GetCallInfoResponse.GetCallInfo_Data();
+			data.ChannelId = _ctx.StringValue("GetCallInfo.Data.ChannelId");
+			data.Status = _ctx.StringValue("GetCallInfo.Data.Status");
+			getCallInfoResponse.Data = data;
+        
+			return getCallInfoResponse;
+        }
+    }
+}
