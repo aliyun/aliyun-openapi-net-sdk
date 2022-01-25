0af6750ef3095a05351d1dea6fe51af4ca1c09ea
sdk-team <sdk-team@alibabacloud.com>
2022-01-25 12:08:20 +0000

    Add APIs, include, ListSharedReports, ListFavoriteReports, ListRecentViewReports, CreateTicket, QueryTicketInfo, UpdateTicketNum, DelayTicketExpireTime, DelayTicketExpireTime.

---
+++ aliyun-net-sdk-quickbi-public/Quickbi_public/Model/V20200809/ListSharedReportsResponse.cs
@@ -0,0 +1,330 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core;
+
+namespace Aliyun.Acs.quickbi_public.Model.V20200809
+{
+	public class ListSharedReportsResponse : AcsResponse
+	{
+
+		private string requestId;
+
+		private bool? success;
+
+		private ListSharedReports_Result result;
+
+		public string RequestId
+		{
+			get
+			{
+				return requestId;
+			}
+			set	
+			{
+				requestId = value;
+			}
+		}
+
+		public bool? Success
+		{
+			get
+			{
+				return success;
+			}
+			set	
+			{
+				success = value;
+			}
+		}
+
+		public ListSharedReports_Result Result
+		{
+			get
+			{
+				return result;
+			}
+			set	
+			{
+				result = value;
+			}
+		}
+
+		public class ListSharedReports_Result
+		{
+
+			private int? totalNum;
+
+			private int? totalPages;
+
+			private int? pageNum;
+
+			private int? pageSize;
+
+			private List<ListSharedReports_DataItem> data;
+
+			public int? TotalNum
+			{
+				get
+				{
+					return totalNum;
+				}
+				set	
+				{
+					totalNum = value;
+				}
+			}
+
+			public int? TotalPages
+			{
+				get
+				{
+					return totalPages;
+				}
+				set	
+				{
+					totalPages = value;
+				}
+			}
+
+			public int? PageNum
+			{
+				get
+				{
+					return pageNum;
+				}
+				set	
+				{
+					pageNum = value;
+				}
+			}
+
+			public int? PageSize
+			{
+				get
+				{
+					return pageSize;
+				}
+				set	
+				{
+					pageSize = value;
+				}
+			}
+
+			public List<ListSharedReports_DataItem> Data
+			{
+				get
+				{
+					return data;
+				}
+				set	
+				{
+					data = value;
+				}
+			}
+
+			public class ListSharedReports_DataItem
+			{
+
+				private string treeId;
+
+				private string type;
+
+				private string name;
+
+				private string ownerNum;
+
+				private string ownerName;
+
+				private string gmtCreate;
+
+				private string gmtModified;
+
+				private int? publishStatus;
+
+				private bool? favorite;
+
+				private bool? hasViewAuth;
+
+				private bool? hasEditAuth;
+
+				private string workspaceId;
+
+				private string workspaceName;
+
+				public string TreeId
+				{
+					get
+					{
+						return treeId;
+					}
+					set	
+					{
+						treeId = value;
+					}
+				}
+
+				public string Type
+				{
+					get
+					{
+						return type;
+					}
+					set	
+					{
+						type = value;
+					}
+				}
+
+				public string Name
+				{
+					get
+					{
+						return name;
+					}
+					set	
+					{
+						name = value;
+					}
+				}
+
+				public string OwnerNum
+				{
+					get
+					{
+						return ownerNum;
+					}
+					set	
+					{
+						ownerNum = value;
+					}
+				}
+
+				public string OwnerName
+				{
+					get
+					{
+						return ownerName;
+					}
+					set	
+					{
+						ownerName = value;
+					}
+				}
+
+				public string GmtCreate
+				{
+					get
+					{
+						return gmtCreate;
+					}
+					set	
+					{
+						gmtCreate = value;
+					}
+				}
+
+				public string GmtModified
+				{
+					get
+					{
+						return gmtModified;
+					}
+					set	
+					{
+						gmtModified = value;
+					}
+				}
+
+				public int? PublishStatus
+				{
+					get
+					{
+						return publishStatus;
+					}
+					set	
+					{
+						publishStatus = value;
+					}
+				}
+
+				public bool? Favorite
+				{
+					get
+					{
+						return favorite;
+					}
+					set	
+					{
+						favorite = value;
+					}
+				}
+
+				public bool? HasViewAuth
+				{
+					get
+					{
+						return hasViewAuth;
+					}
+					set	
+					{
+						hasViewAuth = value;
+					}
+				}
+
+				public bool? HasEditAuth
+				{
+					get
+					{
+						return hasEditAuth;
+					}
+					set	
+					{
+						hasEditAuth = value;
+					}
+				}
+
+				public string WorkspaceId
+				{
+					get
+					{
+						return workspaceId;
+					}
+					set	
+					{
+						workspaceId = value;
+					}
+				}
+
+				public string WorkspaceName
+				{
+					get
+					{
+						return workspaceName;
+					}
+					set	
+					{
+						workspaceName = value;
+					}
+				}
+			}
+		}
+	}
+}
---
+++ aliyun-net-sdk-quickbi-public/Quickbi_public/Transform/V20210325/QueryTicketInfoResponseUnmarshaller.cs
@@ -0,0 +1,54 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System;
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core.Transform;
+using Aliyun.Acs.quickbi_public.Model.V20210325;
+
+namespace Aliyun.Acs.quickbi_public.Transform.V20210325
+{
+    public class QueryTicketInfoResponseUnmarshaller
+    {
+        public static QueryTicketInfoResponse Unmarshall(UnmarshallerContext _ctx)
+        {
+			QueryTicketInfoResponse queryTicketInfoResponse = new QueryTicketInfoResponse();
+
+			queryTicketInfoResponse.HttpResponse = _ctx.HttpResponse;
+			queryTicketInfoResponse.RequestId = _ctx.StringValue("QueryTicketInfo.RequestId");
+			queryTicketInfoResponse.Success = _ctx.BooleanValue("QueryTicketInfo.Success");
+
+			QueryTicketInfoResponse.QueryTicketInfo_Result result = new QueryTicketInfoResponse.QueryTicketInfo_Result();
+			result.AccessTicket = _ctx.StringValue("QueryTicketInfo.Result.AccessTicket");
+			result.OrganizationId = _ctx.StringValue("QueryTicketInfo.Result.OrganizationId");
+			result.UserId = _ctx.StringValue("QueryTicketInfo.Result.UserId");
+			result.UsedTicketNum = _ctx.IntegerValue("QueryTicketInfo.Result.UsedTicketNum");
+			result.MaxTicketNum = _ctx.IntegerValue("QueryTicketInfo.Result.MaxTicketNum");
+			result.RegisterTime = _ctx.StringValue("QueryTicketInfo.Result.RegisterTime");
+			result.InvalidTime = _ctx.StringValue("QueryTicketInfo.Result.InvalidTime");
+			result.WorksId = _ctx.StringValue("QueryTicketInfo.Result.WorksId");
+			result.CmptId = _ctx.StringValue("QueryTicketInfo.Result.CmptId");
+			result.GlobalParam = _ctx.StringValue("QueryTicketInfo.Result.GlobalParam");
+			result.WatermarkParam = _ctx.StringValue("QueryTicketInfo.Result.WatermarkParam");
+			queryTicketInfoResponse.Result = result;
+        
+			return queryTicketInfoResponse;
+        }
+    }
+}
