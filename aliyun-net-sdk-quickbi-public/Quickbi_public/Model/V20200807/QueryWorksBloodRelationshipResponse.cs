663371647fad6de0b0b85c602d83b09bb64d89c9
sdk-team <sdk-team@alibabacloud.com>
2022-02-09 02:59:21 +0000

    Added api QueryWorksBloodRelationship.

---
+++ aliyun-net-sdk-quickbi-public/Quickbi_public/Model/V20200807/QueryWorksBloodRelationshipResponse.cs
@@ -0,0 +1,260 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core;
+
+namespace Aliyun.Acs.quickbi_public.Model.V20200807
+{
+	public class QueryWorksBloodRelationshipResponse : AcsResponse
+	{
+
+		private bool? success;
+
+		private string requestId;
+
+		private List<QueryWorksBloodRelationship_Item> result;
+
+		public bool? Success
+		{
+			get
+			{
+				return success;
+			}
+			set	
+			{
+				success = value;
+			}
+		}
+
+		public string RequestId
+		{
+			get
+			{
+				return requestId;
+			}
+			set	
+			{
+				requestId = value;
+			}
+		}
+
+		public List<QueryWorksBloodRelationship_Item> Result
+		{
+			get
+			{
+				return result;
+			}
+			set	
+			{
+				result = value;
+			}
+		}
+
+		public class QueryWorksBloodRelationship_Item
+		{
+
+			private string componentId;
+
+			private string componentName;
+
+			private int? componentType;
+
+			private string componentTypeName;
+
+			private string datasetId;
+
+			private List<QueryWorksBloodRelationship_QueryParam> queryParams;
+
+			public string ComponentId
+			{
+				get
+				{
+					return componentId;
+				}
+				set	
+				{
+					componentId = value;
+				}
+			}
+
+			public string ComponentName
+			{
+				get
+				{
+					return componentName;
+				}
+				set	
+				{
+					componentName = value;
+				}
+			}
+
+			public int? ComponentType
+			{
+				get
+				{
+					return componentType;
+				}
+				set	
+				{
+					componentType = value;
+				}
+			}
+
+			public string ComponentTypeName
+			{
+				get
+				{
+					return componentTypeName;
+				}
+				set	
+				{
+					componentTypeName = value;
+				}
+			}
+
+			public string DatasetId
+			{
+				get
+				{
+					return datasetId;
+				}
+				set	
+				{
+					datasetId = value;
+				}
+			}
+
+			public List<QueryWorksBloodRelationship_QueryParam> QueryParams
+			{
+				get
+				{
+					return queryParams;
+				}
+				set	
+				{
+					queryParams = value;
+				}
+			}
+
+			public class QueryWorksBloodRelationship_QueryParam
+			{
+
+				private string uid;
+
+				private string pathId;
+
+				private string caption;
+
+				private string dataType;
+
+				private bool? isMeasure;
+
+				private string areaId;
+
+				private string areaName;
+
+				public string Uid
+				{
+					get
+					{
+						return uid;
+					}
+					set	
+					{
+						uid = value;
+					}
+				}
+
+				public string PathId
+				{
+					get
+					{
+						return pathId;
+					}
+					set	
+					{
+						pathId = value;
+					}
+				}
+
+				public string Caption
+				{
+					get
+					{
+						return caption;
+					}
+					set	
+					{
+						caption = value;
+					}
+				}
+
+				public string DataType
+				{
+					get
+					{
+						return dataType;
+					}
+					set	
+					{
+						dataType = value;
+					}
+				}
+
+				public bool? IsMeasure
+				{
+					get
+					{
+						return isMeasure;
+					}
+					set	
+					{
+						isMeasure = value;
+					}
+				}
+
+				public string AreaId
+				{
+					get
+					{
+						return areaId;
+					}
+					set	
+					{
+						areaId = value;
+					}
+				}
+
+				public string AreaName
+				{
+					get
+					{
+						return areaName;
+					}
+					set	
+					{
+						areaName = value;
+					}
+				}
+			}
+		}
+	}
+}
---
+++ aliyun-net-sdk-quickbi-public/Quickbi_public/Transform/V20200807/QueryWorksBloodRelationshipResponseUnmarshaller.cs
@@ -0,0 +1,68 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System;
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core.Transform;
+using Aliyun.Acs.quickbi_public.Model.V20200807;
+
+namespace Aliyun.Acs.quickbi_public.Transform.V20200807
+{
+    public class QueryWorksBloodRelationshipResponseUnmarshaller
+    {
+        public static QueryWorksBloodRelationshipResponse Unmarshall(UnmarshallerContext _ctx)
+        {
+			QueryWorksBloodRelationshipResponse queryWorksBloodRelationshipResponse = new QueryWorksBloodRelationshipResponse();
+
+			queryWorksBloodRelationshipResponse.HttpResponse = _ctx.HttpResponse;
+			queryWorksBloodRelationshipResponse.Success = _ctx.BooleanValue("QueryWorksBloodRelationship.Success");
+			queryWorksBloodRelationshipResponse.RequestId = _ctx.StringValue("QueryWorksBloodRelationship.RequestId");
+
+			List<QueryWorksBloodRelationshipResponse.QueryWorksBloodRelationship_Item> queryWorksBloodRelationshipResponse_result = new List<QueryWorksBloodRelationshipResponse.QueryWorksBloodRelationship_Item>();
+			for (int i = 0; i < _ctx.Length("QueryWorksBloodRelationship.Result.Length"); i++) {
+				QueryWorksBloodRelationshipResponse.QueryWorksBloodRelationship_Item item = new QueryWorksBloodRelationshipResponse.QueryWorksBloodRelationship_Item();
+				item.ComponentId = _ctx.StringValue("QueryWorksBloodRelationship.Result["+ i +"].ComponentId");
+				item.ComponentName = _ctx.StringValue("QueryWorksBloodRelationship.Result["+ i +"].ComponentName");
+				item.ComponentType = _ctx.IntegerValue("QueryWorksBloodRelationship.Result["+ i +"].ComponentType");
+				item.ComponentTypeName = _ctx.StringValue("QueryWorksBloodRelationship.Result["+ i +"].ComponentTypeName");
+				item.DatasetId = _ctx.StringValue("QueryWorksBloodRelationship.Result["+ i +"].DatasetId");
+
+				List<QueryWorksBloodRelationshipResponse.QueryWorksBloodRelationship_Item.QueryWorksBloodRelationship_QueryParam> item_queryParams = new List<QueryWorksBloodRelationshipResponse.QueryWorksBloodRelationship_Item.QueryWorksBloodRelationship_QueryParam>();
+				for (int j = 0; j < _ctx.Length("QueryWorksBloodRelationship.Result["+ i +"].QueryParams.Length"); j++) {
+					QueryWorksBloodRelationshipResponse.QueryWorksBloodRelationship_Item.QueryWorksBloodRelationship_QueryParam queryParam = new QueryWorksBloodRelationshipResponse.QueryWorksBloodRelationship_Item.QueryWorksBloodRelationship_QueryParam();
+					queryParam.Uid = _ctx.StringValue("QueryWorksBloodRelationship.Result["+ i +"].QueryParams["+ j +"].Uid");
+					queryParam.PathId = _ctx.StringValue("QueryWorksBloodRelationship.Result["+ i +"].QueryParams["+ j +"].PathId");
+					queryParam.Caption = _ctx.StringValue("QueryWorksBloodRelationship.Result["+ i +"].QueryParams["+ j +"].Caption");
+					queryParam.DataType = _ctx.StringValue("QueryWorksBloodRelationship.Result["+ i +"].QueryParams["+ j +"].DataType");
+					queryParam.IsMeasure = _ctx.BooleanValue("QueryWorksBloodRelationship.Result["+ i +"].QueryParams["+ j +"].IsMeasure");
+					queryParam.AreaId = _ctx.StringValue("QueryWorksBloodRelationship.Result["+ i +"].QueryParams["+ j +"].AreaId");
+					queryParam.AreaName = _ctx.StringValue("QueryWorksBloodRelationship.Result["+ i +"].QueryParams["+ j +"].AreaName");
+
+					item_queryParams.Add(queryParam);
+				}
+				item.QueryParams = item_queryParams;
+
+				queryWorksBloodRelationshipResponse_result.Add(item);
+			}
+			queryWorksBloodRelationshipResponse.Result = queryWorksBloodRelationshipResponse_result;
+        
+			return queryWorksBloodRelationshipResponse;
+        }
+    }
+}
