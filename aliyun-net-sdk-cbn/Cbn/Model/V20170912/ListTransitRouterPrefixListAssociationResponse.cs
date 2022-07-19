0e1e6e26b5be6faabc3a56f5c9af74ccbcb50ce3
sdk-team <sdk-team@alibabacloud.com>
2022-07-19 03:20:39 +0000

    Support MatchAddressType in RouteMap API.

---
+++ aliyun-net-sdk-cbn/Cbn/Model/V20170912/ListTransitRouterPrefixListAssociationResponse.cs
@@ -0,0 +1,200 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Aliyun.Acs.Core;
+
+namespace Aliyun.Acs.Cbn.Model.V20170912
+{
+	public class ListTransitRouterPrefixListAssociationResponse : AcsResponse
+	{
+
+		private string requestId;
+
+		private int? totalCount;
+
+		private int? pageSize;
+
+		private int? pageNumber;
+
+		private List<ListTransitRouterPrefixListAssociation_PrefixList> prefixLists;
+
+		public string RequestId
+		{
+			get
+			{
+				return requestId;
+			}
+			set	
+			{
+				requestId = value;
+			}
+		}
+
+		public int? TotalCount
+		{
+			get
+			{
+				return totalCount;
+			}
+			set	
+			{
+				totalCount = value;
+			}
+		}
+
+		public int? PageSize
+		{
+			get
+			{
+				return pageSize;
+			}
+			set	
+			{
+				pageSize = value;
+			}
+		}
+
+		public int? PageNumber
+		{
+			get
+			{
+				return pageNumber;
+			}
+			set	
+			{
+				pageNumber = value;
+			}
+		}
+
+		public List<ListTransitRouterPrefixListAssociation_PrefixList> PrefixLists
+		{
+			get
+			{
+				return prefixLists;
+			}
+			set	
+			{
+				prefixLists = value;
+			}
+		}
+
+		public class ListTransitRouterPrefixListAssociation_PrefixList
+		{
+
+			private string prefixListId;
+
+			private long? ownerUid;
+
+			private string status;
+
+			private string nextHop;
+
+			private string nextHopInstanceId;
+
+			private string nextHopType;
+
+			private string transitRouterTableId;
+
+			public string PrefixListId
+			{
+				get
+				{
+					return prefixListId;
+				}
+				set	
+				{
+					prefixListId = value;
+				}
+			}
+
+			public long? OwnerUid
+			{
+				get
+				{
+					return ownerUid;
+				}
+				set	
+				{
+					ownerUid = value;
+				}
+			}
+
+			public string Status
+			{
+				get
+				{
+					return status;
+				}
+				set	
+				{
+					status = value;
+				}
+			}
+
+			public string NextHop
+			{
+				get
+				{
+					return nextHop;
+				}
+				set	
+				{
+					nextHop = value;
+				}
+			}
+
+			public string NextHopInstanceId
+			{
+				get
+				{
+					return nextHopInstanceId;
+				}
+				set	
+				{
+					nextHopInstanceId = value;
+				}
+			}
+
+			public string NextHopType
+			{
+				get
+				{
+					return nextHopType;
+				}
+				set	
+				{
+					nextHopType = value;
+				}
+			}
+
+			public string TransitRouterTableId
+			{
+				get
+				{
+					return transitRouterTableId;
+				}
+				set	
+				{
+					transitRouterTableId = value;
+				}
+			}
+		}
+	}
+}
---
+++ aliyun-net-sdk-cbn/Cbn/Transform/V20170912/ListTransitRouterPrefixListAssociationResponseUnmarshaller.cs
@@ -0,0 +1,57 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System;
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core.Transform;
+using Aliyun.Acs.Cbn.Model.V20170912;
+
+namespace Aliyun.Acs.Cbn.Transform.V20170912
+{
+    public class ListTransitRouterPrefixListAssociationResponseUnmarshaller
+    {
+        public static ListTransitRouterPrefixListAssociationResponse Unmarshall(UnmarshallerContext _ctx)
+        {
+			ListTransitRouterPrefixListAssociationResponse listTransitRouterPrefixListAssociationResponse = new ListTransitRouterPrefixListAssociationResponse();
+
+			listTransitRouterPrefixListAssociationResponse.HttpResponse = _ctx.HttpResponse;
+			listTransitRouterPrefixListAssociationResponse.RequestId = _ctx.StringValue("ListTransitRouterPrefixListAssociation.RequestId");
+			listTransitRouterPrefixListAssociationResponse.TotalCount = _ctx.IntegerValue("ListTransitRouterPrefixListAssociation.TotalCount");
+			listTransitRouterPrefixListAssociationResponse.PageSize = _ctx.IntegerValue("ListTransitRouterPrefixListAssociation.PageSize");
+			listTransitRouterPrefixListAssociationResponse.PageNumber = _ctx.IntegerValue("ListTransitRouterPrefixListAssociation.PageNumber");
+
+			List<ListTransitRouterPrefixListAssociationResponse.ListTransitRouterPrefixListAssociation_PrefixList> listTransitRouterPrefixListAssociationResponse_prefixLists = new List<ListTransitRouterPrefixListAssociationResponse.ListTransitRouterPrefixListAssociation_PrefixList>();
+			for (int i = 0; i < _ctx.Length("ListTransitRouterPrefixListAssociation.PrefixLists.Length"); i++) {
+				ListTransitRouterPrefixListAssociationResponse.ListTransitRouterPrefixListAssociation_PrefixList prefixList = new ListTransitRouterPrefixListAssociationResponse.ListTransitRouterPrefixListAssociation_PrefixList();
+				prefixList.PrefixListId = _ctx.StringValue("ListTransitRouterPrefixListAssociation.PrefixLists["+ i +"].PrefixListId");
+				prefixList.OwnerUid = _ctx.LongValue("ListTransitRouterPrefixListAssociation.PrefixLists["+ i +"].OwnerUid");
+				prefixList.Status = _ctx.StringValue("ListTransitRouterPrefixListAssociation.PrefixLists["+ i +"].Status");
+				prefixList.NextHop = _ctx.StringValue("ListTransitRouterPrefixListAssociation.PrefixLists["+ i +"].NextHop");
+				prefixList.NextHopInstanceId = _ctx.StringValue("ListTransitRouterPrefixListAssociation.PrefixLists["+ i +"].NextHopInstanceId");
+				prefixList.NextHopType = _ctx.StringValue("ListTransitRouterPrefixListAssociation.PrefixLists["+ i +"].NextHopType");
+				prefixList.TransitRouterTableId = _ctx.StringValue("ListTransitRouterPrefixListAssociation.PrefixLists["+ i +"].TransitRouterTableId");
+
+				listTransitRouterPrefixListAssociationResponse_prefixLists.Add(prefixList);
+			}
+			listTransitRouterPrefixListAssociationResponse.PrefixLists = listTransitRouterPrefixListAssociationResponse_prefixLists;
+        
+			return listTransitRouterPrefixListAssociationResponse;
+        }
+    }
+}
