7d8db4394f02ce41afce39465bb55eb99c2ded8f
sdk-team <sdk-team@alibabacloud.com>
2022-07-26 10:39:54 +0000

    Support Service of number analysis.

---
+++ aliyun-net-sdk-dytnsapi/Dytnsapi/Transform/V20200217/PhoneNumberStatusForAccountResponseUnmarshaller.cs
@@ -0,0 +1,46 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System;
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core.Transform;
+using Aliyun.Acs.Dytnsapi.Model.V20200217;
+
+namespace Aliyun.Acs.Dytnsapi.Transform.V20200217
+{
+    public class PhoneNumberStatusForAccountResponseUnmarshaller
+    {
+        public static PhoneNumberStatusForAccountResponse Unmarshall(UnmarshallerContext _ctx)
+        {
+			PhoneNumberStatusForAccountResponse phoneNumberStatusForAccountResponse = new PhoneNumberStatusForAccountResponse();
+
+			phoneNumberStatusForAccountResponse.HttpResponse = _ctx.HttpResponse;
+			phoneNumberStatusForAccountResponse.Code = _ctx.StringValue("PhoneNumberStatusForAccount.Code");
+			phoneNumberStatusForAccountResponse.Message = _ctx.StringValue("PhoneNumberStatusForAccount.Message");
+			phoneNumberStatusForAccountResponse.RequestId = _ctx.StringValue("PhoneNumberStatusForAccount.RequestId");
+
+			PhoneNumberStatusForAccountResponse.PhoneNumberStatusForAccount_Data data = new PhoneNumberStatusForAccountResponse.PhoneNumberStatusForAccount_Data();
+			data.Status = _ctx.StringValue("PhoneNumberStatusForAccount.Data.Status");
+			data.Carrier = _ctx.StringValue("PhoneNumberStatusForAccount.Data.Carrier");
+			phoneNumberStatusForAccountResponse.Data = data;
+        
+			return phoneNumberStatusForAccountResponse;
+        }
+    }
+}
