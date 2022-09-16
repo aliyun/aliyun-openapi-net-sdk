8b57e2ee00f268666480298e8c10d610174cc9f8
sdk-team <sdk-team@alibabacloud.com>
2022-09-16 08:37:42 +0000

    Init amp.

--- aliyun-net-sdk-antiddos-public/Antiddos_public/Model/V20170518/ModifyDefenseThresholdRequest.cs
+++ aliyun-net-sdk-antiddos-public/Antiddos_public/Model/V20170518/ModifyDefenseThresholdRequest.cs
@@ -32,11 +32,6 @@ namespace Aliyun.Acs.antiddos_public.Model.V20170518
         public ModifyDefenseThresholdRequest()
             : base("antiddos-public", "2017-05-18", "ModifyDefenseThreshold", "ddosbasic", "openAPI")
         {
-            if (this.GetType().GetProperty("ProductEndpointMap") != null && this.GetType().GetProperty("ProductEndpointType") != null)
-            {
-                this.GetType().GetProperty("ProductEndpointMap").SetValue(this, Aliyun.Acs.antiddos_public.Endpoint.endpointMap, null);
-                this.GetType().GetProperty("ProductEndpointType").SetValue(this, Aliyun.Acs.antiddos_public.Endpoint.endpointRegionalType, null);
-            }
 			Method = MethodType.POST;
         }
 
