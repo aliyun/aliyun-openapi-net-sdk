b8688c4b36b22d52f4f6954df0ca672f629277ed
sdk-team <sdk-team@alibabacloud.com>
2022-10-17 05:58:16 +0000

    Update RefineMask.

--- aliyun-net-sdk-imageseg/Imageseg/Model/V20191230/SegmentHDCommonImageResponse.cs
+++ aliyun-net-sdk-imageseg/Imageseg/Model/V20191230/SegmentHDCommonImageResponse.cs
@@ -27,6 +27,10 @@ namespace Aliyun.Acs.imageseg.Model.V20191230
 
 		private string requestId;
 
+		private string code;
+
+		private string message;
+
 		private SegmentHDCommonImage_Data data;
 
 		public string RequestId
@@ -38,6 +42,30 @@ namespace Aliyun.Acs.imageseg.Model.V20191230
 			set	
 			{
 				requestId = value;
+			}
+		}
+
+		public string Code
+		{
+			get
+			{
+				return code;
+			}
+			set	
+			{
+				code = value;
+			}
+		}
+
+		public string Message
+		{
+			get
+			{
+				return message;
+			}
+			set	
+			{
+				message = value;
 			}
 		}
 
