70b97030c54a6ec29b949ee5e00e8341707616a9
sdk-team <sdk-team@alibabacloud.com>
2022-12-08 11:43:49 +0000

    Add live message mute related interface.

---
+++ aliyun-net-sdk-live/Live/Model/V20161101/DescribeMeterLiveInteractionDauRequest.cs
@@ -0,0 +1,134 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+using Aliyun.Acs.Core;
+using Aliyun.Acs.Core.Http;
+using Aliyun.Acs.Core.Transform;
+using Aliyun.Acs.Core.Utils;
+using Aliyun.Acs.live.Transform;
+using Aliyun.Acs.live.Transform.V20161101;
+
+namespace Aliyun.Acs.live.Model.V20161101
+{
+    public class DescribeMeterLiveInteractionDauRequest : RpcAcsRequest<DescribeMeterLiveInteractionDauResponse>
+    {
+        public DescribeMeterLiveInteractionDauRequest()
+            : base("live", "2016-11-01", "DescribeMeterLiveInteractionDau", "live", "openAPI")
+        {
+            if (this.GetType().GetProperty("ProductEndpointMap") != null && this.GetType().GetProperty("ProductEndpointType") != null)
+            {
+                this.GetType().GetProperty("ProductEndpointMap").SetValue(this, Aliyun.Acs.live.Endpoint.endpointMap, null);
+                this.GetType().GetProperty("ProductEndpointType").SetValue(this, Aliyun.Acs.live.Endpoint.endpointRegionalType, null);
+            }
+			Method = MethodType.POST;
+        }
+
+		private long? startTs;
+
+		private string serviceArea;
+
+		private string appId;
+
+		private long? endTs;
+
+		private long? interval;
+
+		[JsonProperty(PropertyName = "StartTs")]
+		public long? StartTs
+		{
+			get
+			{
+				return startTs;
+			}
+			set	
+			{
+				startTs = value;
+				DictionaryUtil.Add(QueryParameters, "StartTs", value.ToString());
+			}
+		}
+
+		[JsonProperty(PropertyName = "ServiceArea")]
+		public string ServiceArea
+		{
+			get
+			{
+				return serviceArea;
+			}
+			set	
+			{
+				serviceArea = value;
+				DictionaryUtil.Add(QueryParameters, "ServiceArea", value);
+			}
+		}
+
+		[JsonProperty(PropertyName = "AppId")]
+		public string AppId
+		{
+			get
+			{
+				return appId;
+			}
+			set	
+			{
+				appId = value;
+				DictionaryUtil.Add(QueryParameters, "AppId", value);
+			}
+		}
+
+		[JsonProperty(PropertyName = "EndTs")]
+		public long? EndTs
+		{
+			get
+			{
+				return endTs;
+			}
+			set	
+			{
+				endTs = value;
+				DictionaryUtil.Add(QueryParameters, "EndTs", value.ToString());
+			}
+		}
+
+		[JsonProperty(PropertyName = "Interval")]
+		public long? Interval
+		{
+			get
+			{
+				return interval;
+			}
+			set	
+			{
+				interval = value;
+				DictionaryUtil.Add(QueryParameters, "Interval", value.ToString());
+			}
+		}
+
+		public override bool CheckShowJsonItemName()
+		{
+			return false;
+		}
+
+        public override DescribeMeterLiveInteractionDauResponse GetResponse(UnmarshallerContext unmarshallerContext)
+        {
+            return DescribeMeterLiveInteractionDauResponseUnmarshaller.Unmarshall(unmarshallerContext);
+        }
+    }
+}
---
+++ aliyun-net-sdk-live/Live/Model/V20161101/DescribeMeterLiveInteractionDauResponse.cs
@@ -0,0 +1,107 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Aliyun.Acs.Core;
+
+namespace Aliyun.Acs.live.Model.V20161101
+{
+	public class DescribeMeterLiveInteractionDauResponse : AcsResponse
+	{
+
+		private string requestId;
+
+		private double? peakDau;
+
+		private List<DescribeMeterLiveInteractionDau_DataItem> data;
+
+		[JsonProperty(PropertyName = "RequestId")]
+		public string RequestId
+		{
+			get
+			{
+				return requestId;
+			}
+			set	
+			{
+				requestId = value;
+			}
+		}
+
+		[JsonProperty(PropertyName = "PeakDau")]
+		public double? PeakDau
+		{
+			get
+			{
+				return peakDau;
+			}
+			set	
+			{
+				peakDau = value;
+			}
+		}
+
+		[JsonProperty(PropertyName = "Data")]
+		public List<DescribeMeterLiveInteractionDau_DataItem> Data
+		{
+			get
+			{
+				return data;
+			}
+			set	
+			{
+				data = value;
+			}
+		}
+
+		public class DescribeMeterLiveInteractionDau_DataItem
+		{
+
+			private long? timestamp;
+
+			private double? dau;
+
+			[JsonProperty(PropertyName = "Timestamp")]
+			public long? Timestamp
+			{
+				get
+				{
+					return timestamp;
+				}
+				set	
+				{
+					timestamp = value;
+				}
+			}
+
+			[JsonProperty(PropertyName = "Dau")]
+			public double? Dau
+			{
+				get
+				{
+					return dau;
+				}
+				set	
+				{
+					dau = value;
+				}
+			}
+		}
+	}
+}
