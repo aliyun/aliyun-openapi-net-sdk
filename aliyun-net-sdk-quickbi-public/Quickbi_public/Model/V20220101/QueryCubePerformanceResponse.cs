9d6c4c4f85aab26f9a40d84298035ebaf58b8200
sdk-team <sdk-team@alibabacloud.com>
2023-06-20 12:31:24 +0000

    Add QueryCubePerformance, QueryComponentPerformance, QueryCubeOptimization, and QueryReportPerformance APIs.

---
+++ aliyun-net-sdk-quickbi-public/Quickbi_public/Model/V20220101/QueryCubePerformanceResponse.cs
@@ -0,0 +1,368 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Aliyun.Acs.Core;
+
+namespace Aliyun.Acs.quickbi_public.Model.V20220101
+{
+	public class QueryCubePerformanceResponse : AcsResponse
+	{
+
+		private string requestId;
+
+		private bool? success;
+
+		private List<QueryCubePerformance_Data> result;
+
+		public string RequestId
+		{
+			get
+			{
+				return requestId;
+			}
+			set	
+			{
+				requestId = value;
+			}
+		}
+
+		public bool? Success
+		{
+			get
+			{
+				return success;
+			}
+			set	
+			{
+				success = value;
+			}
+		}
+
+		public List<QueryCubePerformance_Data> Result
+		{
+			get
+			{
+				return result;
+			}
+			set	
+			{
+				result = value;
+			}
+		}
+
+		public class QueryCubePerformance_Data
+		{
+
+			private string cubeId;
+
+			private string cubeName;
+
+			private int? queryTimeoutCount;
+
+			private string repeatSqlQueryPercent;
+
+			private string workspaceName;
+
+			private string workspaceId;
+
+			private double? repeatQueryPercentNum;
+
+			private string repeatQueryPercent;
+
+			private double? quickIndexCostTimeAvg;
+
+			private int? quickIndexQueryCount;
+
+			private double? cacheCostTimeAvg;
+
+			private int? cacheQueryCount;
+
+			private int? repeatSqlQueryCount;
+
+			private double? costTimeAvg;
+
+			private double? queryCountAvg;
+
+			private int? queryCount;
+
+			private double? queryTimeoutCountPercent;
+
+			private double? queryOverTenSecPercentNum;
+
+			private double? queryOverFivePercentNum;
+
+			private string queryOverTenSecPercent;
+
+			private string queryOverFiveSecPercent;
+
+			public string CubeId
+			{
+				get
+				{
+					return cubeId;
+				}
+				set	
+				{
+					cubeId = value;
+				}
+			}
+
+			public string CubeName
+			{
+				get
+				{
+					return cubeName;
+				}
+				set	
+				{
+					cubeName = value;
+				}
+			}
+
+			public int? QueryTimeoutCount
+			{
+				get
+				{
+					return queryTimeoutCount;
+				}
+				set	
+				{
+					queryTimeoutCount = value;
+				}
+			}
+
+			public string RepeatSqlQueryPercent
+			{
+				get
+				{
+					return repeatSqlQueryPercent;
+				}
+				set	
+				{
+					repeatSqlQueryPercent = value;
+				}
+			}
+
+			public string WorkspaceName
+			{
+				get
+				{
+					return workspaceName;
+				}
+				set	
+				{
+					workspaceName = value;
+				}
+			}
+
+			public string WorkspaceId
+			{
+				get
+				{
+					return workspaceId;
+				}
+				set	
+				{
+					workspaceId = value;
+				}
+			}
+
+			public double? RepeatQueryPercentNum
+			{
+				get
+				{
+					return repeatQueryPercentNum;
+				}
+				set	
+				{
+					repeatQueryPercentNum = value;
+				}
+			}
+
+			public string RepeatQueryPercent
+			{
+				get
+				{
+					return repeatQueryPercent;
+				}
+				set	
+				{
+					repeatQueryPercent = value;
+				}
+			}
+
+			public double? QuickIndexCostTimeAvg
+			{
+				get
+				{
+					return quickIndexCostTimeAvg;
+				}
+				set	
+				{
+					quickIndexCostTimeAvg = value;
+				}
+			}
+
+			public int? QuickIndexQueryCount
+			{
+				get
+				{
+					return quickIndexQueryCount;
+				}
+				set	
+				{
+					quickIndexQueryCount = value;
+				}
+			}
+
+			public double? CacheCostTimeAvg
+			{
+				get
+				{
+					return cacheCostTimeAvg;
+				}
+				set	
+				{
+					cacheCostTimeAvg = value;
+				}
+			}
+
+			public int? CacheQueryCount
+			{
+				get
+				{
+					return cacheQueryCount;
+				}
+				set	
+				{
+					cacheQueryCount = value;
+				}
+			}
+
+			public int? RepeatSqlQueryCount
+			{
+				get
+				{
+					return repeatSqlQueryCount;
+				}
+				set	
+				{
+					repeatSqlQueryCount = value;
+				}
+			}
+
+			public double? CostTimeAvg
+			{
+				get
+				{
+					return costTimeAvg;
+				}
+				set	
+				{
+					costTimeAvg = value;
+				}
+			}
+
+			public double? QueryCountAvg
+			{
+				get
+				{
+					return queryCountAvg;
+				}
+				set	
+				{
+					queryCountAvg = value;
+				}
+			}
+
+			public int? QueryCount
+			{
+				get
+				{
+					return queryCount;
+				}
+				set	
+				{
+					queryCount = value;
+				}
+			}
+
+			public double? QueryTimeoutCountPercent
+			{
+				get
+				{
+					return queryTimeoutCountPercent;
+				}
+				set	
+				{
+					queryTimeoutCountPercent = value;
+				}
+			}
+
+			public double? QueryOverTenSecPercentNum
+			{
+				get
+				{
+					return queryOverTenSecPercentNum;
+				}
+				set	
+				{
+					queryOverTenSecPercentNum = value;
+				}
+			}
+
+			public double? QueryOverFivePercentNum
+			{
+				get
+				{
+					return queryOverFivePercentNum;
+				}
+				set	
+				{
+					queryOverFivePercentNum = value;
+				}
+			}
+
+			public string QueryOverTenSecPercent
+			{
+				get
+				{
+					return queryOverTenSecPercent;
+				}
+				set	
+				{
+					queryOverTenSecPercent = value;
+				}
+			}
+
+			public string QueryOverFiveSecPercent
+			{
+				get
+				{
+					return queryOverFiveSecPercent;
+				}
+				set	
+				{
+					queryOverFiveSecPercent = value;
+				}
+			}
+		}
+	}
+}
---
+++ aliyun-net-sdk-quickbi-public/Quickbi_public/Transform/V20220101/QueryReportPerformanceResponseUnmarshaller.cs
@@ -0,0 +1,72 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System;
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core.Transform;
+using Aliyun.Acs.quickbi_public.Model.V20220101;
+
+namespace Aliyun.Acs.quickbi_public.Transform.V20220101
+{
+    public class QueryReportPerformanceResponseUnmarshaller
+    {
+        public static QueryReportPerformanceResponse Unmarshall(UnmarshallerContext _ctx)
+        {
+			QueryReportPerformanceResponse queryReportPerformanceResponse = new QueryReportPerformanceResponse();
+
+			queryReportPerformanceResponse.HttpResponse = _ctx.HttpResponse;
+			queryReportPerformanceResponse.RequestId = _ctx.StringValue("QueryReportPerformance.RequestId");
+			queryReportPerformanceResponse.Success = _ctx.BooleanValue("QueryReportPerformance.Success");
+
+			List<QueryReportPerformanceResponse.QueryReportPerformance_Data> queryReportPerformanceResponse_result = new List<QueryReportPerformanceResponse.QueryReportPerformance_Data>();
+			for (int i = 0; i < _ctx.Length("QueryReportPerformance.Result.Length"); i++) {
+				QueryReportPerformanceResponse.QueryReportPerformance_Data data = new QueryReportPerformanceResponse.QueryReportPerformance_Data();
+				data.ReportId = _ctx.StringValue("QueryReportPerformance.Result["+ i +"].ReportId");
+				data.ReportName = _ctx.StringValue("QueryReportPerformance.Result["+ i +"].ReportName");
+				data.ComponentQueryCount = _ctx.IntegerValue("QueryReportPerformance.Result["+ i +"].ComponentQueryCount");
+				data.ComponentQueryCountAvg = _ctx.DoubleValue("QueryReportPerformance.Result["+ i +"].ComponentQueryCountAvg");
+				data.ReportType = _ctx.StringValue("QueryReportPerformance.Result["+ i +"].ReportType");
+				data.QueryTimeoutCount = _ctx.IntegerValue("QueryReportPerformance.Result["+ i +"].QueryTimeoutCount");
+				data.RepeatSqlQueryPercent = _ctx.StringValue("QueryReportPerformance.Result["+ i +"].RepeatSqlQueryPercent");
+				data.WorkspaceName = _ctx.StringValue("QueryReportPerformance.Result["+ i +"].WorkspaceName");
+				data.WorkspaceId = _ctx.StringValue("QueryReportPerformance.Result["+ i +"].WorkspaceId");
+				data.RepeatQueryPercentNum = _ctx.DoubleValue("QueryReportPerformance.Result["+ i +"].RepeatQueryPercentNum");
+				data.RepeatQueryPercent = _ctx.StringValue("QueryReportPerformance.Result["+ i +"].RepeatQueryPercent");
+				data.QuickIndexCostTimeAvg = _ctx.DoubleValue("QueryReportPerformance.Result["+ i +"].QuickIndexCostTimeAvg");
+				data.QuickIndexQueryCount = _ctx.IntegerValue("QueryReportPerformance.Result["+ i +"].QuickIndexQueryCount");
+				data.CacheCostTimeAvg = _ctx.DoubleValue("QueryReportPerformance.Result["+ i +"].CacheCostTimeAvg");
+				data.CacheQueryCount = _ctx.IntegerValue("QueryReportPerformance.Result["+ i +"].CacheQueryCount");
+				data.RepeatSqlQueryCount = _ctx.IntegerValue("QueryReportPerformance.Result["+ i +"].RepeatSqlQueryCount");
+				data.CostTimeAvg = _ctx.DoubleValue("QueryReportPerformance.Result["+ i +"].CostTimeAvg");
+				data.QueryCountAvg = _ctx.DoubleValue("QueryReportPerformance.Result["+ i +"].QueryCountAvg");
+				data.QueryCount = _ctx.IntegerValue("QueryReportPerformance.Result["+ i +"].QueryCount");
+				data.QueryTimeoutCountPercent = _ctx.DoubleValue("QueryReportPerformance.Result["+ i +"].QueryTimeoutCountPercent");
+				data.QueryOverTenSecPercentNum = _ctx.DoubleValue("QueryReportPerformance.Result["+ i +"].QueryOverTenSecPercentNum");
+				data.QueryOverFivePercentNum = _ctx.DoubleValue("QueryReportPerformance.Result["+ i +"].QueryOverFivePercentNum");
+				data.QueryOverTenSecPercent = _ctx.StringValue("QueryReportPerformance.Result["+ i +"].QueryOverTenSecPercent");
+				data.QueryOverFiveSecPercent = _ctx.StringValue("QueryReportPerformance.Result["+ i +"].QueryOverFiveSecPercent");
+
+				queryReportPerformanceResponse_result.Add(data);
+			}
+			queryReportPerformanceResponse.Result = queryReportPerformanceResponse_result;
+        
+			return queryReportPerformanceResponse;
+        }
+    }
+}
