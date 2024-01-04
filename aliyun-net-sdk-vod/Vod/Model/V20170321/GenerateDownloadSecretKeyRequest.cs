07b4f76f7785fa6e600e3f70b6a8e0be43d66def
sdk-team <sdk-team@alibabacloud.com>
2024-01-04 07:34:09 +0000

    open GenerateDownloadDecryptkey.

---
+++ aliyun-net-sdk-vod/Vod/Model/V20170321/GenerateDownloadSecretKeyRequest.cs
@@ -0,0 +1,123 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core;
+using Aliyun.Acs.Core.Http;
+using Aliyun.Acs.Core.Transform;
+using Aliyun.Acs.Core.Utils;
+using Aliyun.Acs.vod.Transform;
+using Aliyun.Acs.vod.Transform.V20170321;
+
+namespace Aliyun.Acs.vod.Model.V20170321
+{
+    public class GenerateDownloadSecretKeyRequest : RpcAcsRequest<GenerateDownloadSecretKeyResponse>
+    {
+        public GenerateDownloadSecretKeyRequest()
+            : base("vod", "2017-03-21", "GenerateDownloadSecretKey", "vod", "openAPI")
+        {
+            if (this.GetType().GetProperty("ProductEndpointMap") != null && this.GetType().GetProperty("ProductEndpointType") != null)
+            {
+                this.GetType().GetProperty("ProductEndpointMap").SetValue(this, Aliyun.Acs.vod.Endpoint.endpointMap, null);
+                this.GetType().GetProperty("ProductEndpointType").SetValue(this, Aliyun.Acs.vod.Endpoint.endpointRegionalType, null);
+            }
+			Method = MethodType.POST;
+        }
+
+		private long? resourceOwnerId;
+
+		private string appDecryptKey;
+
+		private string resourceOwnerAccount;
+
+		private string appIdentification;
+
+		private long? ownerId;
+
+		public long? ResourceOwnerId
+		{
+			get
+			{
+				return resourceOwnerId;
+			}
+			set	
+			{
+				resourceOwnerId = value;
+				DictionaryUtil.Add(QueryParameters, "ResourceOwnerId", value.ToString());
+			}
+		}
+
+		public string AppDecryptKey
+		{
+			get
+			{
+				return appDecryptKey;
+			}
+			set	
+			{
+				appDecryptKey = value;
+				DictionaryUtil.Add(QueryParameters, "AppDecryptKey", value);
+			}
+		}
+
+		public string ResourceOwnerAccount
+		{
+			get
+			{
+				return resourceOwnerAccount;
+			}
+			set	
+			{
+				resourceOwnerAccount = value;
+				DictionaryUtil.Add(QueryParameters, "ResourceOwnerAccount", value);
+			}
+		}
+
+		public string AppIdentification
+		{
+			get
+			{
+				return appIdentification;
+			}
+			set	
+			{
+				appIdentification = value;
+				DictionaryUtil.Add(QueryParameters, "AppIdentification", value);
+			}
+		}
+
+		public long? OwnerId
+		{
+			get
+			{
+				return ownerId;
+			}
+			set	
+			{
+				ownerId = value;
+				DictionaryUtil.Add(QueryParameters, "OwnerId", value.ToString());
+			}
+		}
+
+        public override GenerateDownloadSecretKeyResponse GetResponse(UnmarshallerContext unmarshallerContext)
+        {
+            return GenerateDownloadSecretKeyResponseUnmarshaller.Unmarshall(unmarshallerContext);
+        }
+    }
+}
