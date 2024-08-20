3608c02e95515785490269622672d121549ab181
sdk-team <sdk-team@alibabacloud.com>
2024-08-20 03:25:02 +0000

    增加DescribeVodDomainMax95BpsData

---
+++ aliyun-net-sdk-vod/Vod/Model/V20170321/DescribeVodDomainMax95BpsDataRequest.cs
@@ -0,0 +1,138 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core;
+using Aliyun.Acs.Core.Http;
+using Aliyun.Acs.Core.Transform;
+using Aliyun.Acs.Core.Utils;
+using Aliyun.Acs.vod.Transform;
+using Aliyun.Acs.vod.Transform.V20170321;
+
+namespace Aliyun.Acs.vod.Model.V20170321
+{
+    public class DescribeVodDomainMax95BpsDataRequest : RpcAcsRequest<DescribeVodDomainMax95BpsDataResponse>
+    {
+        public DescribeVodDomainMax95BpsDataRequest()
+            : base("vod", "2017-03-21", "DescribeVodDomainMax95BpsData", "vod", "openAPI")
+        {
+            if (this.GetType().GetProperty("ProductEndpointMap") != null && this.GetType().GetProperty("ProductEndpointType") != null)
+            {
+                this.GetType().GetProperty("ProductEndpointMap").SetValue(this, Aliyun.Acs.vod.Endpoint.endpointMap, null);
+                this.GetType().GetProperty("ProductEndpointType").SetValue(this, Aliyun.Acs.vod.Endpoint.endpointRegionalType, null);
+            }
+			Protocol = ProtocolType.HTTPS;
+        }
+
+		private string startTime;
+
+		private string cycle;
+
+		private string domainName;
+
+		private string endTime;
+
+		private long? ownerId;
+
+		private string timePoint;
+
+		public string StartTime
+		{
+			get
+			{
+				return startTime;
+			}
+			set	
+			{
+				startTime = value;
+				DictionaryUtil.Add(QueryParameters, "StartTime", value);
+			}
+		}
+
+		public string Cycle
+		{
+			get
+			{
+				return cycle;
+			}
+			set	
+			{
+				cycle = value;
+				DictionaryUtil.Add(QueryParameters, "Cycle", value);
+			}
+		}
+
+		public string DomainName
+		{
+			get
+			{
+				return domainName;
+			}
+			set	
+			{
+				domainName = value;
+				DictionaryUtil.Add(QueryParameters, "DomainName", value);
+			}
+		}
+
+		public string EndTime
+		{
+			get
+			{
+				return endTime;
+			}
+			set	
+			{
+				endTime = value;
+				DictionaryUtil.Add(QueryParameters, "EndTime", value);
+			}
+		}
+
+		public long? OwnerId
+		{
+			get
+			{
+				return ownerId;
+			}
+			set	
+			{
+				ownerId = value;
+				DictionaryUtil.Add(QueryParameters, "OwnerId", value.ToString());
+			}
+		}
+
+		public string TimePoint
+		{
+			get
+			{
+				return timePoint;
+			}
+			set	
+			{
+				timePoint = value;
+				DictionaryUtil.Add(QueryParameters, "TimePoint", value);
+			}
+		}
+
+        public override DescribeVodDomainMax95BpsDataResponse GetResponse(UnmarshallerContext unmarshallerContext)
+        {
+            return DescribeVodDomainMax95BpsDataResponseUnmarshaller.Unmarshall(unmarshallerContext);
+        }
+    }
+}
