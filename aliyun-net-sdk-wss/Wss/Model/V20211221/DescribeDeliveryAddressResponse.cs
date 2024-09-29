93f55116470c0f8e1bb8ec617a784dba17bda6d0
sdk-team <sdk-team@alibabacloud.com>
2024-09-29 14:43:47 +0000

    Generated 2021-12-21 for wss.

---
+++ aliyun-net-sdk-wss/Wss/Model/V20211221/DescribeDeliveryAddressResponse.cs
@@ -0,0 +1,328 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Aliyun.Acs.Core;
+
+namespace Aliyun.Acs.wss.Model.V20211221
+{
+	public class DescribeDeliveryAddressResponse : AcsResponse
+	{
+
+		private int? totalCount;
+
+		private string requestId;
+
+		private List<DescribeDeliveryAddress_Address> addresses;
+
+		public int? TotalCount
+		{
+			get
+			{
+				return totalCount;
+			}
+			set	
+			{
+				totalCount = value;
+			}
+		}
+
+		public string RequestId
+		{
+			get
+			{
+				return requestId;
+			}
+			set	
+			{
+				requestId = value;
+			}
+		}
+
+		public List<DescribeDeliveryAddress_Address> Addresses
+		{
+			get
+			{
+				return addresses;
+			}
+			set	
+			{
+				addresses = value;
+			}
+		}
+
+		public class DescribeDeliveryAddress_Address
+		{
+
+			private string postalCode;
+
+			private string contacts;
+
+			private string mobile;
+
+			private bool? defaultAddress;
+
+			private string detail;
+
+			private DescribeDeliveryAddress_Province province;
+
+			private DescribeDeliveryAddress_City city;
+
+			private DescribeDeliveryAddress_Area area;
+
+			private DescribeDeliveryAddress_Town town;
+
+			public string PostalCode
+			{
+				get
+				{
+					return postalCode;
+				}
+				set	
+				{
+					postalCode = value;
+				}
+			}
+
+			public string Contacts
+			{
+				get
+				{
+					return contacts;
+				}
+				set	
+				{
+					contacts = value;
+				}
+			}
+
+			public string Mobile
+			{
+				get
+				{
+					return mobile;
+				}
+				set	
+				{
+					mobile = value;
+				}
+			}
+
+			public bool? DefaultAddress
+			{
+				get
+				{
+					return defaultAddress;
+				}
+				set	
+				{
+					defaultAddress = value;
+				}
+			}
+
+			public string Detail
+			{
+				get
+				{
+					return detail;
+				}
+				set	
+				{
+					detail = value;
+				}
+			}
+
+			public DescribeDeliveryAddress_Province Province
+			{
+				get
+				{
+					return province;
+				}
+				set	
+				{
+					province = value;
+				}
+			}
+
+			public DescribeDeliveryAddress_City City
+			{
+				get
+				{
+					return city;
+				}
+				set	
+				{
+					city = value;
+				}
+			}
+
+			public DescribeDeliveryAddress_Area Area
+			{
+				get
+				{
+					return area;
+				}
+				set	
+				{
+					area = value;
+				}
+			}
+
+			public DescribeDeliveryAddress_Town Town
+			{
+				get
+				{
+					return town;
+				}
+				set	
+				{
+					town = value;
+				}
+			}
+
+			public class DescribeDeliveryAddress_Province
+			{
+
+				private long? provinceId;
+
+				private string provinceName;
+
+				public long? ProvinceId
+				{
+					get
+					{
+						return provinceId;
+					}
+					set	
+					{
+						provinceId = value;
+					}
+				}
+
+				public string ProvinceName
+				{
+					get
+					{
+						return provinceName;
+					}
+					set	
+					{
+						provinceName = value;
+					}
+				}
+			}
+
+			public class DescribeDeliveryAddress_City
+			{
+
+				private long? cityId;
+
+				private string cityName;
+
+				public long? CityId
+				{
+					get
+					{
+						return cityId;
+					}
+					set	
+					{
+						cityId = value;
+					}
+				}
+
+				public string CityName
+				{
+					get
+					{
+						return cityName;
+					}
+					set	
+					{
+						cityName = value;
+					}
+				}
+			}
+
+			public class DescribeDeliveryAddress_Area
+			{
+
+				private string areaName;
+
+				private long? areaId;
+
+				public string AreaName
+				{
+					get
+					{
+						return areaName;
+					}
+					set	
+					{
+						areaName = value;
+					}
+				}
+
+				public long? AreaId
+				{
+					get
+					{
+						return areaId;
+					}
+					set	
+					{
+						areaId = value;
+					}
+				}
+			}
+
+			public class DescribeDeliveryAddress_Town
+			{
+
+				private string townName;
+
+				private long? townId;
+
+				public string TownName
+				{
+					get
+					{
+						return townName;
+					}
+					set	
+					{
+						townName = value;
+					}
+				}
+
+				public long? TownId
+				{
+					get
+					{
+						return townId;
+					}
+					set	
+					{
+						townId = value;
+					}
+				}
+			}
+		}
+	}
+}
---
+++ aliyun-net-sdk-wss/Wss/Transform/V20211221/DescribeDeliveryAddressResponseUnmarshaller.cs
@@ -0,0 +1,73 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System;
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core.Transform;
+using Aliyun.Acs.wss.Model.V20211221;
+
+namespace Aliyun.Acs.wss.Transform.V20211221
+{
+    public class DescribeDeliveryAddressResponseUnmarshaller
+    {
+        public static DescribeDeliveryAddressResponse Unmarshall(UnmarshallerContext _ctx)
+        {
+			DescribeDeliveryAddressResponse describeDeliveryAddressResponse = new DescribeDeliveryAddressResponse();
+
+			describeDeliveryAddressResponse.HttpResponse = _ctx.HttpResponse;
+			describeDeliveryAddressResponse.TotalCount = _ctx.IntegerValue("DescribeDeliveryAddress.TotalCount");
+			describeDeliveryAddressResponse.RequestId = _ctx.StringValue("DescribeDeliveryAddress.RequestId");
+
+			List<DescribeDeliveryAddressResponse.DescribeDeliveryAddress_Address> describeDeliveryAddressResponse_addresses = new List<DescribeDeliveryAddressResponse.DescribeDeliveryAddress_Address>();
+			for (int i = 0; i < _ctx.Length("DescribeDeliveryAddress.Addresses.Length"); i++) {
+				DescribeDeliveryAddressResponse.DescribeDeliveryAddress_Address address = new DescribeDeliveryAddressResponse.DescribeDeliveryAddress_Address();
+				address.PostalCode = _ctx.StringValue("DescribeDeliveryAddress.Addresses["+ i +"].PostalCode");
+				address.Contacts = _ctx.StringValue("DescribeDeliveryAddress.Addresses["+ i +"].Contacts");
+				address.Mobile = _ctx.StringValue("DescribeDeliveryAddress.Addresses["+ i +"].Mobile");
+				address.DefaultAddress = _ctx.BooleanValue("DescribeDeliveryAddress.Addresses["+ i +"].DefaultAddress");
+				address.Detail = _ctx.StringValue("DescribeDeliveryAddress.Addresses["+ i +"].Detail");
+
+				DescribeDeliveryAddressResponse.DescribeDeliveryAddress_Address.DescribeDeliveryAddress_Province province = new DescribeDeliveryAddressResponse.DescribeDeliveryAddress_Address.DescribeDeliveryAddress_Province();
+				province.ProvinceId = _ctx.LongValue("DescribeDeliveryAddress.Addresses["+ i +"].Province.ProvinceId");
+				province.ProvinceName = _ctx.StringValue("DescribeDeliveryAddress.Addresses["+ i +"].Province.ProvinceName");
+				address.Province = province;
+
+				DescribeDeliveryAddressResponse.DescribeDeliveryAddress_Address.DescribeDeliveryAddress_City city = new DescribeDeliveryAddressResponse.DescribeDeliveryAddress_Address.DescribeDeliveryAddress_City();
+				city.CityId = _ctx.LongValue("DescribeDeliveryAddress.Addresses["+ i +"].City.CityId");
+				city.CityName = _ctx.StringValue("DescribeDeliveryAddress.Addresses["+ i +"].City.CityName");
+				address.City = city;
+
+				DescribeDeliveryAddressResponse.DescribeDeliveryAddress_Address.DescribeDeliveryAddress_Area area = new DescribeDeliveryAddressResponse.DescribeDeliveryAddress_Address.DescribeDeliveryAddress_Area();
+				area.AreaName = _ctx.StringValue("DescribeDeliveryAddress.Addresses["+ i +"].Area.AreaName");
+				area.AreaId = _ctx.LongValue("DescribeDeliveryAddress.Addresses["+ i +"].Area.AreaId");
+				address.Area = area;
+
+				DescribeDeliveryAddressResponse.DescribeDeliveryAddress_Address.DescribeDeliveryAddress_Town town = new DescribeDeliveryAddressResponse.DescribeDeliveryAddress_Address.DescribeDeliveryAddress_Town();
+				town.TownName = _ctx.StringValue("DescribeDeliveryAddress.Addresses["+ i +"].Town.TownName");
+				town.TownId = _ctx.LongValue("DescribeDeliveryAddress.Addresses["+ i +"].Town.TownId");
+				address.Town = town;
+
+				describeDeliveryAddressResponse_addresses.Add(address);
+			}
+			describeDeliveryAddressResponse.Addresses = describeDeliveryAddressResponse_addresses;
+        
+			return describeDeliveryAddressResponse;
+        }
+    }
+}
