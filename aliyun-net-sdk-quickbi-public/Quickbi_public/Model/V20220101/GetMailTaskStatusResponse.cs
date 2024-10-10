887dbd05043bdf7b59406f26605cab4887633258
sdk-team <sdk-team@alibabacloud.com>
2024-10-10 09:56:24 +0000

    Add ManualRunMailTask, GetMailTaskStatus and GetWorksEmbedList apis.

---
+++ aliyun-net-sdk-quickbi-public/Quickbi_public/Model/V20220101/GetMailTaskStatusResponse.cs
@@ -0,0 +1,130 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Aliyun.Acs.Core;
+
+namespace Aliyun.Acs.quickbi_public.Model.V20220101
+{
+	public class GetMailTaskStatusResponse : AcsResponse
+	{
+
+		private string requestId;
+
+		private bool? success;
+
+		private List<GetMailTaskStatus_DATA> result;
+
+		public string RequestId
+		{
+			get
+			{
+				return requestId;
+			}
+			set	
+			{
+				requestId = value;
+			}
+		}
+
+		public bool? Success
+		{
+			get
+			{
+				return success;
+			}
+			set	
+			{
+				success = value;
+			}
+		}
+
+		public List<GetMailTaskStatus_DATA> Result
+		{
+			get
+			{
+				return result;
+			}
+			set	
+			{
+				result = value;
+			}
+		}
+
+		public class GetMailTaskStatus_DATA
+		{
+
+			private string execTime;
+
+			private string mailId;
+
+			private string status;
+
+			private long? taskId;
+
+			public string ExecTime
+			{
+				get
+				{
+					return execTime;
+				}
+				set	
+				{
+					execTime = value;
+				}
+			}
+
+			public string MailId
+			{
+				get
+				{
+					return mailId;
+				}
+				set	
+				{
+					mailId = value;
+				}
+			}
+
+			public string Status
+			{
+				get
+				{
+					return status;
+				}
+				set	
+				{
+					status = value;
+				}
+			}
+
+			public long? TaskId
+			{
+				get
+				{
+					return taskId;
+				}
+				set	
+				{
+					taskId = value;
+				}
+			}
+		}
+	}
+}
---
+++ aliyun-net-sdk-quickbi-public/Quickbi_public/Transform/V20220101/GetMailTaskStatusResponseUnmarshaller.cs
@@ -0,0 +1,52 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System;
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core.Transform;
+using Aliyun.Acs.quickbi_public.Model.V20220101;
+
+namespace Aliyun.Acs.quickbi_public.Transform.V20220101
+{
+    public class GetMailTaskStatusResponseUnmarshaller
+    {
+        public static GetMailTaskStatusResponse Unmarshall(UnmarshallerContext _ctx)
+        {
+			GetMailTaskStatusResponse getMailTaskStatusResponse = new GetMailTaskStatusResponse();
+
+			getMailTaskStatusResponse.HttpResponse = _ctx.HttpResponse;
+			getMailTaskStatusResponse.RequestId = _ctx.StringValue("GetMailTaskStatus.RequestId");
+			getMailTaskStatusResponse.Success = _ctx.BooleanValue("GetMailTaskStatus.Success");
+
+			List<GetMailTaskStatusResponse.GetMailTaskStatus_DATA> getMailTaskStatusResponse_result = new List<GetMailTaskStatusResponse.GetMailTaskStatus_DATA>();
+			for (int i = 0; i < _ctx.Length("GetMailTaskStatus.Result.Length"); i++) {
+				GetMailTaskStatusResponse.GetMailTaskStatus_DATA dATA = new GetMailTaskStatusResponse.GetMailTaskStatus_DATA();
+				dATA.ExecTime = _ctx.StringValue("GetMailTaskStatus.Result["+ i +"].execTime");
+				dATA.MailId = _ctx.StringValue("GetMailTaskStatus.Result["+ i +"].mailId");
+				dATA.Status = _ctx.StringValue("GetMailTaskStatus.Result["+ i +"].status");
+				dATA.TaskId = _ctx.LongValue("GetMailTaskStatus.Result["+ i +"].taskId");
+
+				getMailTaskStatusResponse_result.Add(dATA);
+			}
+			getMailTaskStatusResponse.Result = getMailTaskStatusResponse_result;
+        
+			return getMailTaskStatusResponse;
+        }
+    }
+}
