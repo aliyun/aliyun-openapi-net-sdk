e2b2ad3ca36eeccf4c4b7cba19310bed2f8be89c
sdk-team <sdk-team@alibabacloud.com>
2025-06-24 16:02:12 +0000

    Generated 2025-04-29 for WebsiteBuild.

---
+++ aliyun-net-sdk-websitebuild/WebsiteBuild/Model/V20250429/GetCreateLogoTaskResponse.cs
@@ -0,0 +1,144 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Aliyun.Acs.Core;
+
+namespace Aliyun.Acs.WebsiteBuild.Model.V20250429
+{
+	public class GetCreateLogoTaskResponse : AcsResponse
+	{
+
+		private string requestId;
+
+		private string errorMsg;
+
+		private bool? success;
+
+		private string errorCode;
+
+		private GetCreateLogoTask_Task task;
+
+		public string RequestId
+		{
+			get
+			{
+				return requestId;
+			}
+			set	
+			{
+				requestId = value;
+			}
+		}
+
+		public string ErrorMsg
+		{
+			get
+			{
+				return errorMsg;
+			}
+			set	
+			{
+				errorMsg = value;
+			}
+		}
+
+		public bool? Success
+		{
+			get
+			{
+				return success;
+			}
+			set	
+			{
+				success = value;
+			}
+		}
+
+		public string ErrorCode
+		{
+			get
+			{
+				return errorCode;
+			}
+			set	
+			{
+				errorCode = value;
+			}
+		}
+
+		public GetCreateLogoTask_Task Task
+		{
+			get
+			{
+				return task;
+			}
+			set	
+			{
+				task = value;
+			}
+		}
+
+		public class GetCreateLogoTask_Task
+		{
+
+			private string taskId;
+
+			private string taskStatus;
+
+			private List<string> urls;
+
+			public string TaskId
+			{
+				get
+				{
+					return taskId;
+				}
+				set	
+				{
+					taskId = value;
+				}
+			}
+
+			public string TaskStatus
+			{
+				get
+				{
+					return taskStatus;
+				}
+				set	
+				{
+					taskStatus = value;
+				}
+			}
+
+			public List<string> Urls
+			{
+				get
+				{
+					return urls;
+				}
+				set	
+				{
+					urls = value;
+				}
+			}
+		}
+	}
+}
