177aa188abaec8b0c5cd8bc4ef86349a95d9a5da
sdk-team <sdk-team@alibabacloud.com>
2025-09-11 06:42:03 +0000

    Add some APIs.

---
+++ aliyun-net-sdk-quickbi-public/Quickbi_public/Model/V20220101/AddDataSourceRequest.cs
@@ -0,0 +1,64 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core;
+using Aliyun.Acs.Core.Http;
+using Aliyun.Acs.Core.Transform;
+using Aliyun.Acs.Core.Utils;
+using Aliyun.Acs.quickbi_public.Transform;
+using Aliyun.Acs.quickbi_public.Transform.V20220101;
+
+namespace Aliyun.Acs.quickbi_public.Model.V20220101
+{
+    public class AddDataSourceRequest : RpcAcsRequest<AddDataSourceResponse>
+    {
+        public AddDataSourceRequest()
+            : base("quickbi-public", "2022-01-01", "AddDataSource", "2.2.0", "openAPI")
+        {
+			Protocol = ProtocolType.HTTPS;
+			Method = MethodType.POST;
+        }
+
+		private string addModel;
+
+		public string AddModel
+		{
+			get
+			{
+				return addModel;
+			}
+			set	
+			{
+				addModel = value;
+				DictionaryUtil.Add(QueryParameters, "AddModel", value);
+			}
+		}
+
+		public override bool CheckShowJsonItemName()
+		{
+			return false;
+		}
+
+        public override AddDataSourceResponse GetResponse(UnmarshallerContext unmarshallerContext)
+        {
+            return AddDataSourceResponseUnmarshaller.Unmarshall(unmarshallerContext);
+        }
+    }
+}
