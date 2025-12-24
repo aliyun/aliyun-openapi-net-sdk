a4723d36505a906db7c751cafb1643781fe776a3
sdk-team <sdk-team@alibabacloud.com>
2025-12-24 03:22:49 +0000

    Support PasswordLessConfig.

---
+++ aliyun-net-sdk-ecs-workbench/Ecs_workbench/Model/V20251111/DescribeTerminalSettingsRequest.cs
@@ -0,0 +1,50 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core;
+using Aliyun.Acs.Core.Http;
+using Aliyun.Acs.Core.Transform;
+using Aliyun.Acs.Core.Utils;
+using Aliyun.Acs.ecs_workbench;
+using Aliyun.Acs.ecs_workbench.Transform;
+using Aliyun.Acs.ecs_workbench.Transform.V20251111;
+
+namespace Aliyun.Acs.ecs_workbench.Model.V20251111
+{
+    public class DescribeTerminalSettingsRequest : RpcAcsRequest<DescribeTerminalSettingsResponse>
+    {
+        public DescribeTerminalSettingsRequest()
+            : base("ecs-workbench", "2025-11-11", "DescribeTerminalSettings")
+        {
+			Protocol = ProtocolType.HTTPS;
+			Method = MethodType.POST;
+        }
+
+		public override bool CheckShowJsonItemName()
+		{
+			return false;
+		}
+
+        public override DescribeTerminalSettingsResponse GetResponse(UnmarshallerContext unmarshallerContext)
+        {
+            return DescribeTerminalSettingsResponseUnmarshaller.Unmarshall(unmarshallerContext);
+        }
+    }
+}
---
+++ aliyun-net-sdk-ecs-workbench/Ecs_workbench/Transform/V20251111/ModifyTerminalSettingsResponseUnmarshaller.cs
@@ -0,0 +1,39 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+using System;
+using System.Collections.Generic;
+
+using Aliyun.Acs.Core.Transform;
+using Aliyun.Acs.ecs_workbench.Model.V20251111;
+
+namespace Aliyun.Acs.ecs_workbench.Transform.V20251111
+{
+    public class ModifyTerminalSettingsResponseUnmarshaller
+    {
+        public static ModifyTerminalSettingsResponse Unmarshall(UnmarshallerContext _ctx)
+        {
+			ModifyTerminalSettingsResponse modifyTerminalSettingsResponse = new ModifyTerminalSettingsResponse();
+
+			modifyTerminalSettingsResponse.HttpResponse = _ctx.HttpResponse;
+			modifyTerminalSettingsResponse.RequestId = _ctx.StringValue("ModifyTerminalSettings.RequestId");
+        
+			return modifyTerminalSettingsResponse;
+        }
+    }
+}
